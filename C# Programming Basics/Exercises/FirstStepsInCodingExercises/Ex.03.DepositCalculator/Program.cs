911a30bfe158df857629c01cc5c93bf2e5d75f4a
DenisM97 <denis.m.031997@gmail.com>
2021-06-27 16:51:06 +0300

    Ex.03 Finished.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.03.DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex._03.DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSum = double.Parse(Console.ReadLine());
+            int durrationOfDeposit = int.Parse(Console.ReadLine()); // durration in months 
+            double yearlyInterestRate = double.Parse(Console.ReadLine()); // divided by 100 
+            double endSum = depositSum + durrationOfDeposit * ((depositSum * (yearlyInterestRate / 100)) / 12); // look the logic behing it 
+            Console.WriteLine(endSum);
+        }
+    }
+}
