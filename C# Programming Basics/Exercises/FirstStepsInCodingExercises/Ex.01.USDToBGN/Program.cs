dd4ca48976ff6b46cffde6e53c8154163e6d77c4
DenisM97 <denis.m.031997@gmail.com>
2021-06-27 16:34:21 +0300

    Ex.01 Finished.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.01.USDToBGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex._01.USDToBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double currencyBgn = 1.79549;
+            double bgn = usd * currencyBgn;
+            Console.WriteLine(bgn);
+        }
+    }
+}
