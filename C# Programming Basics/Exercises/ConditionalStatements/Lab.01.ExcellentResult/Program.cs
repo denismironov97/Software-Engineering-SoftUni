2a8be6d281fdc98b773345d6c403b034cb43d727
DenisM97 <denis.m.031997@gmail.com>
2021-06-27 17:08:50 +0300

    Lab.Ex.01 Finished.

---
+++ C# Programming Basics/Exercises/ConditionalStatements/Lab.01.ExcellentResult/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab._01.ExcellentResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputGrade = double.Parse(Console.ReadLine());
+            
+            if (inputGrade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
