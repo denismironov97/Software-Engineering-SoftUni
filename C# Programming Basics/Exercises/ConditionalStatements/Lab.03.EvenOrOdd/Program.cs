4f28efec75a1a09e06cebf93fb853cd0bace3fb0
DenisM97 <denis.m.031997@gmail.com>
2021-06-29 14:19:33 +0300

    Lab.03 uploaded.

--- C# Programming Basics/Exercises/ConditionalStatements/Lab.03.EvenOrOdd/Program.cs
+++ C# Programming Basics/Exercises/ConditionalStatements/Lab.03.EvenOrOdd/Program.cs
@@ -6,7 +6,17 @@ namespace Lab._03.EvenOrOdd
     {
         static void Main(string[] args)
         {
-            
+            int number = int.Parse(Console.ReadLine());
+            int numericalRemainder = number % 2;
+
+            if (numericalRemainder == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
         }
     }
 }
