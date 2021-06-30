f33507e48b79e244a263a41601902898af01ddd4
DenisM97 <denis.m.031997@gmail.com>
2021-06-30 19:29:25 +0300

    Lab.07 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatements/Lab.07.AreaOfFigures/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab._07.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfGeometriFigure = Console.ReadLine();
+            double sideOfSquare = 0;
+            double lenghtOfRectangle = 0;
+            double widthOfRectangle = 0;
+            double radiusOfCircle = 0;
+            double sideOfTriangle = 0;
+            double heightOfTriangle = 0;
+
+            if (typeOfGeometriFigure == "square")
+            {
+                sideOfSquare = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(sideOfSquare * sideOfSquare, 3));
+            }
+            else if (typeOfGeometriFigure == "rectangle")
+            {
+                lenghtOfRectangle = double.Parse(Console.ReadLine());
+                widthOfRectangle = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(lenghtOfRectangle * widthOfRectangle, 3));
+            }
+            else if (typeOfGeometriFigure == "circle")
+            {
+                radiusOfCircle = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(Math.PI * (radiusOfCircle * radiusOfCircle), 3));
+            }
+            else if (typeOfGeometriFigure == "triangle")
+            {
+                sideOfTriangle = double.Parse(Console.ReadLine());
+                heightOfTriangle = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round((sideOfTriangle * heightOfTriangle / 2), 3));
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
