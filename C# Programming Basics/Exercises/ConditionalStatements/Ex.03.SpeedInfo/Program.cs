f1e93b44b8bd7c5dbb77a11fb87808ca59898fa1
DenisM97 <denis.m.031997@gmail.com>
2021-07-01 11:07:17 +0300

    Ex.03 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatements/Ex.03.SpeedInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex._03.SpeedInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+            string typeString = "";
+
+            if (speed <= 10)
+            {
+                typeString = "slow";
+            }
+            else if (speed <= 50)
+            {
+                typeString = "average";
+            }
+            else if (speed <= 150)
+            {
+                typeString = "fast";
+            }
+            else if (speed <= 1000)
+            {
+                typeString = "ultra fast";
+            }
+            else
+            {
+                typeString = "extremely fast";
+            }
+
+            Console.WriteLine(typeString);
+        }
+    }
+}
