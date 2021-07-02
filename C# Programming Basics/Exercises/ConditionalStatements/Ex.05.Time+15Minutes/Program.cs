5e39add02caf78fa46b13fc8e7b3e7702cd52c09
DenisM97 <denis.m.031997@gmail.com>
2021-07-02 12:55:36 +0300

    Ex.05 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatements/Ex.05.Time+15Minutes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex._05.Time_15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int fifteenMinutes = 15;
+            int hoursInMinsPlusFifteenMins = hour * 60 + minutes + fifteenMinutes;
+            int trueHours = hoursInMinsPlusFifteenMins / 60;
+            int trueMins = hoursInMinsPlusFifteenMins % 60;
+            int twentyfourHourFormat = trueHours % 24;
+            
+            if (trueHours < 24)
+            {
+                if (trueMins < 10)
+                {
+                    Console.WriteLine($"{trueHours}:0{trueMins}");
+                }
+                else
+                {
+                    Console.WriteLine($"{trueHours}:{trueMins}");
+                }
+            }
+            else
+            {
+                if (trueMins < 10)
+                {
+                    Console.WriteLine($"{twentyfourHourFormat}:0{trueMins}");
+                }
+                else
+                {
+                    Console.WriteLine($"{twentyfourHourFormat}:{trueMins}");
+                }
+            }
+        }
+    }
+}
