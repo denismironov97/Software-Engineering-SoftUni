901c2614108afb32a15f9baf83334dc749790973
DenisM97 <denis.m.031997@gmail.com>
2021-07-04 15:47:51 +0300

    Lab.02 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatementsAdvanced/Lab.02.WkndWorkingDay/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab._02.WkndWorkingDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfTheWeek = Console.ReadLine();
+            string result;
+
+            switch (dayOfTheWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    result = "Working day";
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    result = "Weekend";
+                    break;
+                default:
+                    result = "Error";
+                    break;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
