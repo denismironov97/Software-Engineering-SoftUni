c3a91b83dc899a618e9759faa9575b2524798420
DenisM97 <denis.m.031997@gmail.com>
2021-07-04 15:48:21 +0300

    Lab.03 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatementsAdvanced/Lab.03.AnimalType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab._03.AnimalType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            string animalType = "";
+            
+            switch (animal)
+            {
+                case "dog":
+                    animalType = "mammal";
+                    break;
+
+                case "snake":
+                case "crocodile":
+                case "tortoise":
+                    animalType = "reptile";
+                    break;
+
+                default:
+                    animalType = "unknown";
+                    break;
+            }
+
+            Console.WriteLine(animalType);
+        }
+    }
+}
