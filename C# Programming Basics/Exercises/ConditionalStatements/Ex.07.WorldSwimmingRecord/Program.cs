7610bc55a472d88f1efc6c9c882ef81441ea6d7f
DenisM97 <denis.m.031997@gmail.com>
2021-07-04 15:18:18 +0300

    Ex.07 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatements/Ex.07.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex._07.WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double worldRecord = double.Parse(Console.ReadLine()); // In seconds 
+            double distance = double.Parse(Console.ReadLine()); // In meters 
+            double timeInSeconds = double.Parse(Console.ReadLine()); // That he swims for 1 meter 
+            double swimmerTime = distance * timeInSeconds; // Ivancho's time without the friction in seconds 
+            double friction = Math.Floor(distance / 15); // friction * 12.5 The "times of delay" based on the swimmer's travelled distance 
+            double delay = friction * 12.5; // The delay in seconds 
+            double swimmerRecord = swimmerTime + delay; // Ivancho's time with the friction(delay) in seconds 
+            
+            if (friction >= 1)
+            {
+                if (worldRecord > swimmerRecord)
+                {
+                    Console.WriteLine($"Yes, he succeeded! The new world record is {swimmerRecord:f2} seconds.");
+                }
+                else
+                {
+                    Console.WriteLine($"No, he failed! He was {(swimmerRecord - worldRecord):f2} seconds slower.");
+                }
+            }
+            else // there is no friction meaning distance is below 15 m 
+            {
+                if (worldRecord > swimmerRecord)
+                {
+                    Console.WriteLine($"Yes, he succeeded! The new world record is {swimmerRecord:f2} seconds.");
+                }
+                else
+                {
+                    Console.WriteLine($"No, he failed! He was {(swimmerRecord - worldRecord):f2} seconds slower.");
+                }
+            }
+        }
+    }
+}
