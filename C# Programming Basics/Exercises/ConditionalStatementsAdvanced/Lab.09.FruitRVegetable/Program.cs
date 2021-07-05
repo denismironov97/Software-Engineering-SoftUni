54867d6ec71e9c8f3a641280d8603e7853fdd8b7
DenisM97 <denis.m.031997@gmail.com>
2021-07-05 15:07:00 +0300

    Lab.09 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatementsAdvanced/Lab.09.FruitRVegetable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab._09.FruitRVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string food = Console.ReadLine();
+            bool fruit = food == "banana" || food == "apple" || food == "kiwi" || food == "cherry" || food == "lemon" || food == "grapes";
+            bool vegetable = food == "tomato" || food == "cucumber" || food == "pepper" || food == "carrot";
+            
+            if (fruit)
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (vegetable)
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
