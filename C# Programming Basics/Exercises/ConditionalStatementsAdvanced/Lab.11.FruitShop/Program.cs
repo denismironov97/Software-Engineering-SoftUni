d1c90e57daf9e3d93bebf3cc8e3461c29054f6e1
DenisM97 <denis.m.031997@gmail.com>
2021-07-07 13:50:01 +0300

    Lab.11 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatementsAdvanced/Lab.11.FruitShop/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Lab._11.FruitShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string dayOfTheWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            bool workday = dayOfTheWeek == "Monday" || dayOfTheWeek == "Tuesday" 
+                        || dayOfTheWeek == "Wednesday" || dayOfTheWeek == "Thursday" || dayOfTheWeek == "Friday";
+            bool weekend = dayOfTheWeek == "Saturday" || dayOfTheWeek == "Sunday";
+           
+            if (workday)
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        Console.WriteLine($"{(quantity * 2.50):f2}");
+                        break;
+                    case "apple":
+                        Console.WriteLine($"{(quantity * 1.20):f2}");
+                        break;
+                    case "orange":
+                        Console.WriteLine($"{(quantity * 0.85):f2}");
+                        break;
+                    case "grapefruit":
+                        Console.WriteLine($"{(quantity * 1.45):f2}");
+                        break;
+                    case "kiwi":
+                        Console.WriteLine($"{(quantity * 2.70):f2}");
+                        break;
+                    case "pineapple":
+                        Console.WriteLine($"{(quantity * 5.50):f2}");
+                        break;
+                    case "grapes":
+                        Console.WriteLine($"{(quantity * 3.85):f2}");
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+            }
+            else if (weekend)
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        Console.WriteLine($"{(quantity * 2.70):f2}");
+                        break;
+                    case "apple":
+                        Console.WriteLine($"{(quantity * 1.25):f2}");
+                        break;
+                    case "orange":
+                        Console.WriteLine($"{(quantity * 0.90):f2}");
+                        break;
+                    case "grapefruit":
+                        Console.WriteLine($"{(quantity * 1.60):f2}");
+                        break;
+                    case "kiwi":
+                        Console.WriteLine($"{(quantity * 3):f2}");
+                        break;
+                    case "pineapple":
+                        Console.WriteLine($"{(quantity * 5.60):f2}");
+                        break;
+                    case "grapes":
+                        Console.WriteLine($"{(quantity * 4.20):f2}");
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
