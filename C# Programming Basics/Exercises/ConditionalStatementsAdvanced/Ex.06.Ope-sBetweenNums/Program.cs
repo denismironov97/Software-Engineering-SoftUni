c333b5c8bcb1dbebd3f71f135b572ca20cf1a05e
DenisM97 <denis.m.031997@gmail.com>
2021-07-09 13:12:39 +0300

    Ex.06 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatementsAdvanced/Ex.06.Ope-sBetweenNums/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex._06.Ope_sBetweenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            char symbolForOperations = char.Parse(Console.ReadLine());
+            int result = 0;
+            bool evenOrOdd = symbolForOperations == '+' || symbolForOperations == '-' || symbolForOperations == '*';
+            
+            if (evenOrOdd)
+            {
+                if (symbolForOperations == '+')
+                {
+                    result = num1 + num2;
+
+                }
+                else if (symbolForOperations == '-')
+                {
+                    result = num1 - num2;
+                }
+                else // *
+                {
+                    result = num1 * num2;
+                }
+                
+                int evenOrNot = result % 2;
+                string oddOrEven = "";
+                
+                if (evenOrNot == 0)
+                {
+                    oddOrEven = "even";
+                }
+                else
+                {
+                    oddOrEven = "odd";
+                }
+
+                Console.WriteLine($"{num1} {symbolForOperations} {num2} = {result} - {oddOrEven}");
+            }
+            else if (symbolForOperations == '/' && num2 != 0)
+            {
+                Console.WriteLine($"{num1} {symbolForOperations} {num2} = {(num1 * 1.0 / num2):f2}");
+            }
+            else if (symbolForOperations == '%' && num2 != 0)
+            {
+                Console.WriteLine($"{num1} {symbolForOperations} {num2} = {num1 % num2}");
+            }
+            else
+            {
+                Console.WriteLine($"Cannot divide {num1} by zero");
+            }
+        }
+    }
+}
