13d0439e3316bf5880f13df3f0f78aeca33ddf29
DenisM97 <denis.m.031997@gmail.com>
2021-07-10 20:10:04 +0300

    Ex.09 uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatementsAdvanced/Ex.09.Volleyball/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex._09.Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfYear = Console.ReadLine();
+            int numOfHolidaysInSingleYear = int.Parse(Console.ReadLine()); // p
+            int numOfWeekendsTravelsHome = int.Parse(Console.ReadLine()); // h // number of weekends he travels home 
+            int weekendsInWholeYear = 48;
+            double weekendsInSofia = weekendsInWholeYear - numOfWeekendsTravelsHome;
+            double weekendsPlaysBallInSofia = (weekendsInSofia * 1.0) * 3 / 4; // Saturdays he plays ball = Times he played ball
+            double playsVoleyBallOnHollidays = (numOfHolidaysInSingleYear * 1.0) * 2 / 3; // Times he played ball
+            double weekendsPlaysBallInHomeTown = numOfWeekendsTravelsHome; // Sundays he played ball = Times he played ball;
+            double totalTimesPlayedBall = 0;
+            
+            if (!(typeOfYear == "leap")) // normal year 
+            {
+                totalTimesPlayedBall = weekendsPlaysBallInSofia + playsVoleyBallOnHollidays + weekendsPlaysBallInHomeTown;
+            }
+            else // leap year plays + 15% more 
+            {
+                totalTimesPlayedBall = (weekendsPlaysBallInSofia + playsVoleyBallOnHollidays + weekendsPlaysBallInHomeTown) * 1.15;
+            }
+            Console.WriteLine(Math.Floor(totalTimesPlayedBall));
+        }
+    }
+}
