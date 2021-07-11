994d8291c9ef49c8e59504af73b74660e79888c6
DenisM97 <denis.m.031997@gmail.com>
2021-07-11 21:49:27 +0300

    Lab.04 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.04.EvenPowersOf2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab._04.EvenPowersOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = int.Parse(Console.ReadLine());
+            int a = 2;
+
+            for (int i = 0; i <= b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(Math.Pow(a, i));
+                }
+
+            }
+
+
+            //for (int i = 0; i <= b; i += 2)
+            //{
+            //    Console.WriteLine(Math.Pow(a, i));
+            //}
+        }
+    }
+}
