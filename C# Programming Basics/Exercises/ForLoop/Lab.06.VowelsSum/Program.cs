4b2672bca5883711553479b8dfeb4949a8d0dd0c
DenisM97 <denis.m.031997@gmail.com>
2021-07-11 21:57:51 +0300

    Lab.06 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.06.VowelsSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab._06.VowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int length = input.Length;
+            int sum = 0;
+            
+            for (int n = 0; n < length; n++)
+            {
+                if (input[n] == 'a')
+                {
+                    sum += 1;//sum++
+                }
+                if (input[n] == 'e')
+                {
+                    sum = sum + 2;
+                }
+                if (input[n] == 'i')
+                {
+                    sum += 3;
+                }
+                if (input[n] == 'o')
+                {
+                    sum += 4;
+                }
+                if (input[n] == 'u')
+                {
+                    sum += 5;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
