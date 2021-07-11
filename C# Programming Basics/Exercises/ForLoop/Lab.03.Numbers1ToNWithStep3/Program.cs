b8918b7528dd5c18d2349356be6635ebf57d3bb4
DenisM97 <denis.m.031997@gmail.com>
2021-07-11 21:47:07 +0300

    Lab.03 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.03.Numbers1ToNWithStep3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab._03.Numbers1ToNWithStep3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for(int a = 1; a <= number; a += 3)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ C# Programming Basics/Exercises/ForLoop/Lab.03.Numbers1ToNWithStep3ModV1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab._03.Numbers1ToNWithStep3ModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i += 3)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+    }
+}
