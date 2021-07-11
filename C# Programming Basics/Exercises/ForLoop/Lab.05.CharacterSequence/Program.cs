77ecde61350f193fa5532a5d46612b093fafd393
DenisM97 <denis.m.031997@gmail.com>
2021-07-11 21:56:01 +0300

    Lab.05 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.05.CharacterSequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab._05.CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int lenght = word.Length;
+            
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
+        }
+    }
+}
