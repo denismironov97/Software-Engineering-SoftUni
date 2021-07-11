e6a43bf683bb1c299ec9e2340ba6f9acff245f19
DenisM97 <denis.m.031997@gmail.com>
2021-07-11 21:39:20 +0300

    Lab.02ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.02.NumbersFromNTo1ModV1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab._02.NumbersFromNTo1ModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = number; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
