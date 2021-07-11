84408b3dfa9a68452aa5418134069ebbb8453d1f
DenisM97 <denis.m.031997@gmail.com>
2021-07-11 21:18:18 +0300

    Lab.01 ModV1

--- C# Programming Basics/Exercises/ForLoop/Lab.01.NumbersFrom1To100/Program.cs
+++ C# Programming Basics/Exercises/ForLoop/Lab.01.NumbersFrom1To100/Program.cs
@@ -6,10 +6,15 @@ namespace Lab._01.NumbersFrom1To100
     {
         static void Main(string[] args)
         {
-            for (int n = 1; n <= 100; n += 1)
+            //for (int n = 1; n <= 100; n += 1)
+            //{
+            //    Console.WriteLine(n);
+            //}
+
+            for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine(n);
+                Console.WriteLine(i + 1);
             }
-        }
+        } 
     }
 }
