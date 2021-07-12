40951cc2c14f425c3a12a2b99989186eeb0763a2
DenisM97 <denis.m.031997@gmail.com>
2021-07-12 19:14:20 +0300

    Lab.08 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.08.NumberSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab._08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int smallest = int.MaxValue; // 100
+            int biggest = int.MinValue; // -100
+            
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < smallest) // max value (100)
+                {
+                    smallest = number;
+                }
+                if (number > biggest) // min value (-100)
+                {
+                    biggest = number;
+                }
+            }
+
+            Console.WriteLine($"Max number: {biggest}");
+            Console.WriteLine($"Min number: {smallest}");
+        }
+    }
+}
