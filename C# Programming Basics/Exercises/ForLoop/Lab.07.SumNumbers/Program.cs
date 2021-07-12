23fd18c8d27dd7a6085812c1f798940573a244c3
DenisM97 <denis.m.031997@gmail.com>
2021-07-12 19:12:52 +0300

    Lab.07 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.07.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab._07.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsOfNums = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            for (int i = 0; i < rowsOfNums; i ++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
