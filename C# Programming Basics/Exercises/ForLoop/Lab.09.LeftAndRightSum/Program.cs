72fda5f756c8ee854cccb1a164308539cd3dad83
DenisM97 <denis.m.031997@gmail.com>
2021-07-12 19:16:45 +0300

    Lab.09 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.09.LeftAndRightSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab._09.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                leftSum = leftSum + number;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                rightSum += number; // rightSum = rightSum + int.Parse(Console.ReadLine());
+            }
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                //No, diff = 1
+                Console.WriteLine($"No, diff = {Math.Abs(rightSum - leftSum)}");
+            }
+        }
+    }
+}
