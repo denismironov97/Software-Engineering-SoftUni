135700ba521a997688f4eebf3360fe6959833d46
DenisM97 <denis.m.031997@gmail.com>
2021-07-13 12:39:38 +0300

    Ex.01 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Ex.01.NumbersEndingIn7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex._01.NumbersEndingIn7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 7; i <= 997; i += 10)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
