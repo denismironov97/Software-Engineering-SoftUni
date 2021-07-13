68126a5ba783f3f49398a6ef79cc4de6fbfaf207
DenisM97 <denis.m.031997@gmail.com>
2021-07-13 12:43:00 +0300

    Ex.01 ModV1 uploaded.

--- C# Programming Basics/Exercises/ForLoop/Ex.01.NumbersEndingIn7/Program.cs
+++ C# Programming Basics/Exercises/ForLoop/Ex.01.NumbersEndingIn7/Program.cs
@@ -6,11 +6,6 @@ namespace Ex._01.NumbersEndingIn7
     {
         static void Main(string[] args)
         {
-            //for (int i = 7; i <= 997; i += 10)
-            //{
-            //    Console.WriteLine(i);
-            //}
-
             for (int i = 1; i <= 1000; i++)
             {
                 if (i % 10 == 7)
