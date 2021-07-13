d86f94d1c068aec1f90c65d176bd9e6a95dfef41
DenisM97 <denis.m.031997@gmail.com>
2021-07-13 12:31:50 +0300

    Lab.11 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Lab.11.CleverLily/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab._11.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int moneyFromBirthdays = 0;
+            int moneyGift = 10;
+
+            int toysCount = 0;
+
+            for (int birthDay = 1; birthDay <= age; birthDay++)
+            {
+                if (birthDay % 2 == 0)
+                {
+                    moneyFromBirthdays = moneyFromBirthdays + moneyGift;
+                    moneyGift = moneyGift + 10;
+                    moneyFromBirthdays -= 1;
+                }
+                else
+                {
+                    toysCount++;
+                }
+            }
+
+            int moneyFromToys = toysCount * toyPrice;
+            moneyFromBirthdays += moneyFromToys;
+
+            if (moneyFromBirthdays >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {(moneyFromBirthdays - washingMachinePrice):f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {(Math.Abs(moneyFromBirthdays - washingMachinePrice)):f2}");
+            }
+        }
+    }
+}
