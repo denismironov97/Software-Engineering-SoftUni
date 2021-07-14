59f08ed12f50ebc26c42dade613bd1196e6e35a6
DenisM97 <denis.m.031997@gmail.com>
2021-07-14 14:14:44 +0300

    Ex.06 uploaded.

---
+++ C# Programming Basics/Exercises/ForLoop/Ex.06.Salary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ex._06.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOpenedBrowsers = int.Parse(Console.ReadLine()); // interval 1 to 10
+            int salary = int.Parse(Console.ReadLine()); // interval from 700 incl to 1500 incl
+            
+            for (int openBrowsers = 1; openBrowsers <= numberOfOpenedBrowsers; openBrowsers++)
+            {
+                string currentOpenedBrowser = Console.ReadLine();
+                
+                if (currentOpenedBrowser == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (currentOpenedBrowser == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (currentOpenedBrowser == "Reddit")
+                {
+                    salary -= 50;
+                }
+               
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break; // learn how to use "break"; understand "break;"
+                }
+            }
+            
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
