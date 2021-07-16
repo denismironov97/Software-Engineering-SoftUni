04dfe4441dd9931673d4d4ea217499c4ce44377c
DenisM97 <denis.m.031997@gmail.com>
2021-07-16 18:29:51 +0300

    Lab.02 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.02.Password/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab._02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usrename = Console.ReadLine();
+            string password = Console.ReadLine();
+            string passForEntrance = Console.ReadLine();
+            
+            while (password != passForEntrance)
+            {
+                passForEntrance = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {usrename}!");
+        }
+    }
+}
