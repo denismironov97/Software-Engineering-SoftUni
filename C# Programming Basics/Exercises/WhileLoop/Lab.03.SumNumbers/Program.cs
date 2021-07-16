1a2d80d0efc6b19078914039d4435d2d8e0a3b22
DenisM97 <denis.m.031997@gmail.com>
2021-07-16 18:31:43 +0300

    Lab.03 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.03.SumNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab._03.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            int currentnumber = 0;
+            int sum = 0;
+            
+            while (true)
+            {
+                currentnumber = int.Parse(Console.ReadLine());
+                sum += currentnumber;
+                
+                if (sum >= numberInput)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+            }
+        }
+    }
+}
