993efb79c29dc9d92f4f3c93cba0d105cf4194bb
DenisM97 <denis.m.031997@gmail.com>
2021-07-16 18:27:38 +0300

    Lab.01 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.01.ReadText/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lab._01.ReadText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            string text = "";
+
+            while ((text = Console.ReadLine()) != "Stop")
+            {
+                Console.WriteLine(text);
+            }
+            
+            //2
+            //string text = Console.ReadLine();
+
+            //while (text != "Stop")
+            //{
+            //    Console.WriteLine(text);
+            //    text = Console.ReadLine();
+            //}
+            //
+            //3
+            //string text = "";
+
+            //while (text != "Stop")
+            //{
+            //    text = Console.ReadLine();
+            //
+            //    if (text == "Stop")
+            //    {
+            //        break;
+            //    }
+            //
+            //    Console.WriteLine(text);
+            //}
+
+            //4
+            //string text = "";
+
+            //while (true)
+            //{
+            //    text = Console.ReadLine();
+            //
+            //    if (text == "Stop")
+            //    {
+            //        break;
+            //    }
+            //
+            //    Console.WriteLine(text);
+            //}
+        }
+    }
+}
