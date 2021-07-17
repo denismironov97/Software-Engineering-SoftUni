7c3035d3dde028df16602d99a43b738d6b0a88e9
DenisM97 <denis.m.031997@gmail.com>
2021-07-17 16:31:14 +0300

    Lab.07 uploaded.

--- C# Programming Basics/Exercises/WhileLoop/Lab.07.MinNumber/Program.cs
+++ C# Programming Basics/Exercises/WhileLoop/Lab.07.MinNumber/Program.cs
@@ -6,7 +6,22 @@ namespace Lab._07.MinNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            int minNum = int.MaxValue;
+            
+            while (input != "Stop")
+            {
+                int currentNum = int.Parse(input);
+                
+                if (currentNum < minNum)
+                {
+                    minNum = currentNum;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(minNum);
         }
     }
 }
