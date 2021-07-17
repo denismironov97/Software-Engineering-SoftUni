aecefb849f8d79a9710837ffe12d068ddb924d04
DenisM97 <denis.m.031997@gmail.com>
2021-07-17 16:23:25 +0300

    Lab.04 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.04.Sequence2k+1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab._04.Sequence2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputNum = double.Parse(Console.ReadLine());
+            double operationNum = 1;
+            
+            while (operationNum <= inputNum)
+            {
+                Console.WriteLine(operationNum);
+                operationNum = operationNum * 2 + 1;
+            }
+            
+            //double inputNum = double.Parse(Console.ReadLine()); // 31
+            //double num = 1;
+            
+            //while (true)
+            //{
+            //    Console.WriteLine(num);
+            //    num = num * 2 + 1;
+                
+            //    if (num == inputNum)
+            //    {
+            //        Console.WriteLine(num);
+            //        break;
+            //    }
+            //    else if (num > inputNum)
+            //    {
+            //        break;
+            //    }
+            //}
+        }
+    }
+}
