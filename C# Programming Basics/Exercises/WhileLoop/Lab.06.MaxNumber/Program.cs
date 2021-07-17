f7458031e0cc8709ecb6f603520820572251ca5d
DenisM97 <denis.m.031997@gmail.com>
2021-07-17 16:27:30 +0300

    Lab.06 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.06.MaxNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab._06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputnum = Console.ReadLine();
+            int maxValue = int.MinValue;
+            
+            while (inputnum != "Stop")
+            {
+                int currentNum = int.Parse(inputnum);
+                
+                if (currentNum > maxValue)
+                {
+                    maxValue = currentNum;
+                }
+                inputnum = Console.ReadLine();
+            }
+
+            Console.WriteLine(maxValue);
+        }
+    }
+}
