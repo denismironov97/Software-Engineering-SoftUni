e413ae865496476359b78ffead10aa63990ef150
DenisM97 <denis.m.031997@gmail.com>
2021-07-17 16:26:18 +0300

    Lab.05 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.05.AccountBalance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab._05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber = Console.ReadLine();
+            double sum = 0;
+            
+            while (inputNumber != "NoMoreMoney")
+            {
+                double currentNum = double.Parse(inputNumber);
+                
+                if (currentNum >= 0)
+                {
+                    Console.WriteLine($"Increase: {currentNum:f2}");
+                    sum = sum + currentNum;
+                    inputNumber = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+            }
+
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
