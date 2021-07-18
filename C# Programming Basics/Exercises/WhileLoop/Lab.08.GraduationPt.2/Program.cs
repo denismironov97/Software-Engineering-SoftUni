42230a427a8e4b0c63c4cf5a92b7e34c2f681fdf
DenisM97 <denis.m.031997@gmail.com>
2021-07-18 13:02:34 +0300

    Lab.08 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Lab.08.GraduationPt.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab._08.GraduationPt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfStudent = Console.ReadLine();
+            double grade = 0;
+            double sumOfGrades = 0;
+            int gradeCounter = 0;
+            
+            while (gradeCounter < 12)
+            {
+                grade = double.Parse(Console.ReadLine());
+                gradeCounter++;
+                
+                if (grade < 4) // excluded
+                {
+                    Console.WriteLine($"{nameOfStudent} has been excluded at {gradeCounter} grade");
+                    break;
+                }
+
+                sumOfGrades += grade;
+            }
+
+            if (gradeCounter >= 11)
+            {
+                Console.WriteLine($"{nameOfStudent} graduated. Average grade: {(sumOfGrades / gradeCounter):f2}");
+            }
+        }
+    }
+}
