bcbe72cfd556a679d1eaaf438c291e23a95dcd66
DenisM97 <denis.m.031997@gmail.com>
2021-07-18 13:13:13 +0300

    Ex.01 ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Ex.01.OldBooksModV1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex._01.OldBooksModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            string currentBook = "";
+            int counterForBooks = 0;
+            
+            while ((currentBook = Console.ReadLine()) != favouriteBook && currentBook != "No More Books")
+            {
+                counterForBooks++;
+            }
+
+            if (currentBook == favouriteBook)
+            {
+                Console.WriteLine($"You checked {counterForBooks} books and found it.");
+            }
+            else if (currentBook == "No More Books")
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {counterForBooks} books.");
+            }
+        }
+    }
+}
