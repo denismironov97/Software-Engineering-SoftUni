e1d0e5c6ffb314c023c80acc2014671c70ed649e
DenisM97 <denis.m.031997@gmail.com>
2021-07-18 13:15:27 +0300

    Ex.02 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Ex.02.ExamPreparation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ex._02.ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfUndesirableGradesTreshold = int.Parse(Console.ReadLine());
+            string nameOfExercise = Console.ReadLine();
+            string lastProblem = "";
+            int counterForUndesirableGrades = 0;
+            int counterForDesirableGrades = 0;
+            int sumOfDesirableGrades = 0;
+            int sumOfUndesirableGrades = 0;
+            int counterForAllExercises = 0;
+            int currentGrade = 0;
+
+            while (nameOfExercise != "Enough")
+            {
+                lastProblem = nameOfExercise;
+                counterForAllExercises++;
+                currentGrade = int.Parse(Console.ReadLine());
+
+                if (currentGrade > 4) // desirable grade
+                {
+                    counterForDesirableGrades++;
+                    sumOfDesirableGrades += currentGrade;
+                }
+                else // undesirable grade 
+                {
+                    counterForUndesirableGrades++;
+                    sumOfUndesirableGrades = sumOfUndesirableGrades + currentGrade;
+                }
+
+                if (counterForUndesirableGrades == numOfUndesirableGradesTreshold)
+                {
+                    Console.WriteLine($"You need a break, {counterForUndesirableGrades} poor grades.");
+                    break;
+                }
+                nameOfExercise = Console.ReadLine();
+            }
+
+            if (nameOfExercise == "Enough")
+            {
+                Console.WriteLine($"Average score: {(sumOfDesirableGrades + sumOfUndesirableGrades) * 1.0 / (counterForDesirableGrades + counterForUndesirableGrades):f2}"); // + sumofundesirable
+                Console.WriteLine($"Number of problems: {counterForAllExercises}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
