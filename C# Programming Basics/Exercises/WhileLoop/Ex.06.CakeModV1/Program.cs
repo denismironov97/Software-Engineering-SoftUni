6a191b1dae60dfe758ecb11b83730987d0d61912
DenisM97 <denis.m.031997@gmail.com>
2021-07-19 20:32:35 +0300

    Ex.06 ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Ex.06.CakeModV1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex._06.CakeModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int widthOfCake = int.Parse(Console.ReadLine());
+            int lenghtOfCake = int.Parse(Console.ReadLine());
+            int allPicesOfCake = widthOfCake * lenghtOfCake; //
+            int totalTakenSlices = 0; //
+            int totalCakeSlicesLeft = widthOfCake * lenghtOfCake;
+            string takenPices = "";
+
+            while ((takenPices = Console.ReadLine()) != "STOP")
+            {
+                int currentTakenPices = int.Parse(takenPices);
+                totalCakeSlicesLeft -= currentTakenPices;// деинкрементираме
+                totalTakenSlices += currentTakenPices;
+
+                if (totalCakeSlicesLeft <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {totalTakenSlices - allPicesOfCake} pieces more.");
+                    break;
+                }
+            }
+
+            if (allPicesOfCake > totalTakenSlices)
+            {
+                Console.WriteLine($"{allPicesOfCake - totalTakenSlices} pieces are left.");
+            }
+        }
+    }
+}
