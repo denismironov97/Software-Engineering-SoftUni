9f83650147d77769f8cb69d8b956c75794d327b2
DenisM97 <denis.m.031997@gmail.com>
2021-07-19 20:30:45 +0300

    Ex.06 uploaded.

---
+++ C# Programming Basics/Exercises/WhileLoop/Ex.06.Cake/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex._06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int widthOfCake = int.Parse(Console.ReadLine());
+            int lenghtOfCake = int.Parse(Console.ReadLine());
+            int totalCakeSlices = widthOfCake * lenghtOfCake;
+            int totalTakenSlices = 0;
+            int allCakeSlices = widthOfCake * lenghtOfCake;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "STOP")
+                {
+                    break;
+                }
+
+                int currentTakenSlices = int.Parse(input);
+                totalCakeSlices -= currentTakenSlices;
+                totalTakenSlices += currentTakenSlices;
+
+                if (totalCakeSlices <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {totalTakenSlices - allCakeSlices} pieces more.");
+                    break;
+                }
+            }
+
+            if (allCakeSlices > totalTakenSlices)
+            {
+                Console.WriteLine($"{allCakeSlices - totalTakenSlices} pieces are left.");
+            }
+        }
+    }
+}
