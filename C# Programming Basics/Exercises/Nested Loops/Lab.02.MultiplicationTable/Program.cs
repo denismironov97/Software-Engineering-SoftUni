888c4e2ad7e608eb81073d849bd914eb947923b0
DenisM97 <denis.m.031997@gmail.com>
2021-07-20 20:25:48 +0300

    Lab.02 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Lab.02.MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab._02.MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int multiplier1 = 1; multiplier1 <= 10; multiplier1++)
+            {
+                for (int multiplier2 = 1; multiplier2 <= 10; multiplier2++)
+                {
+                    Console.WriteLine($"{multiplier1} * {multiplier2} = {multiplier1 * multiplier2}");
+                }
+            }
+        }
+    }
+}
