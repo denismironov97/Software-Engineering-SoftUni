a7762adc9afe8fd466323ac3e1b3f17400941394
DenisM97 <denis.m.031997@gmail.com>
2021-07-20 20:23:10 +0300

    Lab.01 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Lab.01.Clock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab._01.Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = 0;
+            int minutes = 0;
+
+            while (hours <= 23)
+            {
+                while (minutes <= 59)
+                {
+                    Console.WriteLine($"{hours}:{minutes}");
+                    minutes++;
+                }
+
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+}
