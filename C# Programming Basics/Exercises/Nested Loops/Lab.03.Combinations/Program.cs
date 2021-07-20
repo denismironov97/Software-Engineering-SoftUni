7e9225db284826a7639b4807b1e95b460d30f680
DenisM97 <denis.m.031997@gmail.com>
2021-07-20 20:28:25 +0300

    Lab.03 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Lab.03.Combinations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab._03.Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counterValidCombinations = 0;
+            
+            for (int x = 0; x <= n; x++)
+            {
+                for (int y = 0; y <= n; y++)
+                {
+                    for (int z = 0; z <= n; z++)
+                    {
+                        if (x + y + z == n)
+                        {
+                            counterValidCombinations++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(counterValidCombinations);
+        }
+    }
+}
