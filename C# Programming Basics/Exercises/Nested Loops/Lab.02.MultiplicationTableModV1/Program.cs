c09b69fd99e740d1db95596248d56d43f7a2c8c5
DenisM97 <denis.m.031997@gmail.com>
2021-07-20 20:26:51 +0300

    Lab.02 ModV1 uploaded.

--- C# Programming Basics/Exercises/Nested Loops/Lab.02.MultiplicationTableModV1/Program.cs
+++ C# Programming Basics/Exercises/Nested Loops/Lab.02.MultiplicationTableModV1/Program.cs
@@ -6,7 +6,20 @@ namespace Lab._02.MultiplicationTableModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int multiplier1 = 1;
+            int multiplier2 = 1;
+
+            while (multiplier1 <= 10)
+            {
+                while (multiplier2 <= 10)
+                {
+                    Console.WriteLine($"{multiplier1} * {multiplier2} = {multiplier1 * multiplier2}");
+                    multiplier2++;
+                }
+
+                multiplier2 = 1;
+                multiplier1++;
+            }
         }
     }
 }
