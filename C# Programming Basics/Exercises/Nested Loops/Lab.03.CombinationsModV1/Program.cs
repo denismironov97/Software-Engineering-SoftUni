85415a450b84de05cf16f394f2437c27ab73b1bd
DenisM97 <denis.m.031997@gmail.com>
2021-07-20 20:29:53 +0300

    Lab.03 ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Lab.03.CombinationsModV1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab._03.CombinationsModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x1 = 0;
+            int x2 = 0;
+            int x3 = 0;
+            int counterValidCombinations = 0;
+
+            while (x1 <= n)
+            {
+                while (x2 <= n)
+                {
+                    while (x3 <= n)
+                    {
+                        if (x1 + x2 + x3 == n)
+                        {
+                            counterValidCombinations += 1;
+                        }
+                        x3++;
+                    }
+                    x3 = 0;
+                    x2++;
+                }
+                x2 = 0;
+                x1++;
+            }
+
+            Console.WriteLine(counterValidCombinations);
+        }
+    }
+}
