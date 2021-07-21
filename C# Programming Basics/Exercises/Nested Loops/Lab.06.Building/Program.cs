a5c5bb04ef10b4c2f346c4245a7396507a00acc7
DenisM97 <denis.m.031997@gmail.com>
2021-07-21 22:28:49 +0300

    Lab.06 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Lab.06.Building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab._06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numOfFloors = int.Parse(Console.ReadLine());
+            int numOfRooms = int.Parse(Console.ReadLine());
+
+            for (int floor = numOfFloors; floor > 0; floor--)
+            {
+                for (int room = 0; room < numOfRooms; room++)
+                {
+                    if (floor == numOfFloors)
+                    {
+                        Console.Write($"L{floor}{room} ");
+                    }
+                    else
+                    {
+                        if (floor % 2 == 0) // office 
+                        {
+                            Console.Write($"O{floor}{room} ");
+                        }
+                        else // aprtment 
+                        {
+                            Console.Write($"A{floor}{room} ");
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
