99f90a41c426d29228b0b4695f5ac7d527d4a4d7
DenisM97 <denis.m.031997@gmail.com>
2021-07-21 22:25:59 +0300

    Lab.05 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Lab.05.Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab._05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            
+            while (destination != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double totalSavings = 0;
+
+                while (totalSavings < budget)
+                {
+                    double currentSavedMoney = double.Parse(Console.ReadLine());
+                    totalSavings = totalSavings + currentSavedMoney;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
