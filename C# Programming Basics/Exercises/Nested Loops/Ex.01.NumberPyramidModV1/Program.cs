d074652c0d6efbda88f1a80bdaa815bdd2a0cd7b
DenisM97 <denis.m.031997@gmail.com>
2021-07-22 10:39:41 +0300

    Ex.01 ModV1 uploaded.

--- C# Programming Basics/Exercises/Nested Loops/Ex.01.NumberPyramidModV1/Program.cs
+++ C# Programming Basics/Exercises/Nested Loops/Ex.01.NumberPyramidModV1/Program.cs
@@ -6,7 +6,36 @@ namespace Ex._01.NumberPyramidModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int rows = 1;
+            int collons = 1;
+            int number = 1;
+            bool numIsBigger = false;
+
+            while (rows <= n)
+            {
+                while (collons <= rows)
+                {
+                    Console.Write(number + " ");
+                    number++;
+                    collons++;
+
+                    if (number > n)
+                    {
+                        numIsBigger = true;
+                        break;
+                    }
+                }
+
+                collons = 1;
+                rows++;
+                Console.WriteLine();
+
+                if (numIsBigger)
+                {
+                    break;
+                }
+            }
         }
     }
 }
