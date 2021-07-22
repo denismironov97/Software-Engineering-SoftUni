efccb3132688a16085537a08bfe78aa2adba3353
DenisM97 <denis.m.031997@gmail.com>
2021-07-22 10:38:35 +0300

    Ex.01 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Ex.01.NumberPyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex._01.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 1;
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int collons = 1; collons <= rows; collons++)
+                {
+                    Console.Write(number + " ");
+                    number++;
+                    if (number > n)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                if (number > n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
