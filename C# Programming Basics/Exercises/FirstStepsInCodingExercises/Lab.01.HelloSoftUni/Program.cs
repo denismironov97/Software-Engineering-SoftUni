b7f2fd57f107e9b2b1c8bb3d5d757c7bd976c758
DenisM97 <denis.m.031997@gmail.com>
2021-07-23 09:27:49 +0300

    Lab.01 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.01.HelloSoftUni/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab._01.HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello SoftUni");
+            //Console.WriteLine($"Hello SoftUni");
+            Console.Write("Hello SoftUni");
+        }
+    }
+}
