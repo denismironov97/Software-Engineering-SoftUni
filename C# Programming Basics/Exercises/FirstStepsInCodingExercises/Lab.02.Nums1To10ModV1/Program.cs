d47126f730521efb9168b2683037f1567f234585
DenisM97 <denis.m.031997@gmail.com>
2021-07-23 09:32:25 +0300

    Lab.02 ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.02.Nums1To10ModV1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab._02.Nums1To10ModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number < 11; number++)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
