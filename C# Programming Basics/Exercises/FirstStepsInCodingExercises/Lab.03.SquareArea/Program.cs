e1bbf0168a92750b323e4cfd315314f01cc47d51
DenisM97 <denis.m.031997@gmail.com>
2021-07-25 16:43:59 +0300

    Lab.03 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.03.SquareArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab._03.SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int areaOfSquare = a * a;
+            Console.WriteLine(areaOfSquare);
+            //Console.WriteLine(a * a);
+        }
+    }
+}
