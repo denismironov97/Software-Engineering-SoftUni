40c93b0a0ac20a61fe9a64af124ab45a79387867
DenisM97 <denis.m.031997@gmail.com>
2021-07-25 16:48:14 +0300

    Lab.05 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.05.GreetingByName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab._05.GreetingByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + '!');
+        }
+    }
+}
