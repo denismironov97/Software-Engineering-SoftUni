151b8a92171b781ab8833f70b18ed0ac04c15360
DenisM97 <denis.m.031997@gmail.com>
2021-07-25 16:49:32 +0300

    Lab.06 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.06.ConcatenateData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab._06.ConcatenateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма, която прочита от конзолата име, фамилия, възраст и град и печата съобщение от следния вид: 
+            //"You are <firstName> <lastName>, a <age>-years old person from <town>."
+
+            string firstName = Console.ReadLine();
+            string familyName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+
+            Console.WriteLine("You are " + firstName + " " + familyName + ", a " + age + "-years old person from" + " " + town + ".");
+        }
+    }
+}
