ba631d6e66ccfbcdf0437ae6665bd68c1f1dce99
DenisM <denis.m.031997@gmail.com>
2021-07-26 14:36:54 +0300

    Lab.07 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.07.PrjCre-ion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab._07.PrjCre_ion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfArchitect = Console.ReadLine();
+            int numberOfProjcets = int.Parse(Console.ReadLine());
+            int hoursToCompleteProjects = numberOfProjcets * 3;
+            //Console.WriteLine($"The architect {nameOfArchitect} will need {hoursToCompleteProjects} hours to complete {numberOfProjcets} project/s.");
+            Console.WriteLine($"The architect {nameOfArchitect} will need {numberOfProjcets * 3} hours to complete {numberOfProjcets} project/s.");
+        }
+    }
+}
