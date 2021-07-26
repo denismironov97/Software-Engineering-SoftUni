f6bf5d53b3d00da6e26530ef63b8069b43e507f8
DenisM <denis.m.031997@gmail.com>
2021-07-26 14:40:33 +0300

    Lab.08 ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.08.PetShopV1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab._08.PetShopV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDogs = int.Parse(Console.ReadLine());
+            int numberOfPetsOfNeighbor = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{numberOfDogs * 2.50 + numberOfPetsOfNeighbor * 4} lv.");
+        }
+    }
+}
