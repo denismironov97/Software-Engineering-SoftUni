9261540ab048e9e78af8de1c6637a9a7e0bc28b1
DenisM <denis.m.031997@gmail.com>
2021-07-26 14:32:45 +0300

    Lab.06 ModV1 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.06.ConctModV1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab._06.ConctModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string familyName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+            Console.WriteLine($"You are {firstName} {familyName}, a {age}-years old person from {town}.");
+        }
+    }
+}
