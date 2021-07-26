2e178508b17e524b876904658357e1b7a913f425
DenisM <denis.m.031997@gmail.com>
2021-07-26 14:38:50 +0300

    Lab.08 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.08.PetShop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab._08.PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDogs = int.Parse(Console.ReadLine());
+            int numberOfPetsOfNeighbor = int.Parse(Console.ReadLine());
+            double priceOfFoodForDogs = 2.50;
+            int priceOfFoodForNeighborPets = 4;
+            double totalCostOfFood = numberOfDogs * priceOfFoodForDogs + numberOfPetsOfNeighbor * priceOfFoodForNeighborPets;
+            Console.WriteLine($"{totalCostOfFood} lv.");
+        }
+    }
+}
