6f844988d386c5429342b847c13162690853254e
DenisM97 <denis.m.031997@gmail.com>
2021-07-27 18:01:38 +0300

    Ex.05 uploaded.

--- C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.05.BirthdayParty/Program.cs
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.05.BirthdayParty/Program.cs
@@ -6,7 +6,16 @@ namespace Ex._05.BirthdayParty
     {
         static void Main(string[] args)
         {
-            
+            double rentOfHall = double.Parse(Console.ReadLine());
+            double bCake = rentOfHall * 0.2;
+            double drinks = bCake - bCake * 0.45; // = bCake * 0.55;
+            double entartainer = rentOfHall / 3;
+            double budget = rentOfHall + bCake + drinks + entartainer;
+            Console.WriteLine(budget);
+
+            //Торта  – цената ѝ е 20 % от наема на залата
+            //Напитки – цената им е 45 % по - малко от тази на тортата
+            //Аниматор – цената му е 1 / 3 от цената за наема на залата
         }
     }
 }
