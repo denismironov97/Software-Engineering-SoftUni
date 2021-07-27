76b0102d1c9adbc3fea2535bdf4bd94ebee45a04
DenisM97 <denis.m.031997@gmail.com>
2021-07-27 17:56:51 +0300

    Lab.09 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Lab.09.YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab._09.YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double plotOfLand = double.Parse(Console.ReadLine());
+            double priceOfOneSqrMLanscapedPlot = 7.61;
+            double totalPriceOfLanscapedPlot = plotOfLand * priceOfOneSqrMLanscapedPlot; // total price without discount
+            double discountPrice = totalPriceOfLanscapedPlot * 0.18; // the discount
+            double discountedPriceOfLanscapedPlot = totalPriceOfLanscapedPlot - discountPrice; // final price with the discount
+            Console.WriteLine($"The final price is: {discountedPriceOfLanscapedPlot} lv.");
+            Console.WriteLine($"The discount is: {discountPrice} lv.");
+        }
+    }
+}
