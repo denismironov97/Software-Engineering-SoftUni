01ed576d234bec00b1f0c39245d74a8fd1988e3f
DenisM97 <denis.m.031997@gmail.com>
2021-07-27 18:01:02 +0300

    Ex.04 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.04.VacationBooksList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex._04.VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numOfPagesInWholeBook = double.Parse(Console.ReadLine());
+            double numOfPagesOnehour = double.Parse(Console.ReadLine());
+            int numOfDaysToReadBook = int.Parse(Console.ReadLine());
+            double totalTime = numOfPagesInWholeBook / numOfPagesOnehour;
+            double hoursPerDay = totalTime / numOfDaysToReadBook;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.05.BirthdayParty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._05.BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
