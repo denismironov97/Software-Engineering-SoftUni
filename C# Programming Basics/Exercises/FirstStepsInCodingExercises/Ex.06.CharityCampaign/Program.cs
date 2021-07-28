838248e547400928abb14b3cfeca7b343ce8185b
DenisM97 <denis.m.031997@gmail.com>
2021-07-28 13:27:26 +0300

    Ex.06 uploaded.

---
+++ C# Programming Basics/Exercises/FirstStepsInCodingExercises/Ex.06.CharityCampaign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex._06.CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Броят на дните, в които тече кампанията – цяло число в интервала[0 … 365]
+            //2.Броят на сладкарите – цяло число в интервала[0 … 1000]
+            //3.Броят на тортите – цяло число в интервала[0… 2000]
+            //4.Броят на гофретите – цяло число в интервала[0 … 2000]
+            //5.Броят на палачинките – цяло число в интервала[0 … 2000]
+            //•	Торта - 45 лв.
+            //•	Гофрета - 5.80 лв.
+            //•	Палачинка – 3.20 лв.
+
+            int days = int.Parse(Console.ReadLine());
+            int numOfSweets = int.Parse(Console.ReadLine());
+            int numOfCakes = int.Parse(Console.ReadLine());
+            int numOfWaffles = int.Parse(Console.ReadLine());
+            int numOfPancakes = int.Parse(Console.ReadLine());
+            double priceOfCake = 45.00;
+            double priceOfWaffle = 5.80;
+            double priceOfPancake = 3.20;
+            double totalRevenue = days * (numOfSweets * (numOfCakes * priceOfCake + numOfWaffles * priceOfWaffle + numOfPancakes * priceOfPancake));
+            double totalCost = totalRevenue / 8;
+            double netProfit = totalRevenue - totalCost;
+            Console.WriteLine(netProfit);
+        }
+    }
+}
