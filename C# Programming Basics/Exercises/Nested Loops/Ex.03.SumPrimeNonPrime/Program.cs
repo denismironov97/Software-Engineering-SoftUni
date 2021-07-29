b38548f9e6bf38d5694585e6a6326e502c15a77d
DenisM97 <denis.m.031997@gmail.com>
2021-07-29 20:58:40 +0300

    Ex.03 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Ex.03.SumPrimeNonPrime/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex._03.SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;// string = "";
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                int currentNum = int.Parse(input);
+                if (currentNum < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+                int countForDivisions = 0;
+                for (int numberOfDivisors = 1; numberOfDivisors <= currentNum; numberOfDivisors++)
+                {
+                    if (currentNum % numberOfDivisors == 0) // prime number 
+                    {
+                        countForDivisions++;
+                    }
+                }
+                if (countForDivisions > 2) // nonPRimenumber 
+                {
+                    nonPrimeSum += currentNum;
+                }
+                else // prime number 
+                {
+                    primeSum += currentNum;
+                }
+
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
