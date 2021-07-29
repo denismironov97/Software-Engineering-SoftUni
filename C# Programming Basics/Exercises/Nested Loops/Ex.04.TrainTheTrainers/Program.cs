2c50e256c0965a7db49a7c5d064aeec4f5c6d4c1
DenisM97 <denis.m.031997@gmail.com>
2021-07-29 21:02:22 +0300

    Ex.04 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Ex.04.TrainTheTrainers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex._04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfJudges = int.Parse(Console.ReadLine()); //n
+            string presentation = Console.ReadLine();
+            double totalAverageGRadeSumOfStudent = 0;
+            int numOfPresentations = 0;
+
+            while (presentation != "Finish")
+            {
+                double inputGrade = 0;
+                double sumOfInputGrades = 0;
+                double averageGradeOfPresentation = 0;
+                numOfPresentations++;
+                for (int interator = 1; interator <= numOfJudges; interator++)
+                {
+                    inputGrade = double.Parse(Console.ReadLine());
+                    sumOfInputGrades += inputGrade;
+                }
+                averageGradeOfPresentation = sumOfInputGrades / numOfJudges;
+                totalAverageGRadeSumOfStudent += averageGradeOfPresentation; // sum of the average grades for each presentation;
+                Console.WriteLine($"{presentation} - {averageGradeOfPresentation:f2}.");
+                presentation = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Student's final assessment is {totalAverageGRadeSumOfStudent / numOfPresentations:f2}.");
+        }
+    }
+}
