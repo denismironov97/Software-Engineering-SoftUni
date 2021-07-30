0cb38cc151147aa198e3e2a4623d3a64dca17c0c
DenisM97 <denis.m.031997@gmail.com>
2021-07-30 11:17:23 +0300

    Ex. 06 uploaded.

---
+++ C# Programming Basics/Exercises/Nested Loops/Ex.06.SpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex._06.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numN = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int l = 1; l <= 9; l++)
+                        {
+                            if (numN % i == 0 && numN % j == 0 && numN % k == 0 && numN % l == 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
