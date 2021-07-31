193c1e64749d75899318030d3111ebef69801530
DenisM97 <denis.m.031997@gmail.com>
2021-07-31 12:42:41 +0300

    Lab.01 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.01.StudentInformation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L._01.StudentInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Name: {student name}, Age: {student age}, Grade: {student grade}"
+            //Name: John, Age: 15, Grade: 5.40
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+            Console.Write("Name: " + studentName + ", " + "Age: " + age + ", " + "Grade: ");
+            Console.WriteLine($"{averageGrade:f2}");
+        }
+    }
+}
