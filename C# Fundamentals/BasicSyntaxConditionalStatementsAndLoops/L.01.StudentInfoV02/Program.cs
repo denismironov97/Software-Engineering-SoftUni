ba89597da68e25fee0247af72a109f2e8c5215ee
DenisM97 <denis.m.031997@gmail.com>
2021-07-31 12:47:40 +0300

    Lab.01 uploaded. ModV2 Interpolation.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.01.StudentInfoV02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L._01.StudentInfoV02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Name: {student name}, Age: {student age}, Grade: {student grade}"
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade:f2}");
+        }
+    }
+}
