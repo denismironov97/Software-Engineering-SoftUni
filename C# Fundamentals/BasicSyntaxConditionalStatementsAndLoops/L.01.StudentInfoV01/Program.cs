2804c17c2441949c55885c43bf38ed6aeceecbd0
DenisM97 <denis.m.031997@gmail.com>
2021-07-31 12:45:28 +0300

    Lab.01 uploaded. ModV1 Placeholders.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.01.StudentInfoV01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L._01.StudentInfoV01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Name: {student name}, Age: {student age}, Grade: {student grade}"
+            //Name: John, Age: 15, Grade: 5.40
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+            Console.WriteLine("Name: {0}, Age: {1}, Grade: {2:f2}", studentName, age, averageGrade); // 3) Placeholders
+        }
+    }
+}
