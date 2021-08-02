3ce3d1819bd4da08b4f4a51ada80e4ae86a67883
DenisM97 <denis.m.031997@gmail.com>
2021-08-02 11:11:59 +0300

    Lab.02 ModV1 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.02.PassedModV1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace L._02.PassedModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = Convert.ToDouble(Console.ReadLine());
+            
+            bool passed = grade >= 3;
+            switch (passed)
+            {
+                case true:
+                    Console.WriteLine("Passed!");
+                    break;
+            }
+        }
+    }
+}
