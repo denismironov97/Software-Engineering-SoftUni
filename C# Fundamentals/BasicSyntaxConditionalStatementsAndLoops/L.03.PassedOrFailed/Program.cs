a0377b6be500d0049152f18466ece2dc46e561d4
DenisM97 <denis.m.031997@gmail.com>
2021-08-02 11:14:27 +0300

    Lab.03 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.03.PassedOrFailed/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace L._03.PassedOrFailed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            
+            bool passed = grade >= 3;
+            switch (passed)
+            {
+                case true:
+                    Console.WriteLine("Passed!");
+                    break;
+                default:
+                    Console.WriteLine("Failed!");
+                    break;
+            }
+        }
+    }
+}
