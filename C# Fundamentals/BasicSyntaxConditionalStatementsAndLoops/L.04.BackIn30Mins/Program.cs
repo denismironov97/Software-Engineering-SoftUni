5c6f695f97b70914aa414efe40753fb738c1cb7a
DenisM97 <denis.m.031997@gmail.com>
2021-08-03 11:29:27 +0300

    Lab.04 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.04.BackIn30Mins/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace L._04.BackIn30Mins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int totalTimeAfter30InMins = hours * 60 + minutes + 30;
+            
+            if (totalTimeAfter30InMins / 60 >= 24) //24 hour format 
+            {
+                Console.WriteLine($"{totalTimeAfter30InMins / 60 % 24}:{totalTimeAfter30InMins % 60:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{totalTimeAfter30InMins / 60}:{totalTimeAfter30InMins % 60:d2}");
+            }
+        }
+    }
+}
