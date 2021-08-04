27435c8d706e07109364d7ea92a5e00aaab73379
DenisM97 <denis.m.031997@gmail.com>
2021-08-04 12:33:06 +0300

    Lab.06 ModV1 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.06.ForeignLangsV1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L._06.ForeignLangsV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
