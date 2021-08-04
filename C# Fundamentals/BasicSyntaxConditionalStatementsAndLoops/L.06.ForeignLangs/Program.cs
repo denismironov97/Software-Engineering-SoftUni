291fa338551fa129ab81f4f2b95feed0508868be
DenisM97 <denis.m.031997@gmail.com>
2021-08-04 11:27:26 +0300

    Lab.06 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.06.ForeignLangs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L._06.ForeignLangs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string english = "English";
+            string spanish = "Spanish";
+            if (country == "USA" || country == "England")
+            {
+                Console.WriteLine("{0}", english);
+            }
+            else if (country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                Console.WriteLine("{0}", spanish);
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
