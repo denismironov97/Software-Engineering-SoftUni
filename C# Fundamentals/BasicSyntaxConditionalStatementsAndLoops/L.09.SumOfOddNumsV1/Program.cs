2db4ce2444b250de3684da9953ff67fdd1adc408
DenisM97 <denis.m.031997@gmail.com>
2021-08-05 11:26:45 +0300

    Lab.09 ModV1 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.09.SumOfOddNumsV1/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.09.SumOfOddNumsV1/Program.cs
@@ -6,7 +6,14 @@ namespace L._09.SumOfOddNumsV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numRows = int.Parse(Console.ReadLine());
+            var sumOfOddNumbers = 0;
+            for (var number = 1; number <= numRows; number++)
+            {
+                Console.WriteLine("{0}", number * 2 - 1);
+                sumOfOddNumbers = sumOfOddNumbers + 2 * number - 1;
+            }
+            Console.WriteLine("Sum: {0}", sumOfOddNumbers);
         }
     }
 }
