c6e67a31cd70dc3d1498aa8253f551ab06de06fe
DenisM97 <denis.m.031997@gmail.com>
2021-08-06 12:45:41 +0300

    Lab.10 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.10.Multiplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L._10.Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int multiplier = 10;
+            for (int iterations = 1; iterations <= multiplier; iterations++)
+            {
+                Console.WriteLine($"{inputNumber} X {iterations} = {inputNumber * iterations}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.11.Mul-nTable 2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._11.Mul_nTable_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
