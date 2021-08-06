39154f41c74c60d7e37cfba4791f1fdb820844c0
DenisM97 <denis.m.031997@gmail.com>
2021-08-06 12:46:55 +0300

    Lab.11 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.11.Mul-nTable 2.0/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.11.Mul-nTable 2.0/Program.cs
@@ -6,7 +6,20 @@ namespace L._11.Mul_nTable_2._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int inputNumber = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            int product = 0;
+            if (multiplier > 10)
+            {
+                Console.WriteLine("{0} X {1} = {2}", inputNumber, multiplier, inputNumber * multiplier);
+                Environment.Exit(0);
+            }
+            for (int beginingPointOfIterations = multiplier; beginingPointOfIterations <= 10; beginingPointOfIterations++)
+            {
+                product = inputNumber * multiplier;
+                Console.WriteLine("{0} X {1} = {2}", inputNumber, multiplier, product);
+                multiplier += 1;
+            }
         }
     }
 }
