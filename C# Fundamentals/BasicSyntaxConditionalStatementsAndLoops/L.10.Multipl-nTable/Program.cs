535b68b798c8b0e241bede5226e3cebd11f037dc
DenisM97 <denis.m.031997@gmail.com>
2021-08-06 12:46:15 +0300

    Lab.10 ModV1 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.10.Multipl-nTable/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.10.Multipl-nTable/Program.cs
@@ -6,7 +6,14 @@ namespace L._10.Multipl_nTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var number = int.Parse(Console.ReadLine());
+            var multiplier = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                var result = number * multiplier;
+                Console.WriteLine("{0} X {1} = {2}", number, multiplier, result);
+                multiplier++;
+            }
         }
     }
 }
