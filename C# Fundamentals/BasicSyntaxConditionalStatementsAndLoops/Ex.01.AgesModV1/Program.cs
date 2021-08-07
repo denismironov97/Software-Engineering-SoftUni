abb89a27132cc982fdf144f2b73a8463e7b02673
DenisM97 <denis.m.031997@gmail.com>
2021-08-08 00:09:36 +0300

    Ex.01 ModV1 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.01.AgesModV1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex._01.AgesModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputAge = Convert.ToInt32(Console.ReadLine());
+            bool baby = inputAge >= 0 && inputAge <= 2;
+            bool child = inputAge >= 3 && inputAge <= 13;
+            bool teen = inputAge >= 14 && inputAge <= 19;
+            bool adult = inputAge >= 20 && inputAge <= 65;
+            bool elder = inputAge > 65; // 66 and above
+            
+            if (baby)
+                Console.WriteLine("baby");
+            else if (child)
+                Console.WriteLine("child");
+            else if (teen) 
+                Console.WriteLine("teenager");
+            else if (adult)
+                Console.WriteLine("adult");
+            else if (elder)
+                Console.WriteLine("elder");
+
+        }
+    }
+}
