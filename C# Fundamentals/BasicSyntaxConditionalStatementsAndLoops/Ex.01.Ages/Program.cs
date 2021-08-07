acfc97cf3bba25d52f58723e85c9e29f55132765
DenisM97 <denis.m.031997@gmail.com>
2021-08-07 23:57:36 +0300

    Ex.01 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.01.Ages/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.01.Ages/Program.cs
@@ -6,7 +6,32 @@ namespace Ex._01.Ages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int inputAge = Convert.ToInt32(Console.ReadLine());
+            bool baby = inputAge >= 0 && inputAge <= 2;
+            bool child = inputAge >= 3 && inputAge <= 13;
+            bool teen = inputAge >= 14 && inputAge <= 19;
+            bool adult = inputAge >= 20 && inputAge <= 65;
+            bool elder = inputAge > 65; // 66 and above
+            if (baby)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (child)
+            {
+                Console.WriteLine("child");
+            }
+            else if (teen)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (adult)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (elder)
+            {
+                Console.WriteLine("elder");
+            }
         }
     }
 }
