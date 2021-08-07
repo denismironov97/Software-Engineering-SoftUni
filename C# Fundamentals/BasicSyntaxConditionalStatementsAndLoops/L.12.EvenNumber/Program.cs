33bdcdd27819b56635fa5d5cfc6e50e04313ba60
DenisM97 <denis.m.031997@gmail.com>
2021-08-07 23:53:59 +0300

    Lab.12 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.12.EvenNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L._12.EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int inputNum = int.Parse(Console.ReadLine());
+                if (inputNum % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(inputNum)}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/L.13.H-sB-nTwoDates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._13.H_sB_nTwoDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
