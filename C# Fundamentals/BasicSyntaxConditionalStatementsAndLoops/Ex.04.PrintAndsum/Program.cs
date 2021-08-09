4623f517de4299a234020e7de64eac41c3bd455c
DenisM97 <denis.m.031997@gmail.com>
2021-08-09 11:57:46 +0300

    Ex.04 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.04.PrintAndsum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex._04.PrintAndsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stratingNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (stratingNum <= endNum)
+            {
+                Console.Write(stratingNum + " ");
+                sum += stratingNum;
+                stratingNum++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
