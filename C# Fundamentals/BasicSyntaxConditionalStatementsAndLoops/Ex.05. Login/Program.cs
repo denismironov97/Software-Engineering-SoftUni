bd199b316081ab903997c350e18fdcc5137fd26d
DenisM97 <denis.m.031997@gmail.com>
2021-08-09 12:00:25 +0300

    Ex.05 uploaded.

---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.05. Login/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex._05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            int usernameLenght = username.Length; // num representation of lenght of text;
+            string password = string.Empty;
+            //char name = username[usernameLenght - 1]; // the last char/letter of the text;
+            for (int i = usernameLenght - 1; i >= 0; i--)
+            {
+                // Console.WriteLine(username[i]); Obhodvane(Crawling) of the word(username) in reverse;
+                password = password + username[i];
+            }
+            for (int tries = 1; tries <= 4; tries++)
+            {
+                string matchPassword = Console.ReadLine();
+                if (matchPassword == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else if (tries == 4 && matchPassword != password)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                else if (matchPassword != password)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.06.StrongNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
