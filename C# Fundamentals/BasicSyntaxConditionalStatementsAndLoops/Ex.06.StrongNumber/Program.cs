a774e93e062cc4c5d477022c7b1028732706869e
DenisM97 <denis.m.031997@gmail.com>
2021-08-09 12:01:44 +0300

    Ex.06 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.06.StrongNumber/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.06.StrongNumber/Program.cs
@@ -6,7 +6,29 @@ namespace Ex._06.StrongNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputNum = Console.ReadLine(); //145
+            int lenghtOfNum = inputNum.Length;
+            int sumOfFacktoriels = 0;
+            for (int digitCount = 0; digitCount < lenghtOfNum; digitCount++)
+            {
+                int currentFacktoriel = inputNum[digitCount] - 48;//important has to to with ASCII table
+                //Console.WriteLine(currentFacktoriel);
+                int currentProduct = 1;
+                for (int a = 1; a <= currentFacktoriel; a++)//
+                {
+                    currentProduct *= a;
+                }
+                sumOfFacktoriels += currentProduct;
+            }
+            int parsedInputNum = int.Parse(inputNum);
+            if (sumOfFacktoriels == parsedInputNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
