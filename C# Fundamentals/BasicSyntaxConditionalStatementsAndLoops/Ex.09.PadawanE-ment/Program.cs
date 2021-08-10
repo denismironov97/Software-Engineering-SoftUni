1dc4fb00e2646a0cbfdbd0643ac0dbfaff1790ba
DenisM97 <denis.m.031997@gmail.com>
2021-08-10 17:11:41 +0300

    Ex.09 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.09.PadawanE-ment/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.09.PadawanE-ment/Program.cs
@@ -6,7 +6,24 @@ namespace Ex._09.PadawanE_ment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double moneyOnHand = double.Parse(Console.ReadLine());
+            int numOfStudents = int.Parse(Console.ReadLine());
+            double priceOfSingleSaber = double.Parse(Console.ReadLine());
+            double priceOfSingleRobe = double.Parse(Console.ReadLine());
+            double priceOfSingleBelt = double.Parse(Console.ReadLine());
+
+            double numOfNeededLightSabers = Math.Ceiling(numOfStudents * 1.1);
+            int numFreeBelts = numOfStudents / 6; // the num of the free belts 
+            double totalCost = numOfNeededLightSabers * priceOfSingleSaber + (priceOfSingleBelt * (numOfStudents - numFreeBelts)) + numOfStudents * priceOfSingleRobe;
+
+            if (totalCost <= moneyOnHand)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalCost:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {totalCost - moneyOnHand:f2}lv more.");
+            }
         }
     }
 }
