79884ba1d179c105a881bdc97f7afaba96048ac4
DenisM97 <denis.m.031997@gmail.com>
2021-08-10 17:03:14 +0300

    Ex.08 uploaded.

--- C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.08.TriangleNums/Program.cs
+++ C# Fundamentals/BasicSyntaxConditionalStatementsAndLoops/Ex.08.TriangleNums/Program.cs
@@ -6,7 +6,15 @@ namespace Ex._08.TriangleNums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOfRows = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numOfRows; i++) // cyckle for the rows 
+            {
+                for (int j = 1; j <= i; j++) // cyckle for the colllons 
+                {
+                    Console.Write("{0} ", i);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
