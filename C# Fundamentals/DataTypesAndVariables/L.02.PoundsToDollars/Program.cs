08c23e92b6ed20ff671f3fff50a6e72a69a73a90
DenisM97 <denis.m.031997@gmail.com>
2021-08-12 13:25:21 +0300

    Lab.02 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/L.02.PoundsToDollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace L._02.PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 British Pound = 1.31 Dollars
+            decimal britishPounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = britishPounds * 1.31M;
+            Console.WriteLine("{0:f3}", dollars);
+        }
+    }
+}
