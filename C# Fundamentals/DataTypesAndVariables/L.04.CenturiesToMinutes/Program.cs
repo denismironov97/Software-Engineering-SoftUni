fd380530f41bd8c9779ea56cc2b21969426e1b06
DenisM97 <denis.m.031997@gmail.com>
2021-08-12 13:26:34 +0300

    Lab.04 uploaded.

--- C# Fundamentals/DataTypesAndVariables/L.04.CenturiesToMinutes/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/L.04.CenturiesToMinutes/Program.cs
@@ -6,7 +6,12 @@ namespace L._04.CenturiesToMinutes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte centuries = byte.Parse(Console.ReadLine());
+            ushort years = (ushort)(centuries * 100);
+            int days = (int)(years * 365.2422); //•	Assume that a year has 365.2422 days at average 
+            long hours = days * 24L;
+            long minutes = hours * 60L;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
         }
     }
 }
