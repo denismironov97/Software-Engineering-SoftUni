7212be5c22ed4bafa783ef7b25cec9244b07218d
DenisM97 <denis.m.031997@gmail.com>
2021-08-13 16:13:20 +0300

    Lab.05 ModV1 uploaded.

--- C# Fundamentals/DataTypesAndVariables/L.05.SpecialNumbersModV1/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/L.05.SpecialNumbersModV1/Program.cs
@@ -6,7 +6,25 @@ namespace L._05.SpecialNumbersModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int inputNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inputNum; i++)
+            {
+                int sumOfDigits = 0;
+                int number = i;
+                while (number > 0)
+                {
+                    sumOfDigits += number % 10;
+                    number /= 10; // number = number/10;
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
         }
     }
 }
