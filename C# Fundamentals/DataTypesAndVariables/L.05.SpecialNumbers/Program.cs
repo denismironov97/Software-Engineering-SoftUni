903f3eea63b3b77d52898106feec51b7af5199a7
DenisM97 <denis.m.031997@gmail.com>
2021-08-13 16:12:40 +0300

    Lab.05 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/L.05.SpecialNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L._05.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inputNum; i++)
+            {
+                int number = i;
+                int digitSum = 0;
+                bool magicNum = false;
+                while (number > 0)
+                {
+                    digitSum += number % 10;
+                    number /= 10; // number = number / 10;
+                }
+                if (digitSum == 5 || digitSum == 7 || digitSum == 11)
+                {
+                    magicNum = true;
+                }
+                Console.WriteLine($"{i} -> {magicNum}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/DataTypesAndVariables/L.06.ReversedChars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._06.ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
