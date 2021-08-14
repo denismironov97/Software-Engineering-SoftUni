bb56b20adfa662101d0a5a7efd8fe073786c66af
DenisM97 <denis.m.031997@gmail.com>
2021-08-14 12:37:58 +0300

    Lab.07 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/L.07.ConcatNamesModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._07.ConcatNamesModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
