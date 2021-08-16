5198be6744153b55cdd0edbcd049820b28f4f473
DenisM97 <denis.m.031997@gmail.com>
2021-08-16 11:00:00 +0300

    Lab.09 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/L.09.CharsToString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L._09.CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            for (int i = 0; i < 3; i++)
+            {
+                char inputSymbol = char.Parse(Console.ReadLine());
+                output += inputSymbol;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
