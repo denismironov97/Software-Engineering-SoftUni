e68b4acfd297ab11271de393a387048bd6e1814d
DenisM97 <denis.m.031997@gmail.com>
2021-08-17 12:38:05 +0300

    Lab.12 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/L.12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L._12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte inputNum = byte.Parse(Console.ReadLine());
+            ushort operationNum = 0;
+            bool magicNum = false;
+            for (ushort number = 1; number <= inputNum; number++)
+            {
+                int digitSum = 0; //
+                operationNum = number;
+                while (operationNum > 0)
+                {
+                    digitSum += operationNum % 10; // 
+                    operationNum = (ushort)(operationNum / 10); //
+                }
+                magicNum = (digitSum == 5) || (digitSum == 7) || (digitSum == 11); //
+                Console.WriteLine("{0} -> {1}", number, magicNum);
+            }
+        }
+    }
+}
