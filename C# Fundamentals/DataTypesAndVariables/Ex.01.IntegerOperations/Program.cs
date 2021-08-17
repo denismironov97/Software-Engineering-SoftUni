cb36cd18ac4282636dd4c4857e860fcece83676e
DenisM97 <denis.m.031997@gmail.com>
2021-08-17 12:40:59 +0300

    Ex.01 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/Ex.01.IntegerOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex._01.IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            long sum = num1 + num2;
+            long division = sum / num3;
+            long multiplication = division * num4;
+            Console.WriteLine(multiplication);
+        }
+    }
+}
