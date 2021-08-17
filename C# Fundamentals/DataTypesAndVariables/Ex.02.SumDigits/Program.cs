f0fbc2815aa4cf9b7d1945a0a89504d9e5ad9ad2
DenisM97 <denis.m.031997@gmail.com>
2021-08-17 12:43:31 +0300

    Ex.02 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/Ex.02.SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex._02.SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine()); // 543 
+            int sumDigits = 0;
+
+            while (inputNum > 0)
+            {
+                sumDigits += inputNum % 10;
+                inputNum /= 10; // number = number/10;
+            }
+            Console.WriteLine(sumDigits);
+        }
+    }
+}
