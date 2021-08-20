c37c24cff98df25c4c547035038f862918fc21b6
DenisM97 <denis.m.031997@gmail.com>
2021-08-20 16:52:41 +0300

    Ex.06 ModV2 uploaded.

--- C# Fundamentals/DataTypesAndVariables/Ex.06.TriplesOfLatinLettersModV2/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/Ex.06.TriplesOfLatinLettersModV2/Program.cs
@@ -6,7 +6,17 @@ namespace Ex._06.TriplesOfLatinLettersModV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 97; i < 97 + n; i++)
+            {
+                for (int j = 97; j < 97 + n; j++)
+                {
+                    for (int k = 97; k < 97 + n; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }
         }
     }
 }
