8ef5899372d696f98aeaac7957d008858c99761e
DenisM97 <denis.m.031997@gmail.com>
2021-08-20 16:47:01 +0300

    Ex.05 uploaded.

--- C# Fundamentals/DataTypesAndVariables/Ex.05.PrintPartOfASCIITable/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/Ex.05.PrintPartOfASCIITable/Program.cs
@@ -6,7 +6,13 @@ namespace Ex._05.PrintPartOfASCIITable
     {
         static void Main(string[] args)
         {
-            
+            int startSymbol = int.Parse(Console.ReadLine());
+            int endSymbol = int.Parse(Console.ReadLine());
+
+            for (int i = startSymbol; i <= endSymbol; i++)
+            {
+                Console.Write("{0} ", (char)i);
+            }
         }
     }
 }
