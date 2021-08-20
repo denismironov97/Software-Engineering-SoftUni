275f4190444d232547ad532e098b9aa2909a3deb
DenisM97 <denis.m.031997@gmail.com>
2021-08-20 16:50:42 +0300

    Ex.06 uploaded.

--- C# Fundamentals/DataTypesAndVariables/Ex.06.TriplesOfLatinLetters/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/Ex.06.TriplesOfLatinLetters/Program.cs
@@ -6,7 +6,21 @@ namespace Ex._06.TriplesOfLatinLetters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nNum = int.Parse(Console.ReadLine()); // 3-> abc
+
+            for (int i = 0; i < nNum; i++)
+            {
+                for (int j = 0; j < nNum; j++)
+                {
+                    for (int k = 0; k < nNum; k++)
+                    {
+                        char firstChar = (char)('a' + i);
+                        char secondChar = (char)('a' + j);
+                        char thirdChar = (char)('a' + k);
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
         }
     }
 }
