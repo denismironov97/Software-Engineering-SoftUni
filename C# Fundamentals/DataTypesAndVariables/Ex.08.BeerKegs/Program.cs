94c54cf4d25f67e7798d101520e6ca885c95eeb5
DenisM97 <denis.m.031997@gmail.com>
2021-08-21 13:22:44 +0300

    Ex.08 uploaded.

--- C# Fundamentals/DataTypesAndVariables/Ex.08.BeerKegs/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/Ex.08.BeerKegs/Program.cs
@@ -6,7 +6,25 @@ namespace Ex._08.BeerKegs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOfKegs = int.Parse(Console.ReadLine());
+            double maxVolumeBiggestKeg = 0;
+            string biggestKeg = string.Empty;
+
+            for (int i = 0; i < numOfKegs; i++)
+            {
+                string modelKeg = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volumeOfCurrentKeg = (Math.PI * (Math.Pow(radius, 2))) * height;
+
+                if (volumeOfCurrentKeg > maxVolumeBiggestKeg)
+                {
+                    maxVolumeBiggestKeg = volumeOfCurrentKeg;
+                    biggestKeg = modelKeg;
+                }
+            }
+
+            Console.WriteLine(biggestKeg);
         }
     }
 }
