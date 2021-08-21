989388642d25780b4bfad330cdeeccfa6c6ff623
DenisM97 <denis.m.031997@gmail.com>
2021-08-21 13:20:32 +0300

    Ex.07 uploaded.

---
+++ C# Fundamentals/DataTypesAndVariables/Ex.07.WaterOverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex._07.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+            int totalLiters = 0;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int includeLiters = int.Parse(Console.ReadLine());
+
+                if (includeLiters + totalLiters > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    totalLiters += includeLiters;
+                }
+            }
+            Console.WriteLine(totalLiters);
+        }
+    }
+}
