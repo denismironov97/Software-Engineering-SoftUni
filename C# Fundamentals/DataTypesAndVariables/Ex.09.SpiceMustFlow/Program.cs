4768fff2e23dd62cddcc4c03a21b2df4a6ce5b02
DenisM97 <denis.m.031997@gmail.com>
2021-08-21 13:26:04 +0300

    Ex.09 uploaded.

--- C# Fundamentals/DataTypesAndVariables/Ex.09.SpiceMustFlow/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/Ex.09.SpiceMustFlow/Program.cs
@@ -6,7 +6,28 @@ namespace Ex._09.SpiceMustFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int startingYield = int.Parse(Console.ReadLine()); // 111
+            int days = 0;
+            int totalSpiceExtracted = 0;
+
+            while (startingYield >= 100)
+            {
+                int extractedSpice = startingYield; // 111 day 1; 101 day 2
+                extractedSpice -= 26; // 85; 75;
+                totalSpiceExtracted += extractedSpice; // 85; 160;
+                days++;
+                startingYield -= 10;
+            }
+
+            totalSpiceExtracted -= 26;
+
+            if (totalSpiceExtracted < 0)
+            {
+                totalSpiceExtracted = 0;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(totalSpiceExtracted);
         }
     }
 }
