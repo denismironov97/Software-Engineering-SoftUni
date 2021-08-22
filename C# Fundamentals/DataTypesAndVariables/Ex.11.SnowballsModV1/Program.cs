68196937b4991ef4a314f3f725de07a3aa86652e
DenisM97 <denis.m.031997@gmail.com>
2021-08-22 12:22:57 +0300

    Ex.11 uploaded.

--- C# Fundamentals/DataTypesAndVariables/Ex.11.SnowballsModV1/Program.cs
+++ C# Fundamentals/DataTypesAndVariables/Ex.11.SnowballsModV1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using System.Numerics;
 namespace Ex._11.SnowballsModV1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numNSnowBalls = int.Parse(Console.ReadLine()); //range [0, 100]
+            BigInteger maxSnowballValue = 0;
+
+            string result = string.Empty;
+
+            for (int i = 0; i < numNSnowBalls; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine()); //[0, 1000]
+                int snowballTime = int.Parse(Console.ReadLine()); //[1, 500]
+                int snowballQuality = int.Parse(Console.ReadLine()); //[0, 100]
+
+                BigInteger currentSnowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality); // !
+
+                if (currentSnowballValue > maxSnowballValue)
+                {
+                    maxSnowballValue = currentSnowballValue;
+                    result = $"{snowballSnow} : {snowballTime} = {maxSnowballValue} ({snowballQuality})";
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
