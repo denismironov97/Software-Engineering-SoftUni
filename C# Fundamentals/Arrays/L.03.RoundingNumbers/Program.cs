52e5f8d15632bacc8949991c67c4275689bad588
DenisM97 <denis.m.031997@gmail.com>
2021-08-24 22:42:11 +0300

    Lab.03 uploaded.

--- C# Fundamentals/Arrays/L.03.RoundingNumbers/Program.cs
+++ C# Fundamentals/Arrays/L.03.RoundingNumbers/Program.cs
@@ -6,7 +6,23 @@ namespace L._03.RoundingNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string values = Console.ReadLine();
+            string[] valuesString = values.Split();
+            int lenght = valuesString.Length;
+            double[] numArray = new double[lenght]; //
+            for (int i = 0; i < lenght; i++)
+            {
+                numArray[i] = double.Parse(valuesString[i]);
+            }
+
+            foreach (double number in numArray)
+            {
+                int roundedNum = (int)Math.Round(number, MidpointRounding.AwayFromZero);
+                Console.WriteLine("{0} => {1}", (decimal)number, (decimal)roundedNum);
+            }
+
+            //double number = 2.5; // normal rounding in general Math;
+            //Console.WriteLine(Math.Round(number));
         }
     }
 }
