83ace32f050826176d536d6a1f772e0d649438db
DenisM97 <denis.m.031997@gmail.com>
2021-08-24 22:39:46 +0300

    Lab.01 uploaded.

---
+++ C# Fundamentals/Arrays/L.01.DayOfWeek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace L._01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfTheWeek = int.Parse(Console.ReadLine());
+            //string[] week = new string[7] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" , "Sunday" };
+            //string[] week = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (dayOfTheWeek > 0 && dayOfTheWeek <= 7) // 
+            {
+                Console.WriteLine(week[dayOfTheWeek - 1]); // arrays are zero based index
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/L.03.RoundingNumbersModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._03.RoundingNumbersModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
