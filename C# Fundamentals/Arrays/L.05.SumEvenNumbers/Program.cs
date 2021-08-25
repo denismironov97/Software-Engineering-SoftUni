429d151fba60d5cb488aee40755b381f84cf6d86
DenisM <denis.m.031997@gmail.com>
2021-08-25 21:52:26 +0300

    Lab.05 uploaded.

--- C# Fundamentals/Arrays/L.05.SumEvenNumbers/Program.cs
+++ C# Fundamentals/Arrays/L.05.SumEvenNumbers/Program.cs
@@ -6,7 +6,18 @@ namespace L._05.SumEvenNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] arrString = Console.ReadLine().Split();
+            int[] arrayNum = new int[arrString.Length];
+            int sum = 0;
+            for (int i = 0; i < arrString.Length; i++)
+            {
+                arrayNum[i] = int.Parse(arrString[i]);
+                if (arrayNum[i] % 2 == 0)
+                {
+                    sum = sum + arrayNum[i];
+                }
+            }
+            Console.WriteLine(sum);
         }
     }
 }
