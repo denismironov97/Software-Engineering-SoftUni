f2cbd9c2d61be2cfcbafa203db3d538d7fec8615
DenisM <denis.m.031997@gmail.com>
2021-08-25 21:51:50 +0300

    Lab.04 ModV1 uploaded.

--- C# Fundamentals/Arrays/L.04.ReverseArrayOfStringsV1/Program.cs
+++ C# Fundamentals/Arrays/L.04.ReverseArrayOfStringsV1/Program.cs
@@ -6,7 +6,12 @@ namespace L._04.ReverseArrayOfStringsV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] arrayString = Console.ReadLine().Split();
+            int arrLenght = arrayString.Length;
+            for (int i = arrLenght - 1; i >= 0; i--)
+            {
+                Console.Write($"{ arrayString[i]} ");
+            }
         }
     }
 }
