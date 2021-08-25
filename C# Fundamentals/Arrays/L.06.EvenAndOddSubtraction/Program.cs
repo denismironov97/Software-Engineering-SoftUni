06a01c7dbf47a5bd443764a4503c0f6ace4a1bdd
DenisM <denis.m.031997@gmail.com>
2021-08-25 21:53:17 +0300

    Lab.06 uploaded.

--- C# Fundamentals/Arrays/L.06.EvenAndOddSubtraction/Program.cs
+++ C# Fundamentals/Arrays/L.06.EvenAndOddSubtraction/Program.cs
@@ -6,7 +6,25 @@ namespace L._06.EvenAndOddSubtraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] arrString = Console.ReadLine().Split();
+            int[] arrInt = new int[arrString.Length];
+            int sumEven = 0;
+            int sumOdd = 0;
+            for (int i = 0; i < arrString.Length; i++)
+            {
+                arrInt[i] = int.Parse(arrString[i]);
+                if (arrInt[i] % 2 == 0)
+                {
+                    sumEven += arrInt[i];
+                }
+                else
+                {
+                    sumOdd += arrInt[i];
+                }
+            }
+            int difference = 0;
+            difference = sumEven - sumOdd;
+            Console.WriteLine(difference);
         }
     }
 }
