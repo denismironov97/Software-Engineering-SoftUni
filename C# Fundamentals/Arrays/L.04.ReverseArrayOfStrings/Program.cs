d1395fa3983da4918036b6bff743701616fbb480
DenisM <denis.m.031997@gmail.com>
2021-08-25 21:50:40 +0300

    Lab.04 uploaded.

---
+++ C# Fundamentals/Arrays/L.04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L._04.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string values = Console.ReadLine();
+            string[] arrString = values.Split();//
+            int arrStringLenght = arrString.Length;
+            for (int i = 0; i < arrStringLenght / 2; i++)
+            {
+                string oldElement = arrString[i];
+                arrString[i] = arrString[arrString.Length - 1 - i];
+                arrString[arrString.Length - 1 - i] = oldElement;
+            }
+            Console.WriteLine(string.Join(" ", arrString));
+        }
+    }
+}
