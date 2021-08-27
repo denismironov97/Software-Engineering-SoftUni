2d8a451fd7c0906d773106fde5c3831bd19d4d23
DenisM <denis.m.031997@gmail.com>
2021-08-27 14:08:50 +0300

    Lab.07 uploaded.

---
+++ C# Fundamentals/Arrays/L.07.EqualArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace L._07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrString1 = Console.ReadLine().Split();
+            int[] array1 = new int[arrString1.Length];
+            for (int i = 0; i < arrString1.Length; i++)
+            {
+                array1[i] = int.Parse(arrString1[i]);
+            }
+
+            int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    sum += array1[i];
+                }
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/L.08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._08.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
