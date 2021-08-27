49dbfdaaea127bfc954471a450d9d3b6942e0e4c
DenisM <denis.m.031997@gmail.com>
2021-08-27 14:13:45 +0300

    Lab.08 uploaded. Lab.07 algorithm changes applied.

--- C# Fundamentals/Arrays/L.07.EqualArrays/Program.cs
+++ C# Fundamentals/Arrays/L.07.EqualArrays/Program.cs
@@ -8,7 +8,7 @@ namespace L._07.EqualArrays
         {
             string[] arrString1 = Console.ReadLine().Split();
             int[] array1 = new int[arrString1.Length];
-            for (int i = 0; i < arrString1.Length; i++)
+            for (int i = 0; i < array1.Length; i++)
             {
                 array1[i] = int.Parse(arrString1[i]);
             }
--- C# Fundamentals/Arrays/L.08.CondenseArrayToNumber/Program.cs
+++ C# Fundamentals/Arrays/L.08.CondenseArrayToNumber/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using System.Linq;
 namespace L._08.CondenseArrayToNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] mainArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensedArray = new int[mainArray.Length - 1];
+
+            if (mainArray.Length == 1)
+            {
+                Console.WriteLine(mainArray[0]);
+                return;
+            }
+
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                for (int j = 0; j < condensedArray.Length - i; j++)
+                {
+                    condensedArray[j] = mainArray[j] + mainArray[j + 1];
+                }
+                mainArray = condensedArray;
+            }
+            Console.WriteLine(condensedArray[0]);
         }
     }
 }
