40b4b6511797b04f870e3bc81cb367110e845c90
DenisM <denis.m.031997@gmail.com>
2021-08-28 17:31:38 +0300

    Ex.03 uploaded.

---
+++ C# Fundamentals/Arrays/Ex.03.ZigZagArrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ex._03.ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+            int[] array1 = new int[nLines];
+            int[] array2 = new int[nLines];
+
+            for (int i = 0; i < nLines; i++)
+            {
+                string inputValString = Console.ReadLine();
+                string[] arrInputValString = inputValString.Split(' ');
+                int[] arrInputValNums = new int[arrInputValString.Length];
+                for (int j = 0; j < arrInputValNums.Length; j++)
+                {
+                    arrInputValNums[j] = int.Parse(arrInputValString[j]);
+                }
+
+                if (i % 2 == 0)//even row
+                {
+                    array1[i] = arrInputValNums[0];
+                    array2[i] = arrInputValNums[1];
+                }
+                else//odd row
+                {
+                    array1[i] = arrInputValNums[1];
+                    array2[i] = arrInputValNums[0];
+                }
+            }
+
+            foreach (int arr1Num in array1)
+            {
+                Console.Write($"{arr1Num} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int arrNum2 in array2)
+            {
+                Console.Write($"{arrNum2} ");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/Ex.03.ZigZagArraysModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._03.ZigZagArraysModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
