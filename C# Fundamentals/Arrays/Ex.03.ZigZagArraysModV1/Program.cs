b6bce50888990f914af906066fb9f36f40affd49
DenisM <denis.m.031997@gmail.com>
2021-08-28 17:33:10 +0300

    Ex.03 ModV1 uploaded.

--- C# Fundamentals/Arrays/Ex.03.ZigZagArraysModV1/Program.cs
+++ C# Fundamentals/Arrays/Ex.03.ZigZagArraysModV1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using System.Linq;
 namespace Ex._03.ZigZagArraysModV1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLines = int.Parse(Console.ReadLine());
+            int[] array1 = new int[nLines];
+            int[] array2 = new int[nLines];
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int[] currentLineOfSetNums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (i % 2 == 0) // even row
+                {
+                    array1[i] = currentLineOfSetNums[0];
+                    array2[i] = currentLineOfSetNums[1];
+                }
+                else // odd row
+                {
+                    array1[i] = currentLineOfSetNums[1];
+                    array2[i] = currentLineOfSetNums[0];
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', array1));
+            Console.WriteLine(string.Join(' ', array2));
         }
     }
 }
