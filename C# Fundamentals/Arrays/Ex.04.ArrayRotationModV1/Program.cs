d747fc60746422918004e67eb98c9611eaed0928
DenisM <denis.m.031997@gmail.com>
2021-08-29 14:44:45 +0300

    Ex.04 ModV1 uploaded.

---
+++ C# Fundamentals/Arrays/Ex.04.ArrayRotationModV1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex._04.ArrayRotationModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputVals = Console.ReadLine().Split();
+            int[] arrNums = new int[inputVals.Length];
+            for (int i = 0; i < arrNums.Length; i++)
+            {
+                arrNums[i] = int.Parse(inputVals[i]);
+            }
+            //     0   1  2  3 tempArr[j - 1]
+            // 0   1   2  3  4 arrNums[j]       [tempArr]
+            //[51] 47 32 61 21;   {47 32 61 21 [51]};   32 61 21 51 47
+            int rotations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rotations; i++)
+            {
+                int firstElemnt = arrNums[0];
+                int[] tempArr = new int[arrNums.Length];
+                for (int j = 1; j < arrNums.Length; j++)
+                {
+                    tempArr[j - 1] = arrNums[j];
+
+                }
+                tempArr[arrNums.Length - 1] = firstElemnt;
+                arrNums = tempArr;
+            }
+
+            foreach (int value in arrNums)
+            {
+                Console.Write("{0} ", value);
+            }
+        }
+    }
+}
