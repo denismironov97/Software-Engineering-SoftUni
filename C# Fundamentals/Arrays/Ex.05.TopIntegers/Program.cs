d560ee0ae47a020161747e8b265fec303137122c
DenisM <denis.m.031997@gmail.com>
2021-08-29 14:51:31 +0300

    Ex.05 uploaded.

---
+++ C# Fundamentals/Arrays/Ex.05.TopIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex._05.TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValues = Console.ReadLine();
+            string[] arrString = inputValues.Split(' ');
+            int[] arrValues = new int[arrString.Length];//
+            for (int i = 0; i < arrValues.Length; i++)
+            {
+                arrValues[i] = int.Parse(arrString[i]);
+            }
+            // 1 4 3 2
+            for (int i = 0; i < arrValues.Length; i++)
+            {
+                bool isBigger = true;
+                for (int j = i + 1; j < arrValues.Length; j++)
+                {
+                    if (arrValues[i] <= arrValues[j])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+
+                if (isBigger)
+                {
+                    Console.Write(arrValues[i] + " ");
+                }
+            }
+        }
+    }
+}
