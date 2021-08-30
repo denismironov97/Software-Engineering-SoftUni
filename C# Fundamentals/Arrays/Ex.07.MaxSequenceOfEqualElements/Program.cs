b2cec25b1baf30f7a12f55630f18a85274476317
DenisM <denis.m.031997@gmail.com>
2021-08-30 14:17:29 +0300

    Ex.07 uploaded.

--- C# Fundamentals/Arrays/Ex.07.MaxSequenceOfEqualElements/Program.cs
+++ C# Fundamentals/Arrays/Ex.07.MaxSequenceOfEqualElements/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-
+using System.Linq;
 namespace Ex._07.MaxSequenceOfEqualElements
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] sequenceInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            // [0] [1] [2] [3] [4] [5] [6] [7] [8] [9]
+            //  2   1   1   2   3   3   2   2   2   1
+
+            int identicalNumsLength = 1;
+            int maxSequenceOfIdenticalNums = 1;
+            int startIndex = 0;
+            int currSequenceStartIndex = 0;
+
+            for (int i = 1; i < sequenceInput.Length; i++)
+            {
+                int previusNum = sequenceInput[i - 1];
+                int currNum = sequenceInput[i];
+                if (previusNum == currNum)
+                {
+                    identicalNumsLength++;
+                }
+                else
+                {
+                    identicalNumsLength = 1;
+                    currSequenceStartIndex = i;
+                }
+
+                if (identicalNumsLength > maxSequenceOfIdenticalNums)
+                {
+                    maxSequenceOfIdenticalNums = identicalNumsLength;
+                    startIndex = currSequenceStartIndex;
+                }
+            }
+
+            for (int i = startIndex; i < startIndex + maxSequenceOfIdenticalNums; i++)
+            {
+                Console.Write(sequenceInput[i] + " ");
+            }
         }
     }
 }
