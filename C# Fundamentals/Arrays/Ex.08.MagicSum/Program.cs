e39cf2f699bd87dcc07c76b2347c755fb18c568b
DenisM <denis.m.031997@gmail.com>
2021-08-30 12:19:18 +0300

    Ex.08 uploaded.

--- C# Fundamentals/Arrays/Ex.08.MagicSum/Program.cs
+++ C# Fundamentals/Arrays/Ex.08.MagicSum/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using System.Linq;
 namespace Ex._08.MagicSum
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // [0] [1] [2] [3] [4] [5]
+            //  1  7   6   2   19  23
+            // 8
+
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicNum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == magicNum)
+                    {
+                        Console.WriteLine($"{array[i]} {array[j]}");
+                    }
+
+                }
+            }
         }
     }
 }
