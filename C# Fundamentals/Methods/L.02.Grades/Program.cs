4e421b1ccf0b2b941eb7b63578974a7bf34af07a
DenisM <denis.m.031997@gmail.com>
2021-08-31 17:48:51 +0300

    Lab.02 uploaded.

--- C# Fundamentals/Methods/L.02.Grades/Program.cs
+++ C# Fundamentals/Methods/L.02.Grades/Program.cs
@@ -6,7 +6,31 @@ namespace L._02.Grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double number = double.Parse(Console.ReadLine());
+            GetGRades(number);
+        }
+        static void GetGRades(double number)
+        {
+            if (number >= 2 && number <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (number >= 3 && number <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (number >= 3.5 & number <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (number >= 4.5 && number <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (number >= 5.5 && number <= 6)
+            {
+                Console.WriteLine("Excellent");
+            }
         }
     }
 }
