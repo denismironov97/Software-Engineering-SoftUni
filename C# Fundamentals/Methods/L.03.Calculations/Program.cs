4601d198d6bfea93fc0bd09f85e5b3d40233a211
DenisM <denis.m.031997@gmail.com>
2021-08-31 17:51:04 +0300

    Lab.03 uploaded.

--- C# Fundamentals/Methods/L.03.Calculations/Program.cs
+++ C# Fundamentals/Methods/L.03.Calculations/Program.cs
@@ -6,8 +6,48 @@ namespace L._03.Calculations
     {
         static void Main(string[] args)
         {
-            
+            string action = Console.ReadLine();
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            if (action == "add")
+            {
+                Аddition(num1, num2);
+            }
+            else if (action == "subtract")
+            {
+                //Subtraction(number1: double.Parse(Console.ReadLine()), number2: double.Parse(Console.ReadLine()));
+                Subtraction(number1: num1, number2: num2);
+            }
+            else if (action == "multiply")
+            {
+                //Multiplication(number2: double.Parse(Console.ReadLine()), number1: double.Parse(Console.ReadLine()));
+                Multiplication(num1, num2);
+            }
+            else //divide
+            {
+                Division(num1, num2);
+            }
         }
-        
+
+        static void Аddition(double number1, double number2)
+        {
+            double endResult = number1 + number2;
+            Console.WriteLine(endResult);
+        }
+        static void Subtraction(double number1, double number2)
+        {
+            double endResult = number1 - number2;
+            Console.WriteLine(endResult);
+        }
+        static void Multiplication(double number1, double number2)
+        {
+            double endResult = number1 * number2;
+            Console.WriteLine(endResult);
+        }
+        static void Division(double number1, double number2)
+        {
+            double endResult = number1 / number2;
+            Console.WriteLine(endResult);
+        }
     }
 }
