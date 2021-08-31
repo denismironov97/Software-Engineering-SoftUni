5ea1cce192b87adfae117770b0aae74f00c0f79e
DenisM <denis.m.031997@gmail.com>
2021-08-31 17:49:43 +0300

    Lab.02 ModV1 uploaded.

--- C# Fundamentals/Methods/L.02.GradesModV1/Program.cs
+++ C# Fundamentals/Methods/L.02.GradesModV1/Program.cs
@@ -6,7 +6,32 @@ namespace L._02.GradesModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double num = double.Parse(Console.ReadLine());
+            string result = GetGradeResult(num);
+            Console.WriteLine(result);
+        }
+        static string GetGradeResult(double number)
+        {
+            if (number >= 2 && number <= 2.99)
+            {
+                return "Fail";
+            }
+            else if (number >= 3 && number <= 3.49)
+            {
+                return "Poor";
+            }
+            else if (number >= 3.5 & number <= 4.49)
+            {
+                return "Good";
+            }
+            else if (number >= 4.5 && number <= 5.49)
+            {
+                return "Very good";
+            }
+            else
+            {
+                return "Excellent";
+            }
         }
     }
 }
