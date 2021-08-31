adb517a2c607f0dbbb9b810ca799d3482dbd22fd
DenisM <denis.m.031997@gmail.com>
2021-08-31 17:08:10 +0300

    Lab.01 uploaded and issues in projs mixing fixed.

---
+++ C# Fundamentals/Methods/L.03.Calculations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L._03.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+        static void PrintSign(int number)
+        {
+            if (number > 0)
+                Console.WriteLine("The number {0} is positive.", number);
+            else if (number < 0)
+                Console.WriteLine("The number {0} is negative.", number);
+            else
+                Console.WriteLine("The number {0} is zero.", number);
+        }
+    }
+}
