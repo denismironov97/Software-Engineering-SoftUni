b46d8a1a8c762e36c1820a33643cbbaec90bb6a7
DenisM <denis.m.031997@gmail.com>
2021-09-01 16:00:22 +0300

    Lab.04 Different Mods uploaded.

---
+++ C# Fundamentals/Methods/L.04.PrintingTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace L._04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());//3
+            for (int row = 1; row <= n; row++)//rows
+            {
+                PrintLinesTopPyramid(row);
+            }
+            for (int row = n; row > 1; row--)//bottom rows
+            {
+                PritLinesBottomPyramid(row);
+            }
+        }
+        static void PrintLinesTopPyramid(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write($"{col} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PritLinesBottomPyramid(int row)
+        {
+            for (int col = 1; col < row; col++)
+            {
+                Console.Write($"{col} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Fundamentals/Methods/L.04.PrintingTriangleModV1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace L._04.PrintingTriangleModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintFigure(n);
+        }
+        static void PrintLine(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write($"{col} ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintFigure(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                PrintLine(row);
+            }
+
+            for (int row = n - 1; row >= 1; row--)
+            {
+                PrintLine(row);
+            }
+        }
+    }
+}
