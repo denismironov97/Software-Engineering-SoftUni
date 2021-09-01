2e69a4cfbb1d99c3b535c06ab33cfec60df2a4aa
DenisM <denis.m.031997@gmail.com>
2021-09-01 16:05:20 +0300

    Lab.05 uploaded.

---
+++ C# Fundamentals/Methods/Lab.05.Orders/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab._05.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string beverage = Console.ReadLine(); //water, coffee, coke, snacks 
+            int quantity = int.Parse(Console.ReadLine());
+            PrintPriceOrder(beverage, quantity);
+        }
+        static void PrintPriceOrder(string beverage, int quantity)
+        {
+            double priceOfProduct = 0;
+            if (beverage == "coffee")
+            {
+                priceOfProduct = 1.50;
+            }
+            else if (beverage == "water")
+            {
+                priceOfProduct = 1;
+            }
+            else if (beverage == "coke")
+            {
+                priceOfProduct = 1.40;
+            }
+            else//snacks
+            {
+                priceOfProduct = 2;
+            }
+            double totalPrice = priceOfProduct * quantity;
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+    }
+}
---
+++ C# Fundamentals/Methods/Lab.06.CalculateRectangleAreaModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._06.CalculateRectangleAreaModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
