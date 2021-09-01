c22e291c8138552443954618b8dc3077d7e1ff37
DenisM <denis.m.031997@gmail.com>
2021-09-01 16:21:30 +0300

    Lab.07 ModV2 uploaded.

---
+++ C# Fundamentals/Methods/L.07.RepeatStringModV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L._07.RepeatStringModV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string[] result = PrintLine(n, input);
+            foreach (string text in result)
+            {
+                Console.Write(text);
+            }
+        }
+        static string[] PrintLine(int nCopyedVal, string inputCopyedVal)
+        {
+            string[] arrCollection = new string[nCopyedVal];
+            for (int i = 0; i < arrCollection.Length; i++)
+            {
+                arrCollection[i] = inputCopyedVal;
+            }
+            return arrCollection;
+        }
+    }
+}
