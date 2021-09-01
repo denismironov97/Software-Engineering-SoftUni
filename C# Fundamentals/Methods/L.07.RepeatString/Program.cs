504239e42347fe4e79c37470d8b487e9bd4c47b5
DenisM <denis.m.031997@gmail.com>
2021-09-01 16:11:15 +0300

    Lab.07 uploaded.

---
+++ C# Fundamentals/Methods/L.07.RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L._07.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = PrintLine(n, input);
+            Console.WriteLine(result);
+        }
+        static string PrintLine(int n, string input)
+        {
+            string collection = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                collection += input;
+            }
+            return collection;
+        }
+    }
+}
---
+++ C# Fundamentals/Methods/L.07.RepeatStringModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._07.RepeatStringModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
