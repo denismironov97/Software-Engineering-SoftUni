3c31ead11a3226fe783e2918fae29e62b8937ddb
DenisM <denis.m.031997@gmail.com>
2021-09-02 14:17:42 +0300

    Lab.08 uploaded.

---
+++ C# Fundamentals/Methods/L.08.MathPower/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L._08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputNum = double.Parse(Console.ReadLine());
+            int inputPower = int.Parse(Console.ReadLine());
+            double output = RaiseToPower(inputNum, inputPower);
+            Console.WriteLine(output);
+        }
+        static double RaiseToPower(double number, int power)
+        {
+            double endResult = 1;
+            for (int i = 0; i < power; i++)
+            {
+                endResult *= number;
+            }
+            return endResult;
+        }
+    }
+}
---
+++ C# Fundamentals/Methods/L.08.MathPowerModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._08.MathPowerModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
