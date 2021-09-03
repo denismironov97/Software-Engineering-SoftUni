5783fc53afc43e40c58ecde576634e616da5d6d7
DenisM <denis.m.031997@gmail.com>
2021-09-03 16:49:07 +0300

    Lab.11 uploaded.

---
+++ C# Fundamentals/Methods/L.11.MathOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace L._11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double resultFromMethod = Calculate(num1, @operator, num2);
+            Console.WriteLine(resultFromMethod);
+        }
+        static double Calculate(double num1, char @operator, double num2)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            return result;
+        }
+    }
+}
