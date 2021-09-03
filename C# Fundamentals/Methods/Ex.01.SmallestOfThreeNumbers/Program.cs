a7393ca27c1b84faa39f00cd2387d647af0cd671
DenisM <denis.m.031997@gmail.com>
2021-09-03 16:51:39 +0300

    Ex.01 uploaded.

---
+++ C# Fundamentals/Methods/Ex.01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex._01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            GetMinNum(num1, num2, num3);
+        }
+
+        static void GetMinNum(int num1, int num2, int num3)
+        {
+            int minNum = 0;
+            if (num1 < num2)
+            {
+                minNum = num1;
+            }
+            else // num1 >= num2
+            {
+                minNum = num2;
+            }
+            if (minNum > num3)
+            {
+                minNum = num3;
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
