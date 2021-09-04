7a010cded5bbd58cba7757d10396903ca5895996
DenisM <denis.m.031997@gmail.com>
2021-09-04 11:32:35 +0300

    Ex.02 ModV1(Improved solution to problem) uploaded.

---
+++ C# Fundamentals/Methods/Ex.02.VowelsCountModV1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex._02.VowelsCountModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int vowelsSum = GetVowelsCount(input);
+            Console.WriteLine(vowelsSum);
+        }
+
+        private static int GetVowelsCount(string inputMsg)
+        {
+            int sumVowels = 0;
+            foreach (char symbol in inputMsg)
+            {
+                switch (symbol)
+                {
+                    case 'a':
+                    case 'A':
+                    case 'e':
+                    case 'E':
+                    case 'i':
+                    case 'I':
+                    case 'o':
+                    case 'O':
+                    case 'u':
+                    case 'U':
+                    case 'y':
+                    case 'Y':
+                        sumVowels++;
+                        break;
+                }
+            }
+
+            return sumVowels;
+        }
+    }
+}
