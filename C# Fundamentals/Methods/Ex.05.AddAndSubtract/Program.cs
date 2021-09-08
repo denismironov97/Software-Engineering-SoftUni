99dc239212d9ed155f3e37902a859d233e8fe517
DenisM97 <denis.m.031997@gmail.com>
2021-09-08 13:58:36 +0300

    Ex.05 uploaded.

---
+++ C# Fundamentals/Methods/Ex.05.AddAndSubtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex._05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int sumResult = Sum(num1, num2);
+            int subtractResult = Subtract(sumReturned: sumResult, num3Copied: num3);
+            Console.WriteLine(subtractResult);
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Subtract(int sumReturned, int num3Copied)
+        {
+            return sumReturned - num3Copied;
+        }
+    }
+}
