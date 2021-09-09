4d42f3c14cbed78951b3d35c3e65b83de0ad1905
DenisM <denis.m.031997@gmail.com>
2021-09-09 20:48:59 +0300

    Ex.07 uploaded.

---
+++ C# Fundamentals/Methods/Ex.07.NxNMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex._07.NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+
+        static void PrintMatrix(int n)
+        {
+            for (int row = 1; row <= n; row++) // rows
+            {
+                for (int col = 1; col <= n; col++) // collons
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Methods/Ex.08.FactorialDivisionModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._08.FactorialDivisionModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
