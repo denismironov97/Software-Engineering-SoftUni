0e9a14531fe64d57f9d4d627ebc4aa6313532b8e
DenisM <denis.m.031997@gmail.com>
2021-09-09 21:00:46 +0300

    Ex.08 ModV1 uploaded.

--- C# Fundamentals/Methods/Ex.08.FactorialDivisionModV1/Program.cs
+++ C# Fundamentals/Methods/Ex.08.FactorialDivisionModV1/Program.cs
@@ -6,7 +6,31 @@ namespace Ex._08.FactorialDivisionModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            decimal result = FactorielDivision(nums[0], nums[1]);
+            Console.WriteLine("{0:f2}", result);
+        }
+
+        private static decimal FactorielDivision(int num1, int num2)
+        {
+            long factoriel1 = 1;
+            long factoriel2 = 1;
+            for (int i = 1; i <= num1; i++)
+            {
+                factoriel1 *= i;
+            }
+            for (int i = 1; i <= num2; i++)
+            {
+                factoriel2 *= i;
+            }
+
+            decimal result = factoriel1 * 1.0M / factoriel2;
+            return result;
         }
     }
 }
