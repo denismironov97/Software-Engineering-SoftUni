ab5bd77a23d80af90417c56b3a844b6307710de2
DenisM <denis.m.031997@gmail.com>
2021-09-09 20:50:38 +0300

    Ex.08 uploaded.

--- C# Fundamentals/Methods/Ex.08.FactorialDivision/Program.cs
+++ C# Fundamentals/Methods/Ex.08.FactorialDivision/Program.cs
@@ -6,7 +6,26 @@ namespace Ex._08.FactorialDivision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            decimal result = FactorielDivision(num1, num2);
+            Console.WriteLine("{0:f2}", result);
+        }
+
+        static decimal FactorielDivision(int num1, int num2)
+        {
+            long factoriel1 = 1;
+            long factoriel2 = 1;
+            for (int i = 1; i <= num1; i++)
+            {
+                factoriel1 = factoriel1 * i;
+            }
+            for (int i = 1; i <= num2; i++)
+            {
+                factoriel2 *= i;
+            }
+            decimal result = factoriel1 * 1.0M / factoriel2;
+            return result;
         }
     }
 }
