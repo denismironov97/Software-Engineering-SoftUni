d4f55bb0b925bf43a5fe63cdc48714237430b18a
DenisM97 <denis.m.031997@gmail.com>
2021-09-10 16:38:59 +0300

    Ex.09 uploaded.

---
+++ C# Fundamentals/Methods/Ex.09.PalindromeIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace Ex._09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                Console.WriteLine(ChecksIfIsPalidrome(input).ToString().ToLower());
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool ChecksIfIsPalidrome(string text)
+        {
+            char[] reversedText = text.Reverse().ToArray();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!(text[i] == reversedText[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ C# Fundamentals/Methods/Ex.09.PalindromeIntegersModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._09.PalindromeIntegersModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
