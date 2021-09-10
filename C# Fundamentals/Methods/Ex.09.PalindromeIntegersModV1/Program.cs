fd2bae94b13c15a2363fc33e8799006bd0ab26dc
DenisM97 <denis.m.031997@gmail.com>
2021-09-10 16:41:44 +0300

    Ex.09 ModV1 uploaded.

--- C# Fundamentals/Methods/Ex.09.PalindromeIntegersModV1/Program.cs
+++ C# Fundamentals/Methods/Ex.09.PalindromeIntegersModV1/Program.cs
@@ -6,7 +6,46 @@ namespace Ex._09.PalindromeIntegersModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                bool isIntegerPalindrome = CheckForPalidromeNumber(input);
+                if (isIntegerPalindrome)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        static bool CheckForPalidromeNumber(string input)
+        {
+            int number = int.Parse(input);
+            bool result = false;
+            if (number >= 0 && number <= 9)
+            {
+                result = true;
+            }
+            else
+            {
+                for (int i = 0; i < input.Length / 2; i++)
+                {
+                    if (input[i] == input[input.Length - 1])
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
