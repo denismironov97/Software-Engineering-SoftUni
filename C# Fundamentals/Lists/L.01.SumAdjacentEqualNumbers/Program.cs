2054318efe87ef35519313e11f2b5f9be6e835e7
DenisM97 <denis.m.031997@gmail.com>
2021-09-13 15:05:34 +0300

    Lab.01 uploaded.

--- C# Fundamentals/Lists/L.01.SumAdjacentEqualNumbers/Program.cs
+++ C# Fundamentals/Lists/L.01.SumAdjacentEqualNumbers/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace L._01.SumAdjacentEqualNumbers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            GetResult(numbers);
+        }
+        static void GetResult(List<double> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (i + 1 > numbers.Count - 1)
+                {
+                    break;
+                }
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] += numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
