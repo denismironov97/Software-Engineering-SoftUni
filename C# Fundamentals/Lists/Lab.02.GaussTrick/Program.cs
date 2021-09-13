0712fbb9033797fed1b0c58c960c5e897447940b
DenisM97 <denis.m.031997@gmail.com>
2021-09-13 15:24:36 +0300

    Lab.02 uploaded.

---
+++ C# Fundamentals/Lists/Lab.02.GaussTrick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab._02.GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int lengh = numbersList.Count / 2;
+            for (int i = 0; i < lengh; i++)
+            {
+                numbersList[i] += numbersList[numbersList.Count - 1];
+                numbersList.RemoveAt(numbersList.Count - 1);
+            }
+            Console.WriteLine(string.Join(" ", numbersList));
+        }
+    }
+}
