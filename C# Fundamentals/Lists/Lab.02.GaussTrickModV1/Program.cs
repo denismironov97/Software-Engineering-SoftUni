1141e61f6b39ce524c2d4e745288a4f7dce47137
DenisM97 <denis.m.031997@gmail.com>
2021-09-14 17:34:12 +0300

    Lab.02 ModV1 uploaded.

--- C# Fundamentals/Lists/Lab.02.GaussTrickModV1/Program.cs
+++ C# Fundamentals/Lists/Lab.02.GaussTrickModV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._02.GaussTrickModV1
 {
@@ -6,7 +8,26 @@ namespace Lab._02.GaussTrickModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbersList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = GetGausResult(numbersList);
+            foreach (int num in result)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        static List<int> GetGausResult(List<int> numbersList)
+        {
+            int lengh = numbersList.Count / 2;
+            for (int i = 0; i < lengh; i++)
+            {
+                numbersList[i] += numbersList[numbersList.Count - 1];
+                numbersList.RemoveAt(numbersList.Count - 1);
+            }
+            return numbersList;
         }
     }
 }
