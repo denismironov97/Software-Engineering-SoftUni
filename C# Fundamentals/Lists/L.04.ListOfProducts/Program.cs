ab486a8b0e0f65a7d951add896ea274320dec661
DenisM97 <denis.m.031997@gmail.com>
2021-09-14 17:53:30 +0300

    L.04 uploaded.

---
+++ C# Fundamentals/Lists/L.04.ListOfProducts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace L._04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+
+            products.Sort(); // sorts the list (currently in alphabetical order);
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine("{0}.{1}", i + 1, products[i]);
+            }
+        }
+    }
+}
