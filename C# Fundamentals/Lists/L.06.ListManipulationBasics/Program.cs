1b477e3f957e5e548ea048ae5e7234f453fe2cde
DenisM97 <denis.m.031997@gmail.com>
2021-09-15 14:32:05 +0300

    Lab.06 uploaded.

--- C# Fundamentals/Lists/L.06.ListManipulationBasics/Program.cs
+++ C# Fundamentals/Lists/L.06.ListManipulationBasics/Program.cs
@@ -8,7 +8,42 @@ namespace L._06.ListManipulationBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+            List<int> resultList = GetListManipulation(numbers, command);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static List<int> GetListManipulation(List<int> numbers, string command)
+        {
+            while (command != "end")
+            {
+                string[] currentCommand = command.Split();
+                if (currentCommand[0] == "Add")
+                {
+                    int numberToAdd = int.Parse(currentCommand[1]);
+                    numbers.Add(numberToAdd);
+                }
+                else if (currentCommand[0] == "Remove")
+                {
+                    int numberToRemove = int.Parse(currentCommand[1]);
+                    numbers.Remove(numberToRemove);
+
+                }
+                else if (currentCommand[0] == "RemoveAt")
+                {
+                    int index = int.Parse(currentCommand[1]);
+                    numbers.RemoveAt(index);
+                }
+                else if (currentCommand[0] == "Insert")
+                {
+                    int numberToIncerst = int.Parse(currentCommand[1]);
+                    int index = int.Parse(currentCommand[2]);
+                    numbers.Insert(index, numberToIncerst);
+                }
+                command = Console.ReadLine();
+            }
+            return numbers;
         }
     }
 }
