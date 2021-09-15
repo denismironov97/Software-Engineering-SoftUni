a41dc0b63ee3cc6b2d67f02d5ed4c7411a863b43
DenisM97 <denis.m.031997@gmail.com>
2021-09-15 14:29:36 +0300

    Projs created. From now on running on the .NET 5 Framework.

---
+++ C# Fundamentals/Lists/L.05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace L._05.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Fundamentals/Lists/L.06.ListManipulationBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace L._06.ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
