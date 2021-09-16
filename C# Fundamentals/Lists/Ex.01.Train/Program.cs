6874a9db807e840666a11720e8047673ba2c9c55
DenisM97 <denis.m.031997@gmail.com>
2021-09-16 20:26:36 +0300

    Ex.01 uploaded.

---
+++ C# Fundamentals/Lists/Ex.01.Train/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex._01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> passengersWagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int passengersLimit = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                int peopleToAdd = 0;
+                string[] currentCommand = input.Split();
+                if (currentCommand[0] == "Add")
+                {
+                    peopleToAdd = int.Parse(currentCommand[1]);
+                    passengersWagons.Add(peopleToAdd);
+                }
+                else // If(currentCommand[0] != "Add")
+                {
+                    peopleToAdd = int.Parse(currentCommand[0]);
+                    for (int i = 0; i < passengersWagons.Count; i++)
+                    {
+                        if ((passengersWagons[i] + peopleToAdd) <= passengersLimit)
+                        {
+                            passengersWagons[i] += peopleToAdd;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            foreach (int numPeople in passengersWagons)
+            {
+                Console.Write($"{numPeople} ");
+            }
+        }
+    }
+}
