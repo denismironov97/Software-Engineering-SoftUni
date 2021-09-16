4ddf09331c78a34c179f6e0b15942a646f69bbc7
DenisM97 <denis.m.031997@gmail.com>
2021-09-16 20:43:54 +0300

    Ex.03 uploaded.

---
+++ C# Fundamentals/Lists/Ex.03.HouseParty/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex._03.HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Allie is going!
+            //  0     1   2
+            // John is not going!
+            //   0  1   2    3
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> peopleOnList = new List<string>();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+                bool going = command.Count == 3;
+                if (going)
+                {
+                    string name = command[0];
+                    if (peopleOnList.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        peopleOnList.Add(name);
+                    }
+                }
+                else //not going 
+                {
+                    string name = command[0];
+                    if (peopleOnList.Contains(name) == false)
+                    {
+                        Console.WriteLine("{0} is not in the list!", name);
+                    }
+                    else
+                    {
+                        peopleOnList.Remove(name);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, peopleOnList));
+        }
+    }
+}
