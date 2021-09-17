d34fceb2302609cf5cd644107732561921eabbfb
DenisM97 <denis.m.031997@gmail.com>
2021-09-17 13:16:01 +0300

    Ex.01 fixed and uploaded.

---
+++ C# Programming Basics/Exercises/ConditionalStatements/Ex.01.SumSeconds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex._01.SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+            int totalSumOfSeconds = firstTime + secondTime + thirdTime;
+            int minutes = totalSumOfSeconds / 60; // int is chosen on pupose  
+            int seconds = totalSumOfSeconds % 60; // this will give you the seconds 
+            // all would look like "minite : seconds";
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
