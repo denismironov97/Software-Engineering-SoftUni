bdcb0985ec42d0ed48724dd702da11ac0ebaf7c0
DenisM97 <denis.m.031997@gmail.com>
2021-09-20 20:02:14 +0300

    Ex.05 uploaded.

---
+++ C# Fundamentals/Lists/Ex.05.BombNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex._05.BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] bombData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int specialBombNum = bombData[0];
+            int bombPower = bombData[1];
+            int bombIndexInList = numbersList.IndexOf(specialBombNum);
+
+            while (bombIndexInList != -1)
+            {
+                int leftNumbers = bombIndexInList - bombPower;
+                int rightNumbers = bombIndexInList + bombPower;
+                if (leftNumbers < 0)
+                {
+                    leftNumbers = 0;
+                }
+                if (rightNumbers > numbersList.Count - 1)
+                {
+                    rightNumbers = numbersList.Count - 1;
+                }
+                numbersList.RemoveRange(leftNumbers, rightNumbers - leftNumbers + 1); // important method for list 
+                bombIndexInList = numbersList.IndexOf(specialBombNum);
+            }
+            int sumOfListAfterDetonation = default;
+            foreach (int number in numbersList)
+            {
+                sumOfListAfterDetonation += number;
+            }
+            Console.WriteLine(sumOfListAfterDetonation);
+        }
+    }
+}
