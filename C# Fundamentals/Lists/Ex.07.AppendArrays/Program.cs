8a4589111d8b04773ca1b3aaa567f4f48d188e2b
DenisM97 <denis.m.031997@gmail.com>
2021-09-22 12:42:34 +0300

    Ex.07 uploaded.

---
+++ C# Fundamentals/Lists/Ex.07.AppendArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex._07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 2 3 |4 5 6 |  7  8
+            // List<int> numbersInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            // var input = Console.ReadLine().Split('|', ' ').ToList();
+
+            //List<int> input = Console.ReadLine()
+            //    .Split(new char[] { '|', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToList();
+
+            List<string> input = Console.ReadLine().Split('|').ToList();
+            for (int i = 0; i < input.Count / 2; i++) // reversing places array algorithm // important
+            {
+                var oldElement = input[i];
+                input[i] = input[input.Count - 1 - i];
+                input[input.Count - 1 - i] = oldElement;
+            }
+            // or input.Reverse;
+            List<int> numbers = new List<int>();
+            foreach (string str in input) //important
+            {
+                numbers.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToList()
+                                     );
+            }
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
