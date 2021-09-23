c19442ae5bbccbcddb3391cf7871e177cb925567
DenisM97 <denis.m.031997@gmail.com>
2021-09-23 14:04:06 +0300

    Lab.02 uploaded.

---
+++ C# Fundamentals/ObjectsAndClasses/L.02.BigFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace L._02.BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNum = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = 1; i <= nNum; i++)
+            {
+                factorial *= i; // factorial = factorial * i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
