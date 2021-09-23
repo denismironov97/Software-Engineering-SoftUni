028548527b7daeb365d5a1bf37e752edfad3cffc
DenisM97 <denis.m.031997@gmail.com>
2021-09-23 14:02:45 +0300

    Lab.01 uploaded.

--- C# Fundamentals/ObjectsAndClasses/L.01.RandomizeWords/Program.cs
+++ C# Fundamentals/ObjectsAndClasses/L.01.RandomizeWords/Program.cs
@@ -6,7 +6,20 @@ namespace L._01.RandomizeWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Welcome to SoftUni and have fun learning programming
+
+            string input = Console.ReadLine();
+            string[] wordsArray = input.Split(' ');
+            Random random = new Random(); // obj
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                int randomIndex = random.Next(0, wordsArray.Length);
+                string randomElement = wordsArray[randomIndex];
+                string element = wordsArray[i];
+                wordsArray[randomIndex] = element;
+                wordsArray[i] = randomElement;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, wordsArray));
         }
     }
 }
