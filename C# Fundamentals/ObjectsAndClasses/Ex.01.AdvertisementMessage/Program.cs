12a24734f29ec1839c718e5de28815524ecaa84c
DenisM <denis.m.031997@gmail.com>
2021-10-01 17:02:46 +0300

    Created Project.

---
+++ C# Fundamentals/ObjectsAndClasses/Ex.01.AdvertisementMessage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._01.AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
