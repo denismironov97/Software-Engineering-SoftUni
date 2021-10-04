491e2e52e554a3d6e809dc017b8b4c9799c71159
DenisM <denis.m.031997@gmail.com>
2021-10-04 14:08:44 +0300

    Project created.

---
+++ C# Fundamentals/ObjectsAndClasses/Ex.02.Articles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._02.Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
