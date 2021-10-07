a49a141b40730012fb80772bb57e6c5362d5992e
DenisM <denis.m.031997@gmail.com>
2021-10-07 16:24:10 +0300

    Ex.04 uploaded.

--- C# Fundamentals/ObjectsAndClasses/Ex.04.Students/Program.cs
+++ C# Fundamentals/ObjectsAndClasses/Ex.04.Students/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._04.Students
 {
@@ -6,7 +8,45 @@ namespace Ex._04.Students
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLines = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < nLines; i++)
+            {
+                string[] studentData = Console.ReadLine().Split();
+                string fistName = studentData[0];
+                string lastName = studentData[1];
+                float grade = float.Parse(studentData[2]);
+
+                var student = new Student(fistName, lastName, grade);
+
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(s => s.Grade).ToList();
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i].ToString());
+            }
         }
+    }
+
+    public class Student
+    {
+        public Student(string fistName, string lastName, float grade)
+        {
+            this.FirstName = fistName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+
     }
 }
