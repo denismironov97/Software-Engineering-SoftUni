d05117a88bc71adcdbbf674574287a675b5718e0
DenisM <denis.m.031997@gmail.com>
2021-10-11 15:09:32 +0300

    Project created.

---
+++ C# Fundamentals/ObjectsAndClasses/Ex.05.TeamworkProjectsModV1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex._05.TeamworkProjectsModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
