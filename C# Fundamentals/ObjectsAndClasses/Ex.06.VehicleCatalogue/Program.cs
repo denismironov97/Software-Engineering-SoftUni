686f5ea7108498afcb18d611cf54736b68fdcc74
DenisM <denis.m.031997@gmail.com>
2021-10-15 15:42:43 +0300

    Project created.

---
+++ C# Fundamentals/ObjectsAndClasses/Ex.06.VehicleCatalogue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._06.VehicleCatalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
