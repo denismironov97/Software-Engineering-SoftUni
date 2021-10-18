3925576ce93315667cef9b93f590c11e0aee5c55
DenisM <denis.m.031997@gmail.com>
2021-10-18 19:50:54 +0300

    Ex.07 uploaded.

--- C# Fundamentals/ObjectsAndClasses/Ex.07.OrderByAge/Program.cs
+++ C# Fundamentals/ObjectsAndClasses/Ex.07.OrderByAge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._07.OrderByAge
 {
@@ -6,7 +8,56 @@ namespace Ex._07.OrderByAge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Person> identificationPerson = new Dictionary<string, Person>();
+            while (true)
+            {
+                string[] personDataParts = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (personDataParts[0] == "End")
+                {
+                    break;
+                }
+
+                string name = personDataParts[0];
+                string ID = personDataParts[1];
+                int age = int.Parse(personDataParts[2]);
+
+                if (!identificationPerson.ContainsKey(ID))
+                {
+                    identificationPerson.Add(ID, new Person(name, ID, age));
+                }
+                else
+                {
+                    identificationPerson[ID].Name = name;
+                    identificationPerson[ID].Age = age;
+                }
+            }
+
+            foreach (var kvp in identificationPerson.OrderBy(x => x.Value.Age))
+            {
+                Console.WriteLine(kvp.Value);
+            }
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, string ID, int age)
+        {
+            this.Name = name;
+            this.Identification = ID;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public string Identification { get; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} with ID: {this.Identification} is {this.Age} years old.";
         }
     }
 }
