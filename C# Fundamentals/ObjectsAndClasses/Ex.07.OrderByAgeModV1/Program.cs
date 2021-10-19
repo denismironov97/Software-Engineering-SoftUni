eb588ea6ccef4b19d55219f22c874c7069f7fe13
DenisM <denis.m.031997@gmail.com>
2021-10-19 11:57:44 +0300

    Ex.07 ModV1 uploaded.

--- C# Fundamentals/ObjectsAndClasses/Ex.07.OrderByAgeModV1/Program.cs
+++ C# Fundamentals/ObjectsAndClasses/Ex.07.OrderByAgeModV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._07.OrderByAgeModV1
 {
@@ -6,7 +8,54 @@ namespace Ex._07.OrderByAgeModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Person> identificationPerson = new Dictionary<string, Person>();
+            while (true)
+            {
+                string[] personDataParts = Console.ReadLine().Split();
+
+                if (personDataParts[0] == "End")
+                {
+                    break;
+                }
+
+                var personData = (personDataParts[0], personDataParts[1], int.Parse(personDataParts[2]));//Custom Tuple Value composition of three 
+
+                if (!identificationPerson.ContainsKey(personData.Item2))
+                {
+                    identificationPerson.Add(personData.Item2, new Person(personData.Item1, personData.Item2, personData.Item3));
+                }
+                else
+                {
+                    identificationPerson[personData.Item2].Name = personData.Item2;
+                    identificationPerson[personData.Item2].Age = personData.Item3;
+                }
+            }
+
+            foreach (var kvp in identificationPerson.OrderBy(x => x.Value.Age))
+            {
+                Console.WriteLine(kvp.Value);
+            }
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, string ID, int age)
+        {
+            this.Name = name;
+            this.Identification = ID;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public string Identification { get; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} with ID: {this.Identification} is {this.Age} years old.";
         }
     }
 }
