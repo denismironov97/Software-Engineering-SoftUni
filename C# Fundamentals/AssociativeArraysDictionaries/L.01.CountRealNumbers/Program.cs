3acdc9969ca2f1f204ca4106e4d4fb5f64f3121a
DenisM <denis.m.031997@gmail.com>
2021-10-20 14:24:38 +0300

    Solution added.Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/L.01.CountRealNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
