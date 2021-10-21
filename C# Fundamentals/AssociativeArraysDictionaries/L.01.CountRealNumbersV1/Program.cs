e64847a60a7e24b995ec2893fd16e0c45587a87f
DenisM <denis.m.031997@gmail.com>
2021-10-21 17:59:32 +0300

    Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/L.01.CountRealNumbersV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._01.CountRealNumbersV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
