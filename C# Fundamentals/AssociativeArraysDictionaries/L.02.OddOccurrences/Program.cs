c22c525a4c7a536cacade204af178f18f2f72c79
DenisM <denis.m.031997@gmail.com>
2021-11-01 19:54:58 +0200

    Lab.02 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/L.02.OddOccurrences/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/L.02.OddOccurrences/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace L._02.OddOccurrences
 {
@@ -6,7 +8,30 @@ namespace L._02.OddOccurrences
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] words = Console.ReadLine().Split(" ");
+            Dictionary<string, int> countsOfOccurences = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string tempWordInLowerCase = word.ToLower();
+
+                if (countsOfOccurences.ContainsKey(tempWordInLowerCase))
+                {
+                    countsOfOccurences[tempWordInLowerCase]++;
+                }
+                else
+                {
+                    countsOfOccurences.Add(tempWordInLowerCase, 1);
+                }
+            }
+
+            foreach (var keyValuePair in countsOfOccurences)
+            {
+                if (keyValuePair.Value % 2 != 0) // odd value 
+                {
+                    Console.Write(keyValuePair.Key + " ");
+                }
+            }
         }
     }
 }
