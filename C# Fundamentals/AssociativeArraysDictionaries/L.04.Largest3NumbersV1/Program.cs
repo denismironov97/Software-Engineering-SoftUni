743db47fbb547da3dbd34bfee7f483411fcb8096
DenisM <denis.m.031997@gmail.com>
2021-11-04 15:53:09 +0200

    Lab.04 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/L.04.Largest3NumbersV1/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/L.04.Largest3NumbersV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace L._04.Largest3NumbersV1
 {
@@ -6,7 +8,24 @@ namespace L._04.Largest3NumbersV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] sortedNums = nums.OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (sortedNums.Length < 3)
+                {
+                    for (int j = 0; j < sortedNums.Length; j++)
+                    {
+                        Console.Write($"{sortedNums[j]} ");
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.Write(sortedNums[i] + " ");
+                }
+            }
         }
     }
 }
