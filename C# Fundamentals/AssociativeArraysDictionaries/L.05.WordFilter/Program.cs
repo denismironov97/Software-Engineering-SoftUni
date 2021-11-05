6c4ff72db15f834bd09c41a63628022320e90345
DenisM <denis.m.031997@gmail.com>
2021-11-05 17:49:47 +0200

    Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/L.05.WordFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._05.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
