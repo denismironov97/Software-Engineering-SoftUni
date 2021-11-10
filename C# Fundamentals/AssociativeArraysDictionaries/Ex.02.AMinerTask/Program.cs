7fd484fc6c3ede98c430afe3a57774956d895370
DenisM <denis.m.031997@gmail.com>
2021-11-10 15:44:52 +0200

    Ex.02 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.02.AMinerTask/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.02.AMinerTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex._02.AMinerTask
 {
@@ -6,7 +7,28 @@ namespace Ex._02.AMinerTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputData = string.Empty;
+            Dictionary<string, int> resourseData = new Dictionary<string, int>();
+
+            while ((inputData = Console.ReadLine()) != "stop")
+            {
+                string resource = inputData;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resourseData.ContainsKey(resource))
+                {
+                    resourseData[resource] += quantity;
+                }
+                else
+                {
+                    resourseData.Add(resource, quantity);
+                }
+            }
+
+            foreach (var kvp in resourseData)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
         }
     }
 }
