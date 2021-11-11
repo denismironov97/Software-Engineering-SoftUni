1e4d6b972b3282df180babb33a3d99a942b72f3e
Denis M <denis.m.031997@gmail.com>
2021-11-11 16:00:06 +0200

    Ex.03 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.03.LegendaryFarming/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.03.LegendaryFarming/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._03.LegendaryFarming
 {
@@ -6,7 +8,68 @@ namespace Ex._03.LegendaryFarming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string legendaryItem = string.Empty;
+            Dictionary<string, int> materials = new Dictionary<string, int>();
+            materials.Add("motes", 0);
+            materials.Add("shards", 0);
+            materials.Add("fragments", 0);
+            Dictionary<string, int> junk = new Dictionary<string, int>();
+
+            while (materials["motes"] < 250 && materials["fragments"] < 250 && materials["shards"] < 250)
+            {
+                string input = Console.ReadLine().ToLower();
+                string[] inputTokens = input.Split(" ");
+                for (int i = 0; i < inputTokens.Length; i += 2)
+                {
+                    int quantity = int.Parse(inputTokens[i]);
+                    string material = inputTokens[i + 1];
+
+                    switch (material)
+                    {
+                        case "shards":
+                        case "fragments":
+                        case "motes":
+                            materials[material] += quantity;
+                            break;
+                        default:
+                            if (!junk.ContainsKey(material))
+                                junk.Add(material, 0);
+                            junk[material] += quantity;
+                            break;
+                    }
+
+                    if (materials["shards"] >= 250 || materials["fragments"] >= 250 || materials["motes"] >= 250)
+                        break;
+                }
+            }
+
+            if (materials["shards"] >= 250)
+            {
+                legendaryItem = "Shadowmourne";
+                materials["shards"] -= 250;
+            }
+            else if (materials["fragments"] >= 250)
+            {
+                legendaryItem = "Valanyr";
+                materials["fragments"] -= 250;
+            }
+            else
+            {
+                legendaryItem = "Dragonwrath";
+                materials["motes"] -= 250;
+            }
+
+            Console.WriteLine($"{legendaryItem} obtained!");
+
+            foreach (var item in materials.OrderByDescending(entry => entry.Value).ThenBy(entry => entry.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            foreach (var item in junk.OrderBy(entry => entry.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
         }
     }
 }
