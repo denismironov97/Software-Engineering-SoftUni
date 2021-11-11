0e6fc62ab4291538677546104a28d7d39a25c12f
Denis M <denis.m.031997@gmail.com>
2021-11-11 15:57:09 +0200

    Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.03.LegendaryFarming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._03.LegendaryFarming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
