596af7b7b56343ec32e5fdceee13782838c27a92
Denis M <denis.m.031997@gmail.com>
2021-11-12 16:44:00 +0200

    Ex.03 ModV1 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.03.LegendaryFa-ingV1/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.03.LegendaryFa-ingV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._03.LegendaryFa_ingV1
 {
@@ -6,7 +8,77 @@ namespace Ex._03.LegendaryFa_ingV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> junkItems = new Dictionary<string, int>();
+            Dictionary<string, int> legendaryItemsData = new Dictionary<string, int>()
+            {
+                {"shards", 0},
+                {"fragments", 0},
+                {"motes", 0}
+            };
+
+            string legendaryItem = string.Empty;
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                string[] inputData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < inputData.Length; i += 2)
+                {
+                    int quantity = int.Parse(inputData[i]);
+                    string item = inputData[i + 1].ToLower();
+
+                    if (legendaryItemsData.ContainsKey(item))
+                    {
+                        legendaryItemsData[item] += quantity;
+
+                        if (legendaryItemsData[item] >= 250)
+                        {
+                            legendaryItem = item;
+                            legendaryItemsData[item] -= 250;
+                            isRunning = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (junkItems.ContainsKey(item))
+                        {
+                            junkItems[item] += quantity;
+                        }
+                        else
+                        {
+                            junkItems.Add(item, quantity);
+                        }
+                    }
+                }
+            }
+
+            if (legendaryItem == "shards")
+            {
+                Console.WriteLine($"Shadowmourne obtained!");
+            }
+            else if (legendaryItem == "fragments")
+            {
+                Console.WriteLine($"Valanyr obtained!");
+            }
+            else if (legendaryItem == "motes")
+            {
+                Console.WriteLine("Dragonwrath obtained!");
+            }
+
+            Dictionary<string, int> soretedLegendaryItems = legendaryItemsData.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+            Dictionary<string, int> sortedJunkIntems = junkItems.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var kvp in soretedLegendaryItems)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+            foreach (var kvp in sortedJunkIntems)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
         }
     }
 }
