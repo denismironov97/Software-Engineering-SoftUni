0a5179df68cfb48ec52be0e729830a2c3929a650
DenisM <denis.m.031997@gmail.com>
2021-11-15 21:24:09 +0200

    Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.04.Orders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._04.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
