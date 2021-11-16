1ee7308d16aa5cab761d96cbbec94b2d0abe2560
DenisM <denis.m.031997@gmail.com>
2021-11-16 21:19:12 +0200

    Ex.05 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.05.SoftUniParking/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.05.SoftUniParking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._05.SoftUniParking
 {
@@ -6,7 +8,58 @@ namespace Ex._05.SoftUniParking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOfCommands = int.Parse(Console.ReadLine());
+            Dictionary<string, string> parkingInformnation = new Dictionary<string, string>();
+
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] inputData = Console.ReadLine().Split(' ');
+                string command = inputData[0];
+                string nameOfPerson = inputData[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = inputData[2];
+
+                    if (!parkingInformnation.ContainsKey(nameOfPerson))
+                    {
+                        parkingInformnation.Add(nameOfPerson, licensePlateNumber);
+                        Console.WriteLine($"{nameOfPerson} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        foreach (var kvp in parkingInformnation)
+                        {
+                            string username = kvp.Key;
+                            string plateNumber = kvp.Value;
+
+                            if (username == nameOfPerson && plateNumber == licensePlateNumber
+                                || username == nameOfPerson && plateNumber != licensePlateNumber) //important did it by myself and I'm proud
+                            {
+                                Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+                                break;
+                            }
+                        }
+                    }
+                }
+                else //unregister
+                {
+                    if (!parkingInformnation.ContainsKey(nameOfPerson))
+                    {
+                        Console.WriteLine($"ERROR: user {nameOfPerson} not found");
+                    }
+                    else
+                    {
+                        parkingInformnation.Remove(nameOfPerson);
+                        Console.WriteLine($"{nameOfPerson} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var kvp in parkingInformnation)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
         }
     }
 }
