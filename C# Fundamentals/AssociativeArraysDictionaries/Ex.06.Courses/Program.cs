563c49e37a2a944fac5d7337baa7270e5b59497f
DenisM <denis.m.031997@gmail.com>
2021-11-17 18:19:56 +0200

    Ex.06 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.06.Courses/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.06.Courses/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._06.Courses
 {
@@ -6,7 +8,41 @@ namespace Ex._06.Courses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputData = string.Empty;
+            Dictionary<string, List<string>> coursesAndStudentsData = new Dictionary<string, List<string>>(); // review objects and classes basics 
+
+            while ((inputData = Console.ReadLine()) != "end")
+            {
+                string[] commandData = inputData.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string courseName = commandData[0];
+                string studentName = commandData[1];
+                List<string> students = new List<string>();
+
+                if (!coursesAndStudentsData.ContainsKey(courseName))
+                {
+                    students.Add(studentName);
+                    coursesAndStudentsData.Add(courseName, students);
+                }
+                else
+                {
+                    coursesAndStudentsData[courseName].Add(studentName);
+                }
+            }
+
+            Dictionary<string, List<string>> sortedDictionary = coursesAndStudentsData.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value); // what's this?
+
+            foreach (var kvp in sortedDictionary)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+                // Console.WriteLine($"--{string.Join(Environment.NewLine, kvp.Value)}"); // how to make it without using the cyckle
+
+                kvp.Value.Sort();
+
+                foreach (var student in kvp.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
         }
     }
 }
