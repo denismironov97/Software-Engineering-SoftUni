02a8db3b7097a554912ee71d105f85a83943f6a1
DenisM <denis.m.031997@gmail.com>
2021-11-18 18:49:18 +0200

    Ex.07 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.07.StudentAcademy/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.07.StudentAcademy/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._07.StudentAcademy
 {
@@ -6,7 +8,43 @@ namespace Ex._07.StudentAcademy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLines = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentNameGradeInformation = new Dictionary<string, List<double>>();
+            Dictionary<string, double> studentNameAverageGradeInformation = new Dictionary<string, double>();
+
+            for (int i = 0; i < nLines; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+                //List<double> grades = new List<double>();
+
+                if (!studentNameGradeInformation.ContainsKey(studentName))
+                {
+
+                    studentNameGradeInformation.Add(studentName, new List<double>());
+                    studentNameGradeInformation[studentName].Add(studentGrade);
+                    studentNameAverageGradeInformation.Add(studentName, 0); //
+                }
+                else
+                {
+                    studentNameGradeInformation[studentName].Add(studentGrade);
+                }
+            }
+
+            foreach (var kvp in studentNameGradeInformation)
+            {
+                List<double> gradesPerStudent = kvp.Value;
+                double studentAverageGrade = gradesPerStudent.Average();
+
+                studentNameAverageGradeInformation[kvp.Key] = studentAverageGrade;
+            }
+
+            Dictionary<string, double> sortedAverageGradesOfSudents = studentNameAverageGradeInformation.Where(x => x.Value >= 4.50).OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var kvp in sortedAverageGradesOfSudents)
+            {
+                Console.WriteLine("{0} -> {1:f2}", kvp.Key, kvp.Value);
+            }
         }
     }
 }
