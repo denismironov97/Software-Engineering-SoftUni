42d2025f1f1b0da969445606604061d83013b42b
DenisM <denis.m.031997@gmail.com>
2021-11-18 18:47:02 +0200

    Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.07.StudentAcademy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._07.StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
