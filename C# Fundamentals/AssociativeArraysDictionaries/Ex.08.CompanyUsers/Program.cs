0a83f3847fe3343fe56bbc8aa40ee53681e376e5
DenisM <denis.m.031997@gmail.com>
2021-11-20 14:02:03 +0200

    Ex.08 uploaded.

--- C# Fundamentals/AssociativeArraysDictionaries/Ex.08.CompanyUsers/Program.cs
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.08.CompanyUsers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._08.CompanyUsers
 {
@@ -6,7 +8,52 @@ namespace Ex._08.CompanyUsers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, List<string>> companyEmployeeID = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string inputData = Console.ReadLine();
+
+                if (inputData == "End")
+                {
+                    break;
+                }
+
+                string[] companyData = inputData.Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                string company = companyData[0];
+                string employeeID = companyData[1];
+
+                if (!companyEmployeeID.ContainsKey(company))
+                {
+                    companyEmployeeID.Add(company, new List<string>());
+                    companyEmployeeID[company].Add(employeeID);
+                }
+
+                companyEmployeeID[company].Add(employeeID);
+            }
+
+            companyEmployeeID = companyEmployeeID.OrderBy(companyName => companyName.Key).ToDictionary(company => company.Key, employee => employee.Value);
+
+            foreach (var kvp in companyEmployeeID)
+            {
+                Console.WriteLine($"{kvp.Key}");
+
+                //foreach (string emplyeeID in kvp.Value)
+                //{
+                //    Console.WriteLine($"-- {emplyeeID}");
+                //}
+
+                //for (int i = 0; i < kvp.Value.Count; i++)
+                //{
+                //    Console.WriteLine($"-- {kvp.Value[i]}");
+                //}
+
+                List<string> uniqueEmployeesID = kvp.Value.Distinct().ToList();
+                foreach (var employeeID in uniqueEmployeesID)
+                {
+                    Console.WriteLine($"-- {employeeID}");
+                }
+            }
         }
     }
 }
