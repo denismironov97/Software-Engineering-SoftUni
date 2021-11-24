c483e591ee2358afc991337b160c215d9e208c5f
DenisM <denis.m.031997@gmail.com>
2021-11-24 11:06:24 +0200

    Project created.

---
+++ C# Fundamentals/AssociativeArraysDictionaries/Ex.09.ForceBookModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._09.ForceBookModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
