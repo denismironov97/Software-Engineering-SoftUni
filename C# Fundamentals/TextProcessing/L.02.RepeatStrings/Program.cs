314527e3af045be6394ceb3340e9115242d1e3ef
DenisM <denis.m.031997@gmail.com>
2021-11-29 11:30:50 +0200

    Project created.

---
+++ C# Fundamentals/TextProcessing/L.02.RepeatStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
