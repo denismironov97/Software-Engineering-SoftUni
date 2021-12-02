2ab32b5c0499edb344e80dae0bda2b7bc5ce7578
DenisM <denis.m.031997@gmail.com>
2021-12-02 12:36:47 +0200

    Lab.04 ModV1 uploaded.

--- C# Fundamentals/TextProcessing/L.04.TextFilterModV1/Program.cs
+++ C# Fundamentals/TextProcessing/L.04.TextFilterModV1/Program.cs
@@ -6,7 +6,28 @@ namespace L._04.TextFilterModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] banListOfWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+            Solution01(banListOfWords, text);
+        }
+        private static void Solution01(string[] banlist, string textToEdit)
+        {
+            foreach (string banWord in banlist)
+            {
+                string replacement = string.Empty;
+
+                for (int i = 0; i < banWord.Length; i++)
+                {
+                    replacement += "*";
+                }
+
+                if (textToEdit.Contains(banWord))
+                {
+                    textToEdit = textToEdit.Replace(banWord, replacement);
+                }
+            }
+
+            Console.WriteLine(textToEdit);
         }
     }
 }
