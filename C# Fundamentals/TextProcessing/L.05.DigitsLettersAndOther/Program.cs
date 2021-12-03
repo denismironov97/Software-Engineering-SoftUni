5db9c72e85001aafb856689f7355189d64a7786a
DenisM <denis.m.031997@gmail.com>
2021-12-03 11:53:11 +0200

    Project created.

---
+++ C# Fundamentals/TextProcessing/L.05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._05.DigitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
