0fe97e2f88b2237290a5ab942f1a131543cb77d7
DenisM <denis.m.031997@gmail.com>
2021-12-06 11:05:42 +0200

    Ex.01 uploaded.

--- C# Fundamentals/TextProcessing/Ex.01.ValidUsernames/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.01.ValidUsernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Ex._01.ValidUsernames
 {
@@ -6,7 +7,37 @@ namespace Ex._01.ValidUsernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] usernameData = Console.ReadLine().Split(", ");
+            StringBuilder validUsernames = new StringBuilder();
+
+            foreach (string username in usernameData)
+            {
+                int usernameLengh = username.Length;
+                bool validLengh = usernameLengh >= 3 && usernameLengh <= 16;
+                bool isInvalidCharFound = false;
+
+                if (validLengh)
+                {
+                    foreach (char letter in username)
+                    {
+                        int asciiCodeNumber = letter;
+
+                        if (asciiCodeNumber >= 32 && asciiCodeNumber <= 44 || asciiCodeNumber == 46 || asciiCodeNumber == 47 || asciiCodeNumber >= 58 && asciiCodeNumber <= 64
+                            || asciiCodeNumber >= 91 && asciiCodeNumber <= 94 || asciiCodeNumber == 96 || asciiCodeNumber >= 123 && asciiCodeNumber <= 126)
+                        {
+                            isInvalidCharFound = true;
+                            break;
+                        }
+                    }
+
+                    if (isInvalidCharFound == false)
+                    {
+                        validUsernames.AppendLine(username);
+                    }
+                }
+            }
+
+            Console.Write(validUsernames);
         }
     }
 }
