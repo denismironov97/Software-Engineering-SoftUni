23c1d34cc21ac638a368084ba0ab06968f9f9ba9
DenisM <denis.m.031997@gmail.com>
2021-12-07 16:14:23 +0200

    Ex.01 ModV1 uploaded.

--- C# Fundamentals/TextProcessing/Ex.01.ValidUsernamesModV1/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.01.ValidUsernamesModV1/Program.cs
@@ -6,7 +6,38 @@ namespace Ex._01.ValidUsernamesModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] usernames = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string username in usernames)
+            {
+                if (IsValid(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        private static bool IsValid(string username)
+        {
+            return IsValidLength(username) && ContainsValidSymbols(username);
+        }
+
+        private static bool IsValidLength(string username)
+        {
+            return username.Length >= 3 && username.Length <= 16;
+        }
+
+        private static bool ContainsValidSymbols(string username)
+        {
+            foreach (char symbol in username)
+            {
+                if (!char.IsLetterOrDigit(symbol) && symbol != '-' && symbol != '_')
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
