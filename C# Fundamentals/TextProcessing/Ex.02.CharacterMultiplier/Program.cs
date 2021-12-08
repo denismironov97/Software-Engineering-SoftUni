9aa1fd425e11c806c25a8fd464c22a4107d9705d
DenisM <denis.m.031997@gmail.com>
2021-12-08 13:39:02 +0200

    Ex.02 uploaded.

--- C# Fundamentals/TextProcessing/Ex.02.CharacterMultiplier/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.02.CharacterMultiplier/Program.cs
@@ -6,7 +6,44 @@ namespace Ex._02.CharacterMultiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputStringsData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            long result = GetProductSumOfTwoStrings(inputStringsData[0], inputStringsData[1]);
+            Console.WriteLine(result);
+        }
+
+        private static long GetProductSumOfTwoStrings(string text1, string text2)
+        {
+            int textMinLength = Math.Min(text1.Length, text2.Length);
+            int textMaxLength = Math.Max(text1.Length, text2.Length);
+            long productSum = 0;
+
+            for (int i = 0; i < textMinLength; i++)
+            {
+                productSum = productSum + (text1[i] * text2[i]);
+            }
+
+            if (text1.Length > text2.Length)
+            {
+                productSum += SumRemainingChars(textMinLength, textMaxLength, text1);
+            }
+            else if (text2.Length > text1.Length)
+            {
+                productSum += SumRemainingChars(textMinLength, textMaxLength, text2);
+            }
+
+            return productSum;
+        }
+
+        private static int SumRemainingChars(int startIndex, int endIndex, string text)
+        {
+            int sumOfChars = 0;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                sumOfChars += text[i];
+            }
+
+            return sumOfChars;
         }
     }
 }
