203f085eb220d3c7f0c8dd53e390fe232d12c051
DenisM <denis.m.031997@gmail.com>
2021-12-09 15:57:24 +0200

    Project created.

---
+++ C# Fundamentals/TextProcessing/Ex.02.CharacterMultiplierModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._02.CharacterMultiplierModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
