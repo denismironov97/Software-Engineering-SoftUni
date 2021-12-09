a777b731f7f20796c93722449a8d3bbaf9c2c59b
DenisM <denis.m.031997@gmail.com>
2021-12-09 15:58:10 +0200

    Ex.02 ModV1 uploaded.

--- C# Fundamentals/TextProcessing/Ex.02.CharacterMultiplierModV1/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.02.CharacterMultiplierModV1/Program.cs
@@ -6,7 +6,42 @@ namespace Ex._02.CharacterMultiplierModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputStringsData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            long result = GetProductOfTwoStrings(inputStringsData[0], inputStringsData[1]);
+            Console.WriteLine(result);
+        }
+
+        private static long GetProductOfTwoStrings(string text1, string text2)
+        {
+            int text1Length = text1.Length;
+            int text2Length = text2.Length;
+            int textMinLength = Math.Min(text1Length, text2Length);
+            int textMaxLength = Math.Max(text1Length, text2Length);
+            int difference = textMaxLength - textMinLength;
+
+            long productSum = 0;
+
+            for (int i = 0; i < textMinLength; i++)
+            {
+                productSum += text1[i] * text2[i];
+            }
+
+            if (text1Length > text2Length)
+            {
+                for (int i = textMaxLength - difference; i < textMaxLength; i++)
+                {
+                    productSum += text1[i];
+                }
+            }
+            else if (text1Length < text2Length)
+            {
+                for (int i = textMaxLength - difference; i < textMaxLength; i++)
+                {
+                    productSum += text2[i];
+                }
+            }
+
+            return productSum;
         }
     }
 }
