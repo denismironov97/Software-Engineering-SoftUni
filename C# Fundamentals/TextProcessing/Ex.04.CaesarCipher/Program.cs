47ff583b105fae98c4819959a9fabeaa254ebea6
DenisM <denis.m.031997@gmail.com>
2021-12-17 13:41:20 +0200

    Ex.04 uploaded.

--- C# Fundamentals/TextProcessing/Ex.04.CaesarCipher/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.04.CaesarCipher/Program.cs
@@ -6,7 +6,16 @@ namespace Ex._04.CaesarCipher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = Console.ReadLine();
+            string encryptedmessage = string.Empty;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                char encrypedSymbol = (char)(message[i] + 3);
+                encryptedmessage += encrypedSymbol;
+            }
+
+            Console.WriteLine(encryptedmessage);
         }
     }
 }
