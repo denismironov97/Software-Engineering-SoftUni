b4e4b49f8e2f71d5c1042dc206c59e94ce728a7d
DenisM <denis.m.031997@gmail.com>
2021-12-17 13:40:56 +0200

    Program created.

---
+++ C# Fundamentals/TextProcessing/Ex.04.CaesarCipher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._04.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
