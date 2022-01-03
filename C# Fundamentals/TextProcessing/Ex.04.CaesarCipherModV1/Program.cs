84ae43531a3b4f3f6c6d4e4412176d9434fe56ed
DenisM <denis.m.031997@gmail.com>
2022-01-03 16:51:16 +0200

    Ex.04 ModV1 uploaded.

--- C# Fundamentals/TextProcessing/Ex.04.CaesarCipherModV1/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.04.CaesarCipherModV1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Ex._04.CaesarCipherModV1
 {
@@ -6,7 +7,34 @@ namespace Ex._04.CaesarCipherModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = Console.ReadLine();
+            StringBuilder encryptedmessage = new StringBuilder();
+
+            //string encryptedmessage = string.Empty;
+
+            //for (int i = 0; i < message.Length; i++)
+            //{
+            //    char originalSymbol = message[i];
+            //    char encrypedSymbol = (char)(message[i] + 3);
+            //    encryptedmessage += encrypedSymbol;
+            //}
+            //Console.WriteLine(encryptedmessage);
+
+
+            //foreach (char symbol in message)
+            //{
+            //    char encryptedSymbol = (char)(symbol + 3);
+            //    message = message.Replace(symbol, encryptedSymbol);
+            //}
+            //Console.WriteLine(message);
+
+            foreach (char symbol in message)
+            {
+                char encryptedSymbol = (char)(symbol + 3);
+                encryptedmessage.Append(encryptedSymbol);
+            }
+
+            Console.WriteLine(encryptedmessage.ToString());
         }
     }
 }
