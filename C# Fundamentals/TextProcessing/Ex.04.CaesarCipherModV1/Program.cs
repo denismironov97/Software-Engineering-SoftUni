f4a679be7c5e9d498a067a847a91eda15776f3ef
DenisM <denis.m.031997@gmail.com>
2022-01-03 16:47:31 +0200

    Project created.

---
+++ C# Fundamentals/TextProcessing/Ex.04.CaesarCipherModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._04.CaesarCipherModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
