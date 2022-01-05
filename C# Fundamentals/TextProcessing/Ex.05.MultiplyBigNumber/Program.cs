fa9676cb793c20dea42a32e5fa78879dafae72af
DenisM <denis.m.031997@gmail.com>
2022-01-05 19:00:19 +0200

    Project created.

---
+++ C# Fundamentals/TextProcessing/Ex.05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._05.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
