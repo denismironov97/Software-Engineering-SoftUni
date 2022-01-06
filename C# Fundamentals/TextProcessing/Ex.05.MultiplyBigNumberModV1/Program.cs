8b3dcdacfa397da01bad7b5f31447c8a1e18a95c
Denis M <denis.m.031997@gmail.com>
2022-01-06 16:38:02 +0200

    Ex.05 ModV1 uploaded.

--- C# Fundamentals/TextProcessing/Ex.05.MultiplyBigNumberModV1/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.05.MultiplyBigNumberModV1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex._05.MultiplyBigNumberModV1
 {
@@ -6,7 +7,42 @@ namespace Ex._05.MultiplyBigNumberModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string num1 = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+            int remainder = 0;
+            List<string> resultProduct = new List<string>();
+
+            if (multiplier == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int i = num1.Length - 1; i >= 0; i--)
+            {
+                int currentDigit = num1[i] - 48;
+                remainder = remainder + (multiplier * currentDigit);
+
+                if (remainder > 9)
+                {
+                    int remainderLastDigit = remainder % 10;
+                    remainder = remainder / 10;
+                    resultProduct.Add(remainderLastDigit.ToString());
+                }
+                else // <= 9
+                {
+                    resultProduct.Add(remainder.ToString());
+                    remainder = 0;
+                }
+            }
+
+            if (remainder > 0)
+            {
+                resultProduct.Add(remainder.ToString());
+            }
+
+            resultProduct.Reverse();
+            Console.WriteLine(string.Join(string.Empty, resultProduct));
         }
     }
 }
