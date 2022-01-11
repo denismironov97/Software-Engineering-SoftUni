895416ea65ee6fab66ae847ce59b6cdd5cc2bc6d
DenisM <denis.m.031997@gmail.com>
2022-01-11 12:48:57 +0200

    Ex.07 uploaded.

--- C# Fundamentals/TextProcessing/Ex.07.StringExplosion/Program.cs
+++ C# Fundamentals/TextProcessing/Ex.07.StringExplosion/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Ex._07.StringExplosion
 {
@@ -6,7 +7,28 @@ namespace Ex._07.StringExplosion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+            StringBuilder resultText = new StringBuilder();
+            int bomb = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '>')
+                {
+                    bomb += text[i + 1] - 48;
+                    resultText.Append(text[i]);
+                }
+                else if (bomb > 0)
+                {
+                    bomb -= 1;
+                }
+                else
+                {
+                    resultText.Append(text[i]);
+                }
+            }
+
+            Console.WriteLine(resultText);
         }
     }
 }
