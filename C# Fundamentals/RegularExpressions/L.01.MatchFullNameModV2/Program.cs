fd587c8318457659d7b288380939bb78a648c16f
DenisM <denis.m.031997@gmail.com>
2022-01-17 18:29:08 +0200

    Project created.

---
+++ C# Fundamentals/RegularExpressions/L.01.MatchFullNameModV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._01.MatchFullNameModV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
