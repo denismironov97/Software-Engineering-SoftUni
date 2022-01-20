7fa6cd262340ebfe5c005db3e2da649b468ebb4b
DenisM <denis.m.031997@gmail.com>
2022-01-20 14:04:39 +0200

    Project created.

---
+++ C# Fundamentals/RegularExpressions/L.02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L._02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
