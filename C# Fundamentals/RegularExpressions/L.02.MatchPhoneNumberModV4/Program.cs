980bccedcc58f7f66347f18c7b33dbfc0f7c9716
DenisM <denis.m.031997@gmail.com>
2022-01-28 14:52:17 +0200

    Lab.02 Version 4 uploaded.

--- C# Fundamentals/RegularExpressions/L.02.MatchPhoneNumberModV4/Program.cs
+++ C# Fundamentals/RegularExpressions/L.02.MatchPhoneNumberModV4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace L._02.MatchPhoneNumberModV4
 {
@@ -6,7 +8,14 @@ namespace L._02.MatchPhoneNumberModV4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string phones = Console.ReadLine();
+
+            string pattern = @"\+([359]+)([- ])2(\2)(\d{3})(\2)(\d{4})\b";
+
+            MatchCollection phoneMatches = Regex.Matches(phones, pattern);
+
+            string[] matchPhones = phoneMatches.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", matchPhones));
         }
     }
 }
