dc8783b20632946f033a57d3f3a37cf72d9a64d7
Denis M <denis.m.031997@gmail.com>
2022-02-02 15:14:07 +0200

    Project created.

---
+++ C# Fundamentals/RegularExpressions/L.03.MatchDatesModV1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace L._03.MatchDatesModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
