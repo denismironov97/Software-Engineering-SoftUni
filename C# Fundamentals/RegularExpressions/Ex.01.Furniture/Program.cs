617c18ab146cb5e4ab5fd9cbeff1d33a86b5004a
Denis M <denis.m.031997@gmail.com>
2022-02-03 12:21:50 +0200

    Project created.

---
+++ C# Fundamentals/RegularExpressions/Ex.01.Furniture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._01.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
