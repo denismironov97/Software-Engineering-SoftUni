9a99c234480ccbb0e9a738775ae89b90230540ff
Denis M <denis.m.031997@gmail.com>
2022-02-03 12:23:03 +0200

    Ex.01 uploaded.

--- C# Fundamentals/RegularExpressions/Ex.01.Furniture/Program.cs
+++ C# Fundamentals/RegularExpressions/Ex.01.Furniture/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Ex._01.Furniture
 {
@@ -6,7 +8,37 @@ namespace Ex._01.Furniture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ">>{furniture name}<<{price}!{quantity}"
+            //string pattern = @">>[A-Za-z]+<<((\d+\.\d+)|(\d+))!\d+";
+            string pattern = @">>([A-Za-z]+)<<((\d+\.\d+)|(\d+))!(\d+)";
+            Regex regex = new Regex(pattern);
+
+            string furnitureData = string.Empty;
+            double totalPriceOfFurnitureAll = 0;
+            List<string> furniture = new List<string>();
+
+            while ((furnitureData = Console.ReadLine()) != "Purchase")
+            {
+                Match furnitureInformation = regex.Match(furnitureData);
+
+                if (furnitureInformation.Success)
+                {
+                    string furnitureName = furnitureInformation.Groups[1].Value;
+                    double price = double.Parse(furnitureInformation.Groups[2].Value);
+                    int quantity = int.Parse(furnitureInformation.Groups[5].Value);
+                    totalPriceOfFurnitureAll = totalPriceOfFurnitureAll + (price * quantity);
+                    furniture.Add(furnitureName);
+                }
+            }
+
+            Console.WriteLine($"Bought furniture:");
+
+            foreach (string product in furniture)
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine($"Total money spend: {totalPriceOfFurnitureAll:f2}");
         }
     }
 }
