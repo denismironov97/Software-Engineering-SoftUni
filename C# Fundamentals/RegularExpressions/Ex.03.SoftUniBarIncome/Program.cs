bc6686803795e8339ec2346de00036bdc492cb9c
DenisM <denis.m.031997@gmail.com>
2022-02-07 17:37:31 +0200

    Ex.03 uploaded.

--- C# Fundamentals/RegularExpressions/Ex.03.SoftUniBarIncome/Program.cs
+++ C# Fundamentals/RegularExpressions/Ex.03.SoftUniBarIncome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Ex._03.SoftUniBarIncome
 {
@@ -6,7 +7,32 @@ namespace Ex._03.SoftUniBarIncome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+            double totalIncome = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "end of shift")
+                {
+                    break;
+                }
+
+                if (Regex.IsMatch(line, pattern))
+                {
+                    Match match = Regex.Match(line, pattern);
+                    string customer = match.Groups["customer"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+                    double money = price * count;
+                    Console.WriteLine($"{customer}: {product} - {money:F2}");
+                    totalIncome += money;
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:F2}");
         }
     }
 }
