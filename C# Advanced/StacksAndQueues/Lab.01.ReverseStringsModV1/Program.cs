173f15bad267f335e19544876cfd1653f7027f17
DenisM <denis.m.031997@gmail.com>
2022-02-10 13:11:51 +0200

    Lab.01 ModV1 uploaded.

--- C# Advanced/StacksAndQueues/Lab.01.ReverseStringsModV1/Program.cs
+++ C# Advanced/StacksAndQueues/Lab.01.ReverseStringsModV1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._01.ReverseStringsModV1
 {
@@ -7,7 +8,19 @@ namespace Lab._01.ReverseStringsModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            string input = Console.ReadLine();
+            Stack<char> reversedInput = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+                reversedInput.Push(letter);
+            }
+
+            foreach (char symbol in reversedInput)
+            {
+                Console.Write(symbol);
+            }
         }
     }
 }
