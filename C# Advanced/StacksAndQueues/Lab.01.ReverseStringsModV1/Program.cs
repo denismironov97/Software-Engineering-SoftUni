fbb3298b7fe9d720dff364487e93407caf10bc31
DenisM <denis.m.031997@gmail.com>
2022-02-10 13:10:28 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.01.ReverseStringsModV1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab._01.ReverseStringsModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
