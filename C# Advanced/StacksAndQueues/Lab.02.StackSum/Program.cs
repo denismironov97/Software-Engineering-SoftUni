72a50454f9c5c2544b21d195f30398905d1cbfe2
DenisM <denis.m.031997@gmail.com>
2022-02-11 16:28:50 +0200

    Lab.02 uploaded.

--- C# Advanced/StacksAndQueues/Lab.02.StackSum/Program.cs
+++ C# Advanced/StacksAndQueues/Lab.02.StackSum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._02.StackSum
 {
@@ -6,7 +8,50 @@ namespace Lab._02.StackSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numsInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            //Stack<int> numbers = new Stack<int>(numsInput);
+
+            Stack<int> numbers = new Stack<int>();
+
+            foreach (int number in numsInput)
+            {
+                numbers.Push(number);
+            }
+
+            string commandData = string.Empty;
+
+            while ((commandData = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] commandInformation = commandData.Split();
+
+                switch (commandInformation[0])
+                {
+                    case "add":
+                        numbers.Push(int.Parse(commandInformation[1]));
+                        numbers.Push(int.Parse(commandInformation[2]));
+                        break;
+                    case "remove":
+                        int countToRemoveFromStack = int.Parse(commandInformation[1]);
+
+                        if (countToRemoveFromStack <= numbers.Count)
+                        {
+                            for (int i = 0; i < countToRemoveFromStack; i++)
+                            {
+                                numbers.Pop();
+                            }
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                        break;
+                }
+            }
+
+            short sum = (short)numbers.Sum();
+            Console.WriteLine($"Sum: {sum}");
         }
     }
 }
