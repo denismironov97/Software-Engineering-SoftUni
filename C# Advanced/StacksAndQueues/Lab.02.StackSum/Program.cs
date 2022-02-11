4263cb1fb4b015a9270405b276c3c68efbe50d42
DenisM <denis.m.031997@gmail.com>
2022-02-11 16:27:34 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.02.StackSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._02.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
