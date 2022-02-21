312c3adbb0cf3b31e18ce3237281a81e17fc1409
DenisM <denis.m.031997@gmail.com>
2022-02-21 13:20:58 +0200

    Lab.03 uploaded.

--- C# Advanced/StacksAndQueues/Lab.03.SimpleCalculator/Program.cs
+++ C# Advanced/StacksAndQueues/Lab.03.SimpleCalculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._03.SimpleCalculator
 {
@@ -6,7 +8,30 @@ namespace Lab._03.SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputToCalc = Console.ReadLine().Split();
+            Stack<string> calcData = new Stack<string>(inputToCalc.Reverse());
+
+            while (calcData.Count > 1)
+            {
+                int num1 = int.Parse(calcData.Pop());
+                char symbol = char.Parse(calcData.Pop());
+                int num2 = int.Parse(calcData.Pop());
+
+                int tempResult = 0;
+
+                if (symbol == '+')
+                {
+                    tempResult = num1 + num2;
+                }
+                else
+                {
+                    tempResult = num1 - num2;
+                }
+
+                calcData.Push(tempResult.ToString());
+            }
+
+            Console.WriteLine(calcData.Pop());
         }
     }
 }
