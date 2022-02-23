175a70b736d3f6ff43f89858b7752b8bc70777ec
DenisM <denis.m.031997@gmail.com>
2022-02-23 14:06:32 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.04.MatchingBrackets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
