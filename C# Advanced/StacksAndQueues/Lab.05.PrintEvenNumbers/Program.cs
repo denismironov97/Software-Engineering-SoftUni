12b618a5fce222d571788db6007cbf42535ebfa7
DenisM <denis.m.031997@gmail.com>
2022-02-24 20:52:30 +0200

    Lab.05 uploaded.

--- C# Advanced/StacksAndQueues/Lab.05.PrintEvenNumbers/Program.cs
+++ C# Advanced/StacksAndQueues/Lab.05.PrintEvenNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._05.PrintEvenNumbers
 {
@@ -6,7 +8,30 @@ namespace Lab._05.PrintEvenNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] numbersInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //Queue<int> evenNumsQueue = new Queue<int>(numbersInput.Where(num => num % 2 == 0));
+            //Console.WriteLine(string.Join(", ", evenNumsQueue));
+
+            int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Queue<int> queue = new Queue<int>(input);
+            int count = queue.Sum();
+            int sum = 0;
+
+            while (sum != count) //Look up later 
+            {
+                sum += queue.Peek();
+
+                if (queue.Peek() % 2 == 1)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
         }
     }
 }
