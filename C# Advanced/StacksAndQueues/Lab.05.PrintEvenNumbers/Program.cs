ced77f715fa518646b25900ac2982c2c8e0113c2
DenisM <denis.m.031997@gmail.com>
2022-02-24 20:48:20 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
