ce055d3e3e2918b33584266c79ea3e89a0a32425
DenisM <denis.m.031997@gmail.com>
2022-02-28 12:21:52 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.06.Supermarket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
