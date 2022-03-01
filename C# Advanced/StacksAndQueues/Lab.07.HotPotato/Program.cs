7bcf8bcd1979dbac7d2874f7f885db34534cf427
DenisM <denis.m.031997@gmail.com>
2022-03-01 21:44:00 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.07.HotPotato/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
