89c369644de5ce64c99036e975dc4098f2ff36b6
DenisM <denis.m.031997@gmail.com>
2022-03-01 21:45:34 +0200

    Lab.07 uploaded.

--- C# Advanced/StacksAndQueues/Lab.07.HotPotato/Program.cs
+++ C# Advanced/StacksAndQueues/Lab.07.HotPotato/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab._07.HotPotato
 {
@@ -6,7 +7,23 @@ namespace Lab._07.HotPotato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] playerNames = Console.ReadLine().Split();
+            int nToss = int.Parse(Console.ReadLine());
+            Queue<string> players = new Queue<string>(playerNames);
+
+            while (players.Count > 1)
+            {
+                for (int i = 1; i < nToss; i++) // important algorithm 
+                {
+                    //players.Enqueue(players.Dequeue());
+                    string playerToMoveToTheBackOfQueue = players.Dequeue();
+                    players.Enqueue(playerToMoveToTheBackOfQueue);
+                }
+
+                Console.WriteLine("Removed {0}", players.Dequeue());
+            }
+
+            Console.WriteLine("Last is {0}", players.Peek());
         }
     }
 }
