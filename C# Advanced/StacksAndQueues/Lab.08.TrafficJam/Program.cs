267a4dd097cd3be9da3886b0c50ee92818aac18d
DenisM <denis.m.031997@gmail.com>
2022-03-02 13:09:22 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Lab.08.TrafficJam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
