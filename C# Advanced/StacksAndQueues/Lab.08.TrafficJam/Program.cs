87180eea3c6b89bd56914b595540ea56eff6b3fc
DenisM <denis.m.031997@gmail.com>
2022-03-02 13:10:31 +0200

    Lab.08 uploaded.

--- C# Advanced/StacksAndQueues/Lab.08.TrafficJam/Program.cs
+++ C# Advanced/StacksAndQueues/Lab.08.TrafficJam/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._08.TrafficJam
 {
@@ -6,7 +8,33 @@ namespace Lab._08.TrafficJam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nCarsCanPassAtGivenTime = int.Parse(Console.ReadLine());
+            string carModel = Console.ReadLine();
+            Queue<string> carsInTheQueue = new Queue<string>();
+            ushort countCarsPassed = 0;
+
+            while (carModel != "end")
+            {
+                if (carModel == "green")
+                {
+                    for (int i = 0; i < nCarsCanPassAtGivenTime; i++)
+                    {
+                        if (carsInTheQueue.Any())
+                        {
+                            Console.WriteLine($"{carsInTheQueue.Dequeue()} passed!");
+                            countCarsPassed++;
+                        }
+                    }
+                }
+                else
+                {
+                    carsInTheQueue.Enqueue(carModel);
+                }
+
+                carModel = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{countCarsPassed} cars passed the crossroads.");
         }
     }
 }
