a4b5b6db673b69f4d09e8a93bff263f82f342850
Denis M <denis.m.031997@gmail.com>
2022-03-09 21:52:34 +0200

    Ex.04 ModV1 uploaded.

---
+++ C# Advanced/StacksAndQueues/Ex.04.FastFoodModV1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex._04.FastFoodModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantitiy = int.Parse(Console.ReadLine());
+            int[] sequenceInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> orders = new Queue<int>(sequenceInput);
+
+            if (orders.Any())
+            {
+                Console.WriteLine(orders.Max());
+            }
+
+            int ordersCountConst = orders.Count;
+
+            for (int i = 0; i < ordersCountConst; i++)//Can use the length of sequenceInput also
+            {
+                bool enoughtFoodQuantityLeft = foodQuantitiy - orders.Peek() >= 0;
+
+                if (enoughtFoodQuantityLeft)
+                {
+                    foodQuantitiy -= orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
