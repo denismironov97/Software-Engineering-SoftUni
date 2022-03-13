8d6d1842a79f64d1498a64e0e82799273e0f1e26
Denis M <denis.m.031997@gmail.com>
2022-03-13 16:44:16 +0200

    Ex.03 ModV1 uploaded.

--- C# Advanced/StacksAndQueues/Ex.03.MaximumAndMinimumElementV1/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.03.MaximumAndMinimumElementV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._03.MaximumAndMinimumElementV1
 {
@@ -6,7 +8,40 @@ namespace Ex._03.MaximumAndMinimumElementV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLines = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int[] dataCommandInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                switch (dataCommandInfo[0])
+                {
+                    case 1:
+                        numbers.Push(dataCommandInfo[1]);
+                        break;
+                    case 2:
+                        if (numbers.Any())
+                        {
+                            numbers.Pop();
+                        }
+                        break;
+                    case 3:
+                        if (numbers.Any())
+                        {
+                            Console.WriteLine($"{numbers.Max()}");
+                        }
+                        break;
+                    case 4:
+                        if (numbers.Any())
+                        {
+                            Console.WriteLine($"{numbers.Min()}");
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
         }
     }
 }
