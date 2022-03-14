721200bb709ea28c39986a1d2ae863d5bfd6aa8c
Denis M <denis.m.031997@gmail.com>
2022-03-14 19:28:20 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Ex.02.BasicQueueOperationsV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._02.BasicQueueOperationsV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
