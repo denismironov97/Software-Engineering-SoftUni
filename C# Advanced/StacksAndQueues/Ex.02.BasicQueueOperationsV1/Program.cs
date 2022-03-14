ce2cf3a34e0777477909e8e92deea3f46d6c7940
Denis M <denis.m.031997@gmail.com>
2022-03-14 19:29:22 +0200

    Ex.02 ModV1 uploaded.

--- C# Advanced/StacksAndQueues/Ex.02.BasicQueueOperationsV1/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.02.BasicQueueOperationsV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._02.BasicQueueOperationsV1
 {
@@ -6,7 +8,40 @@ namespace Ex._02.BasicQueueOperationsV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] infoData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int nElementsToEnqueue = infoData[0];
+            int sElementsToDequeue = infoData[1];
+            int xElementToLook = infoData[2];
+            int[] numsInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>();
+
+            foreach (int num in numsInput)
+            {
+                numbers.Enqueue(num);
+            }
+
+            for (int i = 0; i < sElementsToDequeue; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            bool isValInQueuePresent = numbers.Contains(xElementToLook);
+
+            if (isValInQueuePresent)
+            {
+                Console.WriteLine(isValInQueuePresent.ToString().ToLower());
+            }
+            else
+            {
+                if (numbers.Any())
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
         }
     }
 }
