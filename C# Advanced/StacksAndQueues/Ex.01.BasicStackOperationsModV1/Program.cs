e1bef86cd39e887c2667fd858a1a00cd30d0a64d
Denis M <denis.m.031997@gmail.com>
2022-03-15 20:56:45 +0200

    Ex.01 ModV1 solution uploaded.

--- C# Advanced/StacksAndQueues/Ex.01.BasicStackOperationsModV1/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.01.BasicStackOperationsModV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._01.BasicStackOperationsModV1
 {
@@ -6,7 +8,60 @@ namespace Ex._01.BasicStackOperationsModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] infoData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int nElementsToStack = infoData[0];
+            int sElementsToPop = infoData[1];
+            int xElementToLook = infoData[2];
+
+            int[] numsInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            //Stack<int> stack = new Stack<int>(numsInput);
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int num in numsInput)
+            {
+                stack.Push(num);
+            }
+
+            for (int i = 0; i < sElementsToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            bool isValueInStack = stack.Contains(xElementToLook);
+
+            if (isValueInStack)
+            {
+                Console.WriteLine(isValueInStack.ToString().ToLower());
+            }
+            else
+            {
+                //1
+                //int minVal = int.MaxValue;
+
+                //foreach (int num in stack)
+                //{
+                //    if (num < minVal)
+                //    {
+                //        minVal = num;
+                //    }
+                //}
+
+                //if (minVal == int.MaxValue)
+                //{
+                //    minVal = 0;
+                //}
+
+                //Console.WriteLine(minVal);
+
+                if (stack.Any())
+                {
+                    Console.WriteLine(stack.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
         }
     }
 }
