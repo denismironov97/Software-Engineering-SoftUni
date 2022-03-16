5eab04c06f9092a738dac845457d2415cdf676de
Denis M <denis.m.031997@gmail.com>
2022-03-16 17:39:38 +0200

    Solution uploaded.

--- C# Advanced/StacksAndQueues/Ex.05.FashionBoutique/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.05.FashionBoutique/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._05.FashionBoutique
 {
@@ -6,7 +8,63 @@ namespace Ex._05.FashionBoutique
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //5 4 8 6 3 8 7 7 9
+            //16
+
+            int[] clothesValues = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();//clothes
+            int capacityOfRack = int.Parse(Console.ReadLine());
+            Stack<int> rack = new Stack<int>(clothesValues);
+            //int rackCount = 0;V01
+            int rackCount = 1;
+            int rackSum = 0;
+
+            while (rack.Any())
+            {
+                int currentVal = rack.Peek();
+
+                /*
+                //V01
+                if (currentVal + rackSum > capacityOfRack)
+                {
+                    rackSum = 0;
+                    rackCount++;
+                }
+                else if (currentVal + rackSum == capacityOfRack)
+                {
+                    rack.Pop();
+                    rackSum = 0;
+                    rackCount++;
+                }
+                else
+                {
+                    rackSum += rack.Pop();
+                }
+                */
+
+                //V02
+                rackSum += currentVal;
+
+                if (rackSum <= capacityOfRack)
+                {
+                    rack.Pop();
+                }
+                else
+                {
+                    rackCount++;
+                    rackSum = 0;
+                }
+
+            }
+
+            /*
+            //V01
+            if (rackSum > 0)
+            {
+                rackCount++;
+            }
+            */
+
+            Console.WriteLine(rackCount);
         }
     }
 }
