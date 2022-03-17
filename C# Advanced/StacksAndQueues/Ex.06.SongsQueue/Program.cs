ba1d66bf4398da3fde007498855ff5fb33dcbd05
Denis M <denis.m.031997@gmail.com>
2022-03-17 19:45:32 +0200

    Ex.06 solution uploaded.

--- C# Advanced/StacksAndQueues/Ex.06.SongsQueue/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.06.SongsQueue/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._06.SongsQueue
 {
@@ -6,7 +8,43 @@ namespace Ex._06.SongsQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] songsSequence = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> playlist = new Queue<string>(songsSequence);
+
+            while (playlist.Any())
+            {
+                string commandData = Console.ReadLine();
+
+                if (commandData == "Play")
+                {
+                    playlist.Dequeue();
+                }
+                else if (commandData.Contains("Add"))
+                {
+                    //string songToAdd = commandData.Replace("Add", string.Empty).TrimStart();//V01
+                    string songToAdd = commandData.Substring(4, commandData.Length - 4);//V02
+
+                    if (!CheckForDuplicate(playlist, songToAdd))
+                    {
+                        playlist.Enqueue(songToAdd);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songToAdd} is already contained!");
+                    }
+                }
+                else if (commandData == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", playlist));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+
+        static bool CheckForDuplicate(Queue<string> songsPlaylist, string specificSong)
+        {
+            return songsPlaylist.Contains(specificSong);
         }
     }
 }
