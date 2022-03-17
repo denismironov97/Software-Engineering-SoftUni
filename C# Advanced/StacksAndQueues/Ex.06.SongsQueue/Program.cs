2630e543a62cebb79b0bf7e420f4dd1a9d01c89e
Denis M <denis.m.031997@gmail.com>
2022-03-17 19:44:20 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Ex.06.SongsQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._06.SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
