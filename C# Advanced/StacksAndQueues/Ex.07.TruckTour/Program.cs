ffd3941df486ad384926f85f1e7bfc8ba596fc8e
Denis M <denis.m.031997@gmail.com>
2022-03-18 22:03:55 +0200

    Solution to Ex.07 uploaded.

--- C# Advanced/StacksAndQueues/Ex.07.TruckTour/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.07.TruckTour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._07.TruckTour
 {
@@ -6,7 +8,46 @@ namespace Ex._07.TruckTour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nStations = int.Parse(Console.ReadLine());
+            Queue<int[]> stationsDataCircle = new Queue<int[]>();
+            FillQueue(nStations, stationsDataCircle);
+
+            int totalFuel = 0;
+
+            for (int i = 0; i < nStations; i++)
+            {
+                int[] stationData = stationsDataCircle.Dequeue();
+                int fuel = stationData[0];
+                int distance = stationData[1];
+                totalFuel += fuel;
+
+                if (totalFuel >= distance)
+                {
+                    totalFuel -= distance;
+                }
+                else
+                {
+                    totalFuel = 0;
+                    i = -1;
+                }
+
+                stationsDataCircle.Enqueue(stationData);
+            }
+
+            Console.WriteLine(stationsDataCircle.Peek()[2]);
+        }
+
+        private static void FillQueue(int nStations, Queue<int[]> stationsDataCircle)
+        {
+            for (int i = 0; i < nStations; i++)
+            {
+                int indexPosOfStation = i;
+                string petrolStationsInfo = Console.ReadLine();
+                //petrolStationsInfo += $" {indexPosOfStation.ToString()}";
+                petrolStationsInfo += $" {indexPosOfStation}";
+                int[] petrolStationsData = petrolStationsInfo.Split(" ").Select(int.Parse).ToArray();
+                stationsDataCircle.Enqueue(petrolStationsData);
+            }
         }
     }
 }
