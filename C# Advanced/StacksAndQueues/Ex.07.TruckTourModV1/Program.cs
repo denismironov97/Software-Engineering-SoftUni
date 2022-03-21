4cfca60c6e573deeb3ad839ed74b4b2c21d2d1ac
Denis M <denis.m.031997@gmail.com>
2022-03-21 20:04:11 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Ex.07.TruckTourModV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._07.TruckTourModV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
