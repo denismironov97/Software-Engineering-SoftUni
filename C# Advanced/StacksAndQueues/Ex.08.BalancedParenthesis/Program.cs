60b275207be426ee6d70308f2f19f7011e29de03
Denis M <denis.m.031997@gmail.com>
2022-03-23 20:25:12 +0200

    Project created.

---
+++ C# Advanced/StacksAndQueues/Ex.08.BalancedParenthesis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
