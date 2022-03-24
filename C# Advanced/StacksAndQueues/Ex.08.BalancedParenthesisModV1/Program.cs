5d79bdabf03405f996ef9e18b5cd87cb6426ea37
Denis M <denis.m.031997@gmail.com>
2022-03-24 11:38:19 +0200

    Ex.08ModV1 solution uploaded.

--- C# Advanced/StacksAndQueues/Ex.08.BalancedParenthesisModV1/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.08.BalancedParenthesisModV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._08.BalancedParenthesisModV1
 {
@@ -6,7 +8,44 @@ namespace Ex._08.BalancedParenthesisModV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string expression = Console.ReadLine();
+            Stack<char> bracketsStack = new Stack<char>();
+            bool firstElementIsClosingBracket = expression[0] == ')' || expression[0] == ']' || expression[0] == '}';
+
+            if (firstElementIsClosingBracket)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            foreach (char currSymbol in expression)
+            {
+                if (bracketsStack.Any())
+                {
+                    char openingBracket = bracketsStack.Peek();
+
+                    if (openingBracket == '{' && currSymbol == '}')
+                    {
+                        bracketsStack.Pop();
+                        continue;
+                    }
+                    else if (openingBracket == '[' && currSymbol == ']')
+                    {
+                        bracketsStack.Pop();
+                        continue;
+                    }
+                    else if (openingBracket == '(' && currSymbol == ')')
+                    {
+                        bracketsStack.Pop();
+                        continue;
+                    }
+                }
+
+                bracketsStack.Push(currSymbol);
+            }
+
+            string resusltMessage = bracketsStack.Count == 0 ? "YES" : "NO";
+            Console.WriteLine(resusltMessage);
         }
     }
 }
