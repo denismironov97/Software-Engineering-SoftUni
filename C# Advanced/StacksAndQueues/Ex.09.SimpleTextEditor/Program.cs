121d7b8be0332ba6548b60854d6cd72b48d4b43e
Denis M <denis.m.031997@gmail.com>
2022-03-25 20:52:47 +0200

    Ex.09 solution uplaoded.

--- C# Advanced/StacksAndQueues/Ex.09.SimpleTextEditor/Program.cs
+++ C# Advanced/StacksAndQueues/Ex.09.SimpleTextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex._09.SimpleTextEditor
 {
@@ -6,7 +7,41 @@ namespace Ex._09.SimpleTextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nCommands = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+            Stack<string> messageBox = new Stack<string>();
+
+            for (int i = 0; i < nCommands; i++)
+            {
+                string[] commandData = Console.ReadLine().Split();
+                int numCommand = int.Parse(commandData[0]);
+
+                if (numCommand == 1)
+                {
+                    messageBox.Push(message);
+                    string messageToAppend = commandData[1];
+                    message += messageToAppend;
+                }
+                else if (numCommand == 2)
+                {
+                    int countToRemove = int.Parse(commandData[1]);
+                    messageBox.Push(message);
+                    //message = message.Substring(0, message.Length - countToRemove); //important index position algorithm
+
+                    int startIndex = message.Length - countToRemove;
+                    message = message.Remove(startIndex, countToRemove);
+                }
+                else if (numCommand == 3)
+                {
+                    int returnIndex = int.Parse(commandData[1]) - 1;
+                    char returnSymbol = message[returnIndex];
+                    Console.WriteLine(returnSymbol);
+                }
+                else if (numCommand == 4)
+                {
+                    message = messageBox.Pop();
+                }
+            }
         }
     }
 }
