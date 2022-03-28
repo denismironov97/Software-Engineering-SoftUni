120153fde17a360d1492f057801032917d84eff9
Denis M <denis.m.031997@gmail.com>
2022-03-28 20:34:09 +0300

    Solution MultidimensionalArrays created.

---
+++ C# Advanced/MultidimensionalArrays/Lab.01.SumMatrixElements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
