3f84519bdfcbfc386ec313d23b9b125488c8867b
Denis M <denis.m.031997@gmail.com>
2022-03-28 20:37:07 +0300

    Lab.01 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Lab.01.SumMatrixElements/Program.cs
+++ C# Advanced/MultidimensionalArrays/Lab.01.SumMatrixElements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lab._01.SumMatrixElements
 {
@@ -6,7 +7,36 @@ namespace Lab._01.SumMatrixElements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] dimensions = ParseArrayFromConsole(Console.ReadLine());
+            int rows = dimensions[0];
+            int columns = dimensions[1];
+            int[,] matrix = new int[rows, columns];
+
+            for (int row = 0; row < matrix.GetLength(0); row++) //matrix.GetLength(0) => rows
+            {
+                int[] currentRow = ParseArrayFromConsole(Console.ReadLine());
+
+                for (int col = 0; col < matrix.GetLength(1); col++) // matrix.GetLength(1) => columns
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sumOfElementsInMatrix = 0;
+
+            foreach (int element in matrix)
+            {
+                sumOfElementsInMatrix += element;
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(columns);
+            Console.WriteLine(sumOfElementsInMatrix);
+        }
+
+        private static int[] ParseArrayFromConsole(string input)
+        {
+            return input.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
         }
     }
 }
