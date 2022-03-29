ba1926abd76ce52606ad307b3e32e1de3d26ca03
Denis M <denis.m.031997@gmail.com>
2022-03-29 21:43:56 +0300

    Project created.

---
+++ C# Advanced/MultidimensionalArrays/Lab.02.SumMatrixColumns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._02.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
