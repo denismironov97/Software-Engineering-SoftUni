9484db60eab7515fdb27bf461197ba322c9caa97
Denis M <denis.m.031997@gmail.com>
2022-03-29 21:45:29 +0300

    Lab.02 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Lab.02.SumMatrixColumns/Program.cs
+++ C# Advanced/MultidimensionalArrays/Lab.02.SumMatrixColumns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lab._02.SumMatrixColumns
 {
@@ -6,7 +7,35 @@ namespace Lab._02.SumMatrixColumns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int columns = dimensions[1];
+            int[,] matrix = new int[rows, columns];
+            int[] columnsSum = new int[columns];
+
+            for (int row = 0; row < matrix.GetLength(0); row++) // rows
+            {
+                int[] currentRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++) // columns
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    int currentElement = matrix[row, col];
+                    columnsSum[col] += currentElement;
+                }
+            }
+
+            foreach (int colSum in columnsSum)
+            {
+                Console.WriteLine(colSum);
+            }
         }
     }
 }
