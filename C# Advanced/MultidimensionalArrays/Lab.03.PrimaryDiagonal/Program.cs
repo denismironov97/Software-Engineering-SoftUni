a16627371dfd8ce476b3ea76a8bc173802bc79fd
Denis M <denis.m.031997@gmail.com>
2022-03-30 23:50:21 +0300

    Lab.03 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Lab.03.PrimaryDiagonal/Program.cs
+++ C# Advanced/MultidimensionalArrays/Lab.03.PrimaryDiagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lab._03.PrimaryDiagonal
 {
@@ -6,7 +7,54 @@ namespace Lab._03.PrimaryDiagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dimensions = int.Parse(Console.ReadLine());//square 
+            int rows = dimensions;
+            int columns = dimensions;
+            int[,] matrix = new int[rows, columns];
+
+            for (int row = 0; row < matrix.GetLength(0); row++) //rows
+            {
+                int[] currentRow = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++) //columns
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int sumOfmatrixPrimaryDiagonal = 0;
+            /*
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = row; col < matrix.GetLength(1);)
+                {
+                    int primaryDiagonalNum = matrix[row, col];
+                    sumOfmatrixPrimaryDiagonal += primaryDiagonalNum;
+                    break;
+                }
+            }
+            */
+
+            /*
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int col = row;
+                int primaryDiagonalNum = matrix[row, col];
+                sumOfmatrixPrimaryDiagonal += primaryDiagonalNum;
+            }
+            */
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                //Since it's a square this means that rows = cols
+                int primaryDiagonalNum = matrix[row, row];
+                sumOfmatrixPrimaryDiagonal += primaryDiagonalNum;
+            }
+
+            Console.WriteLine(sumOfmatrixPrimaryDiagonal);
         }
     }
 }
