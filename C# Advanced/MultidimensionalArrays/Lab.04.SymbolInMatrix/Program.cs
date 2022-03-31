47f110b798cec97bb31142e0892039089649c2c5
Denis M <denis.m.031997@gmail.com>
2022-03-31 19:53:47 +0300

    Project created.

---
+++ C# Advanced/MultidimensionalArrays/Lab.04.SymbolInMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
