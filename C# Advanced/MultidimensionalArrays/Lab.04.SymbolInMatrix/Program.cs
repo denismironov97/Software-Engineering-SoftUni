22bf9dbae70e80551ecacbe4ffbdcf995502126d
Denis M <denis.m.031997@gmail.com>
2022-03-31 19:55:26 +0300

    Lab.04 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Lab.04.SymbolInMatrix/Program.cs
+++ C# Advanced/MultidimensionalArrays/Lab.04.SymbolInMatrix/Program.cs
@@ -6,7 +6,56 @@ namespace Lab._04.SymbolInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dimensions = int.Parse(Console.ReadLine());
+            int rows = dimensions;
+            int columns = dimensions;
+            char[,] squareMatrix = new char[rows, columns];
+
+            for (int row = 0; row < squareMatrix.GetLength(0); row++) //rows
+            {
+                //char[] currentRow = Console.ReadLine().Split(string.Empty).Select(symbol => char.Parse(symbol)).ToArray();
+                char[] currentRow = Console.ReadLine().ToCharArray();
+                //var currentRow = Console.ReadLine();
+
+                for (int col = 0; col < squareMatrix.GetLength(1); col++) //columns
+                {
+                    squareMatrix[row, col] = currentRow[col];
+                }
+            }
+
+            char symbolToFind = char.Parse(Console.ReadLine());
+            int[] coordinatesOfSymbol = new int[2]; // x[0] and y[1] coordinates 
+            bool isSymbolFound = false;
+
+            for (int row = 0; row < squareMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < squareMatrix.GetLength(1); col++)
+                {
+                    char currentElementFromMatrix = squareMatrix[row, col];
+
+                    if (currentElementFromMatrix == symbolToFind)
+                    {
+                        coordinatesOfSymbol[0] = row; //x
+                        coordinatesOfSymbol[1] = col; //y
+                        isSymbolFound = true;
+                        break;
+                    }
+                }
+
+                if (isSymbolFound)
+                {
+                    break;
+                }
+            }
+
+            if (isSymbolFound)
+            {
+                Console.WriteLine($"({string.Join(", ", coordinatesOfSymbol)})");
+            }
+            else
+            {
+                Console.WriteLine($"{symbolToFind} does not occur in the matrix");
+            }
         }
     }
 }
