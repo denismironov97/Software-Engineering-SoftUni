da1178e820700e3bc5712ecbda525c8cfaf112df
DenisM <denis.m.031997@gmail.com>
2022-04-11 18:10:44 +0300

    Solution Lab.06 uploaded.

--- C# Advanced/MultidimensionalArrays/Lab.06.JaggedArrayModification/Program.cs
+++ C# Advanced/MultidimensionalArrays/Lab.06.JaggedArrayModification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lab._06.JaggedArrayModification
 {
@@ -6,7 +7,53 @@ namespace Lab._06.JaggedArrayModification
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rows][];
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                int[] currentRowInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                jaggedArray[row] = currentRowInput;
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine().ToLower();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] commandData = command.Split();
+                string instruction = commandData[0];
+                int row = int.Parse(commandData[1]);
+                int col = int.Parse(commandData[2]);
+                int value = int.Parse(commandData[3]);
+
+                if (row < 0
+                    || col < 0
+                    || row >= rows
+                    || jaggedArray[row].Length <= col) // valid coordinates 
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (instruction == "add")
+                {
+                    jaggedArray[row][col] += value;
+                }
+                else //subtract 
+                {
+                    jaggedArray[row][col] -= value;
+                }
+            }
+
+            foreach (int[] row in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
         }
     }
 }
