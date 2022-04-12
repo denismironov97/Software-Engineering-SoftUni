f75c309da74cc32acc24e454be009b0cddeea525
DenisM <denis.m.031997@gmail.com>
2022-04-12 15:28:37 +0300

    Lab.07 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Lab.07.PascalTriangle/Program.cs
+++ C# Advanced/MultidimensionalArrays/Lab.07.PascalTriangle/Program.cs
@@ -6,7 +6,33 @@ namespace Lab._07.PascalTriangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[rows][];
+
+            for (int row = 0; row < pascalTriangle.Length; row++)
+            {
+                long[] colum = new long[row + 1];
+                pascalTriangle[row] = colum;
+
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    if (col > 0 && col < pascalTriangle[row].Length - 1)
+                    {
+                        pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                    }
+                    else
+                    {
+                        pascalTriangle[row][col] = 1;
+                    }
+
+                    // check the algorithm tomorrow 
+                }
+            }
+
+            foreach (long[] pascalRow in pascalTriangle)
+            {
+                Console.WriteLine(string.Join(" ", pascalRow));
+            }
         }
     }
 }
