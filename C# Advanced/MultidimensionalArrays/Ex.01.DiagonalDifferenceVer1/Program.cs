254798b9b646974f54dca0ee354c4e70bf71a68b
DenisM <denis.m.031997@gmail.com>
2022-04-21 17:47:44 +0300

    Ex.01 Version 1 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Ex.01.DiagonalDifferenceVer1/Program.cs
+++ C# Advanced/MultidimensionalArrays/Ex.01.DiagonalDifferenceVer1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Ex._01.DiagonalDifferenceVer1
 {
@@ -6,7 +7,36 @@ namespace Ex._01.DiagonalDifferenceVer1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int side = int.Parse(Console.ReadLine());
+            int[,] squareMatrix = new int[side, side];
+            ReadMatrix(squareMatrix);
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            int indexCol = squareMatrix.GetLength(0) - 1;
+
+            for (int row = 0; row < squareMatrix.GetLength(0); row++)
+            {
+                primaryDiagonalSum += squareMatrix[row, row];
+                secondaryDiagonalSum += squareMatrix[row, indexCol];
+                indexCol--;
+            }
+
+            int diffAbs = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+            Console.WriteLine(diffAbs);
+        }
+
+        private static void ReadMatrix(int[,] squareMatrix)
+        {
+            for (int row = 0; row < squareMatrix.GetLength(0); row++)
+            {
+                int[] currRowsColumnElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < squareMatrix.GetLength(1); col++)
+                {
+                    squareMatrix[row, col] = currRowsColumnElements[col];
+                }
+            }
         }
     }
 }
