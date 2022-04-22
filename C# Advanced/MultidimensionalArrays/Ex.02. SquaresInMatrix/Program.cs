d7128d5756e7a9329974336d284373de1d8cc80c
DenisM <denis.m.031997@gmail.com>
2022-04-22 19:04:24 +0300

    Project created.

---
+++ C# Advanced/MultidimensionalArrays/Ex.02. SquaresInMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._02._SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
