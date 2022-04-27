f8c122d10a6520b4ccbdb425d46a81c794c404af
DenisM <denis.m.031997@gmail.com>
2022-04-27 22:34:57 +0300

    Project created.

---
+++ C# Advanced/MultidimensionalArrays/Ex.04.MatrixShuffling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
