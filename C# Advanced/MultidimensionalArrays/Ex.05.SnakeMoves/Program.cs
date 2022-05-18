a79e82d1cacc5295c9cb526f3131a1ccf114f61b
DenisM <denis.m.031997@gmail.com>
2022-05-18 16:24:01 +0300

    Project created.

---
+++ C# Advanced/MultidimensionalArrays/Ex.05.SnakeMoves/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
