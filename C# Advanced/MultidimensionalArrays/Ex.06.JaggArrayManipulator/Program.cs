aaffda8ec1eab15d226774de42f5f5c46c5965a5
Denis M <denis.m.031997@gmail.com>
2022-05-20 16:45:50 +0300

    Project created.

---
+++ C# Advanced/MultidimensionalArrays/Ex.06.JaggArrayManipulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._06.JaggArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
