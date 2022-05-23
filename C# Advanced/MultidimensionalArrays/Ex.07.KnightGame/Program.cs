b4e26cc05dbc6a2224cc49dedb4ca994a637de78
Denis M <denis.m.031997@gmail.com>
2022-05-23 16:18:01 +0300

    Ex.07 solution uploaded.

--- C# Advanced/MultidimensionalArrays/Ex.07.KnightGame/Program.cs
+++ C# Advanced/MultidimensionalArrays/Ex.07.KnightGame/Program.cs
@@ -6,7 +6,141 @@ namespace Ex._07.KnightGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dimensionsOfBoard = int.Parse(Console.ReadLine());
+            int rows = dimensionsOfBoard;
+            int columns = dimensionsOfBoard;
+            char[,] chessBoard = new char[rows, columns];
+            ReadMatrix(chessBoard);
+
+            int countOfKReplaced = 0;
+            int rowOfKnightCordinates = 0;
+            int colOfKnightCoordinates = 0;
+
+            while (true)
+            {
+                int maxAttacks = int.MinValue;
+
+                for (int row = 0; row < chessBoard.GetLength(0); row++)
+                {
+                    for (int col = 0; col < chessBoard.GetLength(1); col++)
+                    {
+                        char currentSymbol = chessBoard[row, col];
+                        int countOfAttacks = 0;
+
+                        if (currentSymbol == 'K')
+                        {
+                            countOfAttacks = GetNumOfAttacks(chessBoard, row, col, countOfAttacks);
+
+                            if (countOfAttacks > maxAttacks)
+                            {
+                                maxAttacks = countOfAttacks;
+                                rowOfKnightCordinates = row;
+                                colOfKnightCoordinates = col;
+                            }
+                        }
+                    }
+                }
+
+                if (maxAttacks > 0)
+                {
+                    chessBoard[rowOfKnightCordinates, colOfKnightCoordinates] = '0';
+                    countOfKReplaced++;
+                }
+                else if (maxAttacks == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(countOfKReplaced);
+        }
+
+        static void ReadMatrix(char[,] chessBoard)
+        {
+            for (int row = 0; row < chessBoard.GetLength(0); row++)
+            {
+                char[] currentRow = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < chessBoard.GetLength(1); col++)
+                {
+                    chessBoard[row, col] = currentRow[col];
+                }
+            }
+        }
+
+        static bool ValidCoordinatesForKnightToAttack(char[,] chessBoard, int rowTarget, int colTarget)
+        {
+            return rowTarget >= 0 && rowTarget < chessBoard.GetLength(0)
+                && colTarget >= 0 && colTarget < chessBoard.GetLength(1);
+        }
+
+        static int GetNumOfAttacks(char[,] chessBoard, int row, int col, int countOfAttacks)
+        {
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row + 2, col - 1))
+            {
+                if (chessBoard[row + 2, col - 1] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row + 2, col + 1))
+            {
+                if (chessBoard[row + 2, col + 1] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row + 1, col - 2))
+            {
+                if (chessBoard[row + 1, col - 2] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row + 1, col + 2))
+            {
+                if (chessBoard[row + 1, col + 2] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row - 1, col - 2))
+            {
+                if (chessBoard[row - 1, col - 2] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row - 1, col + 2))
+            {
+                if (chessBoard[row - 1, col + 2] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row - 2, col - 1))
+            {
+                if (chessBoard[row - 2, col - 1] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            if (ValidCoordinatesForKnightToAttack(chessBoard, row - 2, col + 1))
+            {
+                if (chessBoard[row - 2, col + 1] == 'K')
+                {
+                    countOfAttacks++;
+                }
+            }
+
+            return countOfAttacks;
         }
     }
 }
