37a67b9291e9b97d6b074a67eb2ade9f7eda5353
Denis M <denis.m.031997@gmail.com>
2022-05-24 20:10:59 +0300

    Lab.01 solution uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.01.CountSameValuesInArray/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.01.CountSameValuesInArray/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._01.CountSameValuesInArray
 {
@@ -6,7 +8,36 @@ namespace Lab._01.CountSameValuesInArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbersInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Dictionary<double, int> numbersOccurrence = new Dictionary<double, int>();//key: number value occuranceOfTheNumber which is the key
+
+            //foreach (int num in numbersInput)//ver01
+            //{
+            //    if (!numbersOccurrence.ContainsKey(num))
+            //    {
+            //        numbersOccurrence[num] = 0;
+            //    }
+
+            //    numbersOccurrence[num]++;
+            //}
+
+            foreach (int num in numbersInput)
+            {
+                if (!numbersOccurrence.ContainsKey(num))
+                {
+                    numbersOccurrence.Add(num, 1);
+                }
+                else
+                {
+                    numbersOccurrence[num]++;
+                }
+            }
+
+
+            foreach (var kvp in numbersOccurrence)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
         }
     }
 }
