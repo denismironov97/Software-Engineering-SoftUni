d509432e1e6d52f06f570215ccccf2f4aac4e9c3
Denis M <denis.m.031997@gmail.com>
2022-06-09 17:55:49 +0300

    Project created.

---
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.02.AvStdntGrdsV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._02.AvStdntGrdsV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
