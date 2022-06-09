9f1af199a64065ba939869cf34100856883b27a6
Denis M <denis.m.031997@gmail.com>
2022-06-09 17:57:30 +0300

    Lab.02 ModV1 uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.02.AvStdntGrdsV1/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.02.AvStdntGrdsV1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab._02.AvStdntGrdsV1
 {
@@ -6,7 +8,41 @@ namespace Lab._02.AvStdntGrdsV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLinesInput = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentNameGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < nLinesInput; i++)
+            {
+                string[] studentData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string studentName = studentData[0];
+                decimal grade = decimal.Parse(studentData[1]);
+
+                if (!studentNameGrades.ContainsKey(studentName))
+                {
+                    List<decimal> gradesOfStudent = new List<decimal>();
+                    studentNameGrades.Add(studentName, gradesOfStudent);
+                }
+
+                studentNameGrades[studentName].Add(grade);
+            }
+
+            //Stamat -> 2.00 3.00 (avg: 2.50)
+
+            foreach (var kvp in studentNameGrades)
+            {
+                string name = kvp.Key;
+                List<decimal> gradesOfStudent = kvp.Value;
+
+                Console.Write($"{name} -> ");
+
+                foreach (decimal grade in gradesOfStudent)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+
+                decimal averageGrade = gradesOfStudent.Average();
+                Console.WriteLine($"(avg: {averageGrade:f2})");
+            }
         }
     }
 }
