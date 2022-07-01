6b9ace12b7474303d56ff42902360a72d8089ff8
Denis M <denis.m.031997@gmail.com>
2022-07-01 16:56:01 +0300

    Lab.03 solution uplaoded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.03.ProductShop/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.03.ProductShop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab._03.ProductShop
 {
@@ -6,7 +7,45 @@ namespace Lab._03.ProductShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputDataInformation = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            var storeProductPrice = new SortedDictionary<string, Dictionary<string, double>>();//storeBrand ->KVP[productType(key) productPrice(value)]
+
+            while (inputDataInformation[0] != "Revision")
+            {
+                string storeBrand = inputDataInformation[0];
+                string productType = inputDataInformation[1];
+                double productPrice = double.Parse(inputDataInformation[2]);
+
+                if (!storeProductPrice.ContainsKey(storeBrand))
+                {
+                    storeProductPrice.Add(storeBrand, new Dictionary<string, double>());
+                }
+
+                if (!storeProductPrice[storeBrand].ContainsKey(productType))
+                {
+                    storeProductPrice[storeBrand].Add(productType, productPrice);
+                }
+                else
+                {
+                    storeProductPrice[storeBrand][productType] = productPrice;
+                }
+
+                //storeProductPrice[storeBrand][productType] = productPrice;
+
+                inputDataInformation = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var kvp in storeProductPrice)
+            {
+                string storeName = kvp.Key;
+                Dictionary<string, double> typeProductPrice = kvp.Value;
+                Console.WriteLine($"{storeName}->");
+
+                foreach (var kvpOftypeProductPrice in typeProductPrice)
+                {
+                    Console.WriteLine($"Product: {kvpOftypeProductPrice.Key}, Price: {kvpOftypeProductPrice.Value}");
+                }
+            }
         }
     }
 }
