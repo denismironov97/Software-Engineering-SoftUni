06e50d2b25ded506bc2073b6ca36c3af751b6701
DenisM <denis.m.031997@gmail.com>
2022-07-18 18:30:51 +0300

    Project created.

---
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.03.ProductShopV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._03.ProductShopV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
