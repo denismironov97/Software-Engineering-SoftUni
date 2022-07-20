01a460a7d3859f8ea8356a83b1346fa6b068492d
DenisM <denis.m.031997@gmail.com>
2022-07-20 09:07:14 +0300

    Project created.

---
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.04.CtsByCntCtrV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._04.CtsByCntCtrV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
