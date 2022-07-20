b311991fa4e7745523413661725d2bcc39efe318
DenisM <denis.m.031997@gmail.com>
2022-07-20 09:05:45 +0300

    Solution for Lab.04 uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.04.CtsByContCtry/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.04.CtsByContCtry/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab._04.CtsByContCtry
 {
@@ -6,7 +7,53 @@ namespace Lab._04.CtsByContCtry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLines = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> continentsCountriesCities = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < nLines; i++)
+            {
+                string[] informationParts = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = informationParts[0];
+                string country = informationParts[1];
+                string city = informationParts[2];
+
+                if (!continentsCountriesCities.ContainsKey(continent))
+                {
+                    continentsCountriesCities.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!continentsCountriesCities[continent].ContainsKey(country))
+                {
+                    continentsCountriesCities[continent].Add(country, new List<string>());
+                }
+
+                continentsCountriesCities[continent][country].Add(city);
+            }
+
+            foreach (var kvp in continentsCountriesCities)
+            {
+                string continent = kvp.Key;
+                Console.WriteLine($"{continent}:");
+
+                Dictionary<string, List<string>> countiesCities = kvp.Value;
+
+                foreach (var kvpCountryCities in countiesCities)
+                {
+                    string country = kvpCountryCities.Key;
+                    List<string> cities = kvpCountryCities.Value;
+
+                    Console.WriteLine($"  {country} -> {string.Join(", ", cities)}");
+                }
+            }
+
+            //Europe:
+            //  Bulgaria->Sofia, Plovdiv
+            //  Poland->Warsaw, Poznan
+            //  Germany->Berlin
+
+            //Asia:
+            //  China->Beijing, Shanghai
+            //  Japan->Tokyo
         }
     }
 }
