f5cfc93e162af5b34c9ca653d79e2ab87f37c98d
DenisM <denis.m.031997@gmail.com>
2022-07-25 15:05:28 +0300

    Solution for Lab.06 uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.06.ParkingLot/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.06.ParkingLot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab._06.ParkingLot
 {
@@ -6,7 +7,35 @@ namespace Lab._06.ParkingLot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputDataParts = Console.ReadLine().Split(", ");
+            HashSet<string> licenceNumbers = new HashSet<string>();
+
+            while (inputDataParts[0] != "END")
+            {
+                string carLicenceNumber = inputDataParts[1];
+
+                switch (inputDataParts[0])
+                {
+                    case "IN":
+                        licenceNumbers.Add(carLicenceNumber);
+                        break;
+                    case "OUT":
+                        licenceNumbers.Remove(carLicenceNumber);
+                        break;
+                }
+
+                inputDataParts = Console.ReadLine().Split(", ");
+            }
+
+            if (licenceNumbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+            foreach (string licenceNum in licenceNumbers)
+            {
+                Console.WriteLine(licenceNum);
+            }
         }
     }
 }
