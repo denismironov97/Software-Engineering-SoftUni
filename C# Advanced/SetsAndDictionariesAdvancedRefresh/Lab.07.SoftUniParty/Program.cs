4a7305000e30bc6f495393734b703e92d9976f88
DenisM <denis.m.031997@gmail.com>
2022-07-28 19:41:04 +0300

    Lab.07 solution uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.07.SoftUniParty/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.07.SoftUniParty/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Lab._07.SoftUniParty
 {
@@ -6,7 +8,53 @@ namespace Lab._07.SoftUniParty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> vipGuestsInvites = new HashSet<string>();
+            HashSet<string> regularGuestsInvites = new HashSet<string>();
+            Regex regex = new Regex(@"(?<Vip>\d{1}\w{7})|(?<Guest>\w{8})");
+
+            while (true)
+            {
+                string reservationInvites = Console.ReadLine();
+
+                if (regex.IsMatch(reservationInvites))
+                {
+                    if (regex.Match(reservationInvites).Groups["Vip"].Success)
+                    {
+                        string vipReservation = regex.Match(reservationInvites).Groups["Vip"].Value;
+                        vipGuestsInvites.Add(vipReservation);
+                    }
+                    else
+                    {
+                        string regularReservation = regex.Match(reservationInvites).Groups["Guest"].Value;
+                        regularGuestsInvites.Add(regularReservation);
+                    }
+                }
+                else if (reservationInvites == "PARTY")
+                {
+                    string actuallyPresentGuests = Console.ReadLine();
+
+                    while (actuallyPresentGuests != "END")
+                    {
+                        vipGuestsInvites.Remove(actuallyPresentGuests);
+                        regularGuestsInvites.Remove(actuallyPresentGuests);
+                        actuallyPresentGuests = Console.ReadLine();
+                    }
+
+                    break;
+                }
+            }
+
+            Console.WriteLine(vipGuestsInvites.Count + regularGuestsInvites.Count);
+
+            foreach (string vips in vipGuestsInvites) //printing out the vips who did not came to the event
+            {
+                Console.WriteLine(vips);
+            }
+
+            foreach (string regulars in regularGuestsInvites) //printing out the regulars who did not came to the event
+            {
+                Console.WriteLine(regulars);
+            }
         }
     }
 }
