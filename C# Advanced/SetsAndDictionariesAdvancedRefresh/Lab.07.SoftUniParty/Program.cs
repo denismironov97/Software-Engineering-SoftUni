bcfc14d1ef99b2a9ec4bb93b76e04dc00cf31920
DenisM <denis.m.031997@gmail.com>
2022-07-28 19:39:50 +0300

    Project created.

---
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Lab.07.SoftUniParty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
