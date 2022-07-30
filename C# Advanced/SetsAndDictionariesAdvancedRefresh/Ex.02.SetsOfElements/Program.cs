c62e4de0a5110907665a089b873c89bf55664a7a
DenisM <denis.m.031997@gmail.com>
2022-07-30 13:13:59 +0300

    Solution to Ex.02 uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Ex.02.SetsOfElements/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Ex.02.SetsOfElements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Ex._02.SetsOfElements
 {
@@ -6,7 +8,32 @@ namespace Ex._02.SetsOfElements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] lenghtOfSet1AndSet2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            for (int i = 0; i < lenghtOfSet1AndSet2[0]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set1.Add(number);
+            }
+
+            for (int i = 0; i < lenghtOfSet1AndSet2[1]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set2.Add(number);
+            }
+
+            foreach (int numInSet1 in set1)
+            {
+                foreach (int numInSet2 in set2)
+                {
+                    if (numInSet1 == numInSet2)
+                    {
+                        Console.Write($"{numInSet1} ");
+                    }
+                }
+            }
         }
     }
 }
