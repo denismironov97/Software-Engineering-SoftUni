c51597fac396fbfea21c7ed6956c8a32fe40de59
DenisM <denis.m.031997@gmail.com>
2022-07-30 13:15:15 +0300

    Solution to Ex.03 uploaded.

---
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Ex.03.PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex._03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLinesOfInput = int.Parse(Console.ReadLine());
+            SortedSet<string> uniqueChemCompounds = new SortedSet<string>();
+
+            for (int i = 0; i < nLinesOfInput; i++)
+            {
+                string[] chemicalCompoundInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < chemicalCompoundInput.Length; j++)
+                {
+                    uniqueChemCompounds.Add(chemicalCompoundInput[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", uniqueChemCompounds));
+        }
+    }
+}
