06be650eca312a84afc7dee38115259560b7b983
DenisM <denis.m.031997@gmail.com>
2022-08-01 21:38:56 +0300

    Project created.

---
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Ex.04.EvenTimes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex._04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
