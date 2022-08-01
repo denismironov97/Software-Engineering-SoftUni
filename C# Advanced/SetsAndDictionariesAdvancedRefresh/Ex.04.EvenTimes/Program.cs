94276a19bd4af72a1130c56b75d90ab14f7126fd
DenisM <denis.m.031997@gmail.com>
2022-08-01 21:39:57 +0300

    Ex.04 solution uploaded.

--- C# Advanced/SetsAndDictionariesAdvancedRefresh/Ex.04.EvenTimes/Program.cs
+++ C# Advanced/SetsAndDictionariesAdvancedRefresh/Ex.04.EvenTimes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ex._04.EvenTimes
 {
@@ -6,7 +7,30 @@ namespace Ex._04.EvenTimes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nLines = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numberCountOfOccurrence = new Dictionary<int, int>();
+
+            for (int i = 0; i < nLines; i++)
+            {
+                int currNumber = int.Parse(Console.ReadLine());
+
+                if (!numberCountOfOccurrence.ContainsKey(currNumber))
+                {
+                    numberCountOfOccurrence.Add(currNumber, 1);
+                }
+                else
+                {
+                    numberCountOfOccurrence[currNumber]++;
+                }
+            }
+
+            foreach (var kvp in numberCountOfOccurrence)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
         }
     }
 }
