472d53c1452a1825250ec34df3e730ebfc5ad24a
DenisM <denis.m.031997@gmail.com>
2022-08-09 16:58:15 +0300

    Project created.

---
+++ C# Advanced/FunctionalProgramming/Lab.02.SumNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab._02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
