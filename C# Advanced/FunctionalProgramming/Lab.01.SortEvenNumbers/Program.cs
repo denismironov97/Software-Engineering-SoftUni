762058c5ba15c8723611c0040c9597c58549d1fe
DenisM <denis.m.031997@gmail.com>
2022-08-09 16:53:49 +0300

    Solution functional programming created.

---
+++ C# Advanced/FunctionalProgramming/Lab.01.SortEvenNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab._01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
