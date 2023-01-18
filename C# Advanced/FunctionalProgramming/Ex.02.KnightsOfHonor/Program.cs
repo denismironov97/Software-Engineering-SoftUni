96e5472d165b89f8c478a3d59bdedd161f157a3e
DenisM <denis.m.031997@gmail.com>
2023-01-18 16:12:32 +0200

    Added Ex.02 .NET5

---
+++ C# Advanced/FunctionalProgramming/Ex.02.KnightsOfHonor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex._02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string[], string[]> addingKnightTitleFunc = x => x.Select(item => "Sir " + item).ToArray();
+
+            Action<string[]> printAction = x =>
+            {
+                foreach (string name in x)
+                {
+                    Console.WriteLine(name);
+                }
+            };
+
+            printAction(addingKnightTitleFunc(names));
+        }
+    }
+}
