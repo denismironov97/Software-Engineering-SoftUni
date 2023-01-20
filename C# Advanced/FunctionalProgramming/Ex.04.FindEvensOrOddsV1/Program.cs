b3f1fbec1c4489d21a4ddb4c9b9b00c9b6f9a581
DenisM <denis.m.031997@gmail.com>
2023-01-20 13:14:00 +0200

    Ex.04 V1 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.04.FindEvensOrOddsV1/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.04.FindEvensOrOddsV1/Program.cs
@@ -2,6 +2,34 @@
 {
     private static void Main(string[] args)
     {
-        
+        int[] bounds = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        int lowerBound = bounds[0];
+        int upperBound = bounds[1];
+
+        string command = Console.ReadLine();
+
+        Predicate<int> filterPredicate = n =>
+        {
+            if (command == "even")
+            {
+                return n % 2 == 0;
+            }
+            else
+            {
+                return n % 2 != 0;
+            }
+        };
+
+        List<int> resultCollection = new List<int>();
+
+        for (int i = lowerBound; i <= upperBound; i++)
+        {
+            if (filterPredicate(i))
+            {
+                resultCollection.Add(i);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", resultCollection));
     }
 }
