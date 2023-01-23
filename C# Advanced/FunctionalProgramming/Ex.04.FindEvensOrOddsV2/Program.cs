fc497f41216420f08e88f14c4d000e5d54f9dc16
DenisM <denis.m.031997@gmail.com>
2023-01-23 16:46:35 +0200

    Ex.04  V2 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.04.FindEvensOrOddsV2/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.04.FindEvensOrOddsV2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ex._04.FindEvensOrOddsV2
 {
@@ -6,7 +8,43 @@ namespace Ex._04.FindEvensOrOddsV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] bounds = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            int lowerBound = bounds[0];
+            int upperBound = bounds[1];
+
+            string command = Console.ReadLine();
+
+            Predicate<int> filterPredicate;
+
+            if (command == "even")
+            {
+                filterPredicate = x => x % 2 == 0;
+            }
+            else
+            {
+                filterPredicate = x => x % 2 != 0;
+            }
+
+            Func<int, int, Predicate<int>, List<int>> getEvenOrOddCollDelegate = GetCollectionOfNums;
+
+            var resultCollection = getEvenOrOddCollDelegate(lowerBound, upperBound, filterPredicate);
+
+            Console.WriteLine(string.Join(" ", resultCollection));
+        }
+
+        private static List<int> GetCollectionOfNums(int lowerBound, int upperBound, Predicate<int> predicateAr)
+        {
+            List<int> resultCollection = new List<int>();
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (predicateAr(i))
+                {
+                    resultCollection.Add(i);
+                }
+            }
+
+            return resultCollection;
         }
     }
 }
