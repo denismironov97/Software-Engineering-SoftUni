08073d18ef95d260d8674463f515a9b98d32bea3
DenisM <denis.m.031997@gmail.com>
2023-01-24 13:31:11 +0200

    Ex.05 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.05.AppliedArithmetics/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.05.AppliedArithmetics/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+string commandType = Console.ReadLine();
+while (commandType != "end")
+{
+    if (commandType == "add")
+    {
+        numbers = numbers.Select(x => x + 1).ToArray();
+    }
+    else if (commandType == "multiply")
+    {
+        numbers = numbers.Select(x => x * 2).ToArray();
+    }
+    else if (commandType == "subtract")
+    {
+        numbers = numbers.Select(x => x - 1).ToArray();
+    }
+    else if (commandType == "print")
+    {
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+
+    commandType = Console.ReadLine();
+}
