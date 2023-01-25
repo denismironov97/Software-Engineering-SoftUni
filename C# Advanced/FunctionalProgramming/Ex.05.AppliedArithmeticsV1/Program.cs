b38f4d3be78573c41b59cdabd24d44d8dad7282f
DenisM <denis.m.031997@gmail.com>
2023-01-25 11:39:16 +0200

    Ex.05V1 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.05.AppliedArithmeticsV1/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.05.AppliedArithmeticsV1/Program.cs
@@ -2,6 +2,42 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+        string commandType = Console.ReadLine();
+        while (commandType != "end")
+        {
+            Action<int[]> printArray = array => Console.WriteLine(string.Join(" ", array));
+            if (commandType == "print")
+            {
+                printArray(numbers);
+            }
+            else
+            {
+                Func<int, int> procesorOfOperationsFunc = (x) =>
+                {
+                    if (commandType == "add")
+                    {
+                        return x + 1;
+                    }
+                    else if (commandType == "multiply")
+                    {
+                        return x * 2;
+                    }
+                    else if (commandType == "subtract")
+                    {
+                        return x - 1;
+                    }
+                    else
+                    {
+                        return x;
+                    }
+                };
+
+                numbers = numbers.Select(procesorOfOperationsFunc).ToArray();
+            }
+
+            commandType = Console.ReadLine();
+        }
     }
 }
