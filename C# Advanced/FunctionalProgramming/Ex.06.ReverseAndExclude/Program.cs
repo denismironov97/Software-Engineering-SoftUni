2d416a26947f8d3509fc86671f45da8d3c0fd8e6
DenisM <denis.m.031997@gmail.com>
2023-01-26 13:45:08 +0200

    Ex.06 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.06.ReverseAndExclude/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.06.ReverseAndExclude/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        int divisor = int.Parse(Console.ReadLine());
+        int[] resultColl = numbers.Where(x => x % divisor != 0).ToArray();
+        resultColl = resultColl.Reverse().ToArray();
+        Console.WriteLine(string.Join(' ', resultColl));
+    }
+}
