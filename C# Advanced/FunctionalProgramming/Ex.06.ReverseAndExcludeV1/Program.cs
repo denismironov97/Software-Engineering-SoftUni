aaf56b0257ffeb2689181904f4844916fb963dd1
DenisM <denis.m.031997@gmail.com>
2023-01-27 15:16:00 +0200

    Ex.06 V1 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.06.ReverseAndExcludeV1/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.06.ReverseAndExcludeV1/Program.cs
@@ -2,6 +2,33 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        int divisor = int.Parse(Console.ReadLine());
+
+        Func<int[], int[]> reverseArrayFunc = array =>
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                int tempVal = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = tempVal;
+            }
+
+            return array;
+        };
+
+        numbers = reverseArrayFunc(numbers);
+        Predicate<int> predicate = n => n % divisor != 0;
+        Queue<int> filteredCollection = new Queue<int>();
+
+        foreach (int num in numbers)
+        {
+            if (predicate(num))
+            {
+                filteredCollection.Enqueue(num);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", filteredCollection));
     }
 }
