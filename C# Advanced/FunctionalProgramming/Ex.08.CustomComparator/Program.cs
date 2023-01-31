12367f6f8c44fe1e0ed33ae96f2cc6c083e4ee49
DenisM <denis.m.031997@gmail.com>
2023-01-31 16:23:31 +0200

    Ex.08 solution provided.

--- C# Advanced/FunctionalProgramming/Ex.08.CustomComparator/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.08.CustomComparator/Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Func<List<int>, List<int>> returnEvens = inpList => inpList.Where(x => Math.Abs(x) % 2 == 0).ToList();
+        Func<List<int>, List<int>> returnOdds = inpList => inpList.Where(x => Math.Abs(x) % 2 == 1).ToList();
+
+        List<int> numsInput = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        List<int> sortedEvens = returnEvens(numsInput).OrderBy(x => x).ToList();
+        List<int> sortedOdds = returnOdds(numsInput).OrderBy(x => x).ToList();
+
+        List<int> resultList = sortedEvens.Concat(sortedOdds).ToList();
+        Console.WriteLine(string.Join(" ", resultList));
     }
 }
