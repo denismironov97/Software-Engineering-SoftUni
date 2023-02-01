f209bd728c1b04c26d065cdc2c1e0388cf0f380d
DenisM <denis.m.031997@gmail.com>
2023-02-01 21:27:40 +0200

    Ex.09 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.09.ListOfPredicates/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.09.ListOfPredicates/Program.cs
@@ -2,5 +2,29 @@
 {
     private static void Main(string[] args)
     {
+        int numN = int.Parse(Console.ReadLine());
+        int[] divisors = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        List<int> allNumbers = new List<int>();
+
+        for (int i = 1; i <= numN; i++)
+        {
+            allNumbers.Add(i);
+        }
+
+        Func<int, int[], bool> checkDivisorsFunc = (number, divisorsArr) =>
+        {
+            bool devided = true;
+            for (int i = 0; i < divisorsArr.Length; i++)
+            {
+                if (number % divisorsArr[i] != 0)
+                {
+                    devided = false;
+                }
+            }
+            return devided;
+        };
+
+        int[] resultColl = allNumbers.Where(x => checkDivisorsFunc(x, divisors)).ToArray();
+        Console.WriteLine(string.Join(' ', resultColl));
     }
 }
