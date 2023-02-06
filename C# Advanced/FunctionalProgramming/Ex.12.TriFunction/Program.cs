33fa88bd9efd1356cb4773061aecce01a469beb6
DenisM <denis.m.031997@gmail.com>
2023-02-06 10:06:29 +0200

    Ex.12 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.12.TriFunction/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.12.TriFunction/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int readSumChar = int.Parse(Console.ReadLine());
+        List<string> listNames = Console.ReadLine().Split().ToList();
+        Func<string, int> returnCharSum = inpString => inpString.Select(x => (int)x).Sum();
+
+        for (int i = 0; i < listNames.Count; i++)
+        {
+            if (returnCharSum(listNames[i]) >= readSumChar)
+            {
+                Console.WriteLine(listNames[i]);
+                break;
+            }
+        }
+    }
+}
