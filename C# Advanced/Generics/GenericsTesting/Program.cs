2b5c5f0622564481573dbdfe517a9c5064e3a073
DenisM <denis.m.031997@gmail.com>
2023-02-07 14:11:29 +0200

    Made changes.

--- C# Advanced/Generics/GenericsTesting/Program.cs
+++ C# Advanced/Generics/GenericsTesting/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Box<int> numBox = new Box<int>();
+        numBox.Add(55);
+        numBox.Add(33);
+        numBox.Add(99);
+        numBox.Remove();
+        Console.WriteLine(numBox);
+    }
+}
+
+public class Box<T>
+{
+    private Stack<T> innerCollcetion;
+
+    public Box()
+    {
+        this.innerCollcetion = new Stack<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.innerCollcetion.Count;
+        }
+    }
+
+    public void Add(T element)
+    {
+        this.innerCollcetion.Push(element);
+    }
+
+    public T Remove()
+    {
+        return this.innerCollcetion.Pop();
+    }
+}
