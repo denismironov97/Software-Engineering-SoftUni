888138d44e84000fcf742689ab981cdac7b1327a
DenisM <denis.m.031997@gmail.com>
2023-02-07 13:48:52 +0200

    Ex.12 V1 solution uploaded.

--- C# Advanced/FunctionalProgramming/Ex.12.TriFunctionV1/Program.cs
+++ C# Advanced/FunctionalProgramming/Ex.12.TriFunctionV1/Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int number = int.Parse(Console.ReadLine());
+
+        List<string> names = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+        Console.WriteLine(names
+            .FirstOrDefault(x => x.ToCharArray()
+            .Select(y => (int)y)
+            .Sum() >= number));
     }
 }
