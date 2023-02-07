122fad1bf9baa1fbb52882c74c893964802b87cd
DenisM <denis.m.031997@gmail.com>
2023-02-07 14:22:52 +0200

    Ex.01 uploaded.

---
+++ C# Advanced/DefiningClasses/Ex.01.DefineAClassPerson/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._01.DefineAClassPerson
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        public Person()
+        {
+            this.Name = "Default";
+            this.Age = 0;
+        }
+
+        public Person(string name, int age)
+                : this()
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/Ex.01.DefineAClassPerson/Program.cs
@@ -0,0 +1,15 @@
+﻿using Ex._01.DefineAClassPerson;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person();
+        Person person1 = new Person("Name", 99);
+        Person person2 = new Person()
+        {
+            Name = "Name",
+            Age = 99
+        };
+    }
+}
